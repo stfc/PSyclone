7345580b4963b826b296c2a127a606e2e601ba74
rupertford <rupertford@4bbb03d4-c1cc-11e1-9ca4-71d347b01581>
2014-02-14 16:41:35 +0000

    original galerkin rhs example

---
+++ examples/galerkin_rhs/manual_galerkin.F90
@@ -0,0 +1,35 @@
+program main
+  use lfric
+  use algorithm
+  use read_triangle_module
+  use manual_invoke_populate_rhs
+  use manual_invoke_galerkin_rhs_kernel
+  implicit none
+
+  type(element_type) :: h_cg1_element, v_cg1_element
+  type(element_type) :: h_dg0_element, v_dg0_element
+  type(FunctionSpace_type), pointer :: cg1_space, dg0_space
+  type(state_type) :: state
+  type(field_type), pointer :: x, rhs, f
+
+  integer :: i
+
+  call read_triangle(state, "../data/unitsquare.1",&
+       & layer_heights=[real(dp) :: (i/1000., i=0,1000)])
+
+  x => state%extract_field("Coordinate")
+  
+  h_cg1_element = new_Element("CG", cell=referenceTriangle, degree=1)
+  v_cg1_element = new_Element("CG", cell=referenceInterval, degree=1)
+  
+  h_dg0_element = new_Element("DG", cell=referenceTriangle, degree=0)
+  v_dg0_element = new_Element("DG", cell=referenceInterval, degree=0)
+
+  cg1_space => new_FunctionSpace("CG1_space", state%topology, h_cg1_element*v_cg1_element)
+  dg0_space => new_FunctionSpace("DG0_space", state%topology, h_dg0_element*v_dg0_element)
+
+  rhs => new_Field("rhs", cg1_space)
+  f => new_Field("f", dg0_space)
+  call invoke_populate_rhs(x, f)
+  call invoke_galerkin_rhs_kernel(x, rhs, f)
+end program main
