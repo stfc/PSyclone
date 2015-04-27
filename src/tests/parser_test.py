3fab9e9a12e75f23c655ec1df9eed6b3cfff5a14
andrewporter <andrewporter@4bbb03d4-c1cc-11e1-9ca4-71d347b01581>
2015-04-27 12:28:20 +0000

    #221 Add tests that specifying two kernels with different grid-offsets in both a single and multiple invokes raises a ParseError

--- src/tests/parser_test.py
+++ src/tests/parser_test.py
@@ -15,3 +15,15 @@ class TestParserGungHoProto:
     def test_single_invoke_undeclared(self):
         with pytest.raises(ParseError):
             parse(os.path.join(os.path.dirname(os.path.abspath(__file__)),"test_files","gunghoproto","2_undeclared_function.f90"),api="gunghoproto")
+
+class TestParserGOcean1p0:
+
+    def test_kernels_different_grid_offsets_one_invoke(self):
+        with pytest.raises(ParseError):
+            parse(os.path.join(os.path.dirname(os.path.abspath(__file__)),"test_files","gocean1p0","different_grid_offsets_one_invoke.f90"),api="gocean1.0")
+
+    def test_kernels_different_grid_offsets_two_invokes(self):
+        ''' Check that we raise an error if the kernels in different invokes 
+            specify different grid offsets. '''
+        with pytest.raises(ParseError):
+            parse(os.path.join(os.path.dirname(os.path.abspath(__file__)),"test_files","gocean1p0","different_grid_offsets_two_invokes.f90"),api="gocean1.0")
