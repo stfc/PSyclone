a613974a109027a316ecec744c75f703d710df8f
andrewporter <andrewporter@4bbb03d4-c1cc-11e1-9ca4-71d347b01581>
2015-05-08 13:51:57 +0000

    #221 Update the parser tests to reflect the new names of the test files

--- src/tests/parser_test.py
+++ src/tests/parser_test.py
@@ -20,10 +20,16 @@ class TestParserGOcean1p0:
 
     def test_kernels_different_grid_offsets_one_invoke(self):
         with pytest.raises(ParseError):
-            parse(os.path.join(os.path.dirname(os.path.abspath(__file__)),"test_files","gocean1p0","different_grid_offsets_one_invoke.f90"),api="gocean1.0")
+            parse(os.path.join(os.path.dirname(os.path.abspath(__file__)),"test_files","gocean1p0","test01_different_grid_offsets_one_invoke.f90"),api="gocean1.0")
 
     def test_kernels_different_grid_offsets_two_invokes(self):
         ''' Check that we raise an error if the kernels in different invokes 
             specify different grid offsets. '''
         with pytest.raises(ParseError):
-            parse(os.path.join(os.path.dirname(os.path.abspath(__file__)),"test_files","gocean1p0","different_grid_offsets_two_invokes.f90"),api="gocean1.0")
+            parse(os.path.join(os.path.dirname(os.path.abspath(__file__)),"test_files","gocean1p0","test02_different_grid_offsets_two_invokes.f90"),api="gocean1.0")
+
+    def test_kernel_missing_index_offset(self):
+        ''' Check that we raise an error if a kernel's meta-data is 
+            missing the index_offset field. '''
+        with pytest.raises(ParseError):
+            parse(os.path.join(os.path.dirname(os.path.abspath(__file__)),"test_files","gocean1p0","test03_invoke_kernel_missing_offset.f90"),api="gocean1.0")
