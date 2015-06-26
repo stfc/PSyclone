c69d88194984f670db1a0e4643acb82b96719b9b
andrewporter <andrewporter@4bbb03d4-c1cc-11e1-9ca4-71d347b01581>
2015-06-26 12:44:48 +0000

    #251 PSyclone: add test that parser raises ParseError if kernel contains invalid Fortran

---
+++ src/tests/test_files/gocean1p0/kernel_invalid_fortran.f90
@@ -0,0 +1,45 @@
+module kernel_invalid_fortran
+  !use kind_params_mod
+  !use kernel_mod
+  !use argument_mod
+  !use field_mod
+  !use grid_mod
+  implicit none
+
+  private
+
+  public compute_cu, compute_cu_code
+
+  type, extends(kernel_type) :: compute_cu
+     type(arg), dimension(4) :: meta_args =    &
+          (/ arg(WRITE, CU, POINTWISE),        & ! cu
+             arg(READ,  CT, POINTWISE),        & ! p
+             arg(READ,  CU, POINTWISE),        & ! u
+             arg(READ,  GRID_AREA_T)       &
+           /)
+     integer :: ITERATES_OVER = INTERNAL_PTS
+
+     integer :: index_offset = OFFSET_SW
+
+  contains
+    procedure, nopass :: code => compute_cu_code
+    ! Deliberately mis-spell 'type' below so that
+    ! this is not valid Fortran
+  end tpe compute_cu
+
+contains
+
+  !===================================================
+
+  !> Compute the mass flux in the x direction at point (i,j)
+  subroutine compute_cu_code(i, j, cu, p, u)
+    implicit none
+    integer,  intent(in) :: I, J
+    real(wp), intent(out), dimension(:,:) :: cu
+    real(wp), intent(in),  dimension(:,:) :: p, u
+
+    CU(I,J) = 0.5d0*(P(i+1,J)+P(I,J))*U(I,J)
+
+  end subroutine compute_cu_code
+
+end module kernel_invalid_fortran
