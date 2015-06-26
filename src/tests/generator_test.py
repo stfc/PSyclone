244916cd9e240034bcc80beb326e26a75125454a
andrewporter <andrewporter@4bbb03d4-c1cc-11e1-9ca4-71d347b01581>
2015-06-26 12:26:25 +0000

    #251 PSyclone: Tests in TestGenerate would fail if py.test was run from anywhere other than the tests directory. Added full path to cwd to the paths supplied to generate().

--- src/tests/generator_test.py
+++ src/tests/generator_test.py
@@ -14,51 +14,87 @@ class TestGenerate:
    ''' unit tests for the generate function '''
 
    def test_non_existant_filename(self):
-       ''' checks that algGen raises appropriate error when a non-existant filename is supplied '''       
+       ''' checks that algGen raises appropriate error when a
+           non-existant filename is supplied '''       
        import pytest
        with pytest.raises(IOError):
            generate("non_existant_file.f90")
 
    def test_invalid_api(self):
-       ''' checks that algGen raises appropriate error when an invalid api is supplied '''       
+       ''' checks that algGen raises appropriate error when an invalid
+           api is supplied '''       
        import pytest
        with pytest.raises(GenerationError):
-           generate(os.path.join("test_files","dynamo0p1","algorithm","1_single_function.f90"), api="invalid")
+           generate(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                 "test_files", "dynamo0p1", "algorithm",
+                                 "1_single_function.f90"), api="invalid")
 
    def test_invalid_kernel_path(self):
-      ''' checks that algGen raises appropriate error when an invalid search path for kernel source files is supplied '''
+      ''' checks that algGen raises appropriate error when an invalid
+          search path for kernel source files is supplied '''
       with pytest.raises(IOError):
-         generate(os.path.join("test_files","dynamo0p1","algorithm","1_single_function.f90"), api="dynamo0.1", kernel_path="does_not_exist")
+         generate(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                               "test_files", "dynamo0p1", "algorithm",
+                               "1_single_function.f90"),
+                  api="dynamo0.1",
+                  kernel_path="does_not_exist")
 
    def test_wrong_kernel_path(self):
-      ''' checks that algGen raises appropriate error when the kernel code cannot be found in the specified search path '''
+      ''' checks that algGen raises appropriate error when the kernel
+          code cannot be found in the specified search path '''
+      root_path = os.path.dirname(os.path.abspath(__file__))
       with pytest.raises(IOError):
-         generate(os.path.join("test_files","dynamo0p1","algorithm","1_single_function.f90"), api="dynamo0.1", kernel_path=os.path.join("test_files","gocean0p1"))
+         generate(os.path.join(root_path,
+                               "test_files", "dynamo0p1", "algorithm",
+                               "1_single_function.f90"),
+                  api="dynamo0.1",
+                  kernel_path=os.path.join(root_path,
+                                           "test_files", "gocean0p1"))
 
    def test_correct_kernel_path(self):
-      ''' checks that algGen succeeds when the location of the kernel source code is *not* the same as that of the algorithm code '''
-      alg,psy=generate(os.path.join("test_files","dynamo0p1","algorithm","1_single_function.f90"), 
+      ''' checks that algGen succeeds when the location of the kernel
+          source code is *not* the same as that of the algorithm code '''
+      root_path = os.path.dirname(os.path.abspath(__file__))
+      alg,psy=generate(os.path.join(root_path,
+                                    "test_files", "dynamo0p1", "algorithm",
+                                    "1_single_function.f90"), 
                        api="dynamo0.1",
-                       kernel_path=os.path.join("test_files","dynamo0p1","kernels"))
+                       kernel_path=os.path.join(root_path, "test_files",
+                                                "dynamo0p1", "kernels"))
 
    def test_same_kernel_path(self):
-      ''' checks that the generator succeeds when the search directory is the same as the algorithm code directory and a path is specified '''
-      path=os.path.join("test_files","dynamo0p1","algorithm")
+      ''' checks that the generator succeeds when the search directory
+          is the same as the algorithm code directory and a path is
+          specified '''
+      path=os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                        "test_files", "dynamo0p1", "algorithm")
       alg,psy=generate(os.path.join(path,"1_single_function.f90"), 
                        api="dynamo0.1",
                        kernel_path=path)
 
    def test_similar_kernel_name(self):
       ''' checks that the generator does not match incorrect files '''
-      alg,psy=generate(os.path.join("test_files","dynamo0p1","algorithm","1_single_function.f90"), 
+      root_path = os.path.dirname(os.path.abspath(__file__))
+      alg,psy=generate(os.path.join(root_path,
+                                    "test_files", "dynamo0p1", "algorithm",
+                                    "1_single_function.f90"), 
                        api="dynamo0.1",
-                       kernel_path=os.path.join("test_files","dynamo0p1","kernels2"))
+                       kernel_path=os.path.join(root_path,
+                                                "test_files", "dynamo0p1",
+                                                "kernels2"))
 
    def test_recurse_correct_kernel_path(self):
-      ''' checks that the generator succeeds when the location of the kernel source code is *not* the same as that of the algorithm code and recursion through subdirectories is required '''
-      alg,psy=generate(os.path.join("test_files","dynamo0p1","algorithm","1_single_function.f90"), 
+      '''checks that the generator succeeds when the location of the kernel
+         source code is *not* the same as that of the algorithm code and
+         recursion through subdirectories is required'''
+      root_path = os.path.dirname(os.path.abspath(__file__))
+      alg,psy=generate(os.path.join(root_path,
+                                    "test_files", "dynamo0p1", "algorithm",
+                                    "1_single_function.f90"), 
                        api="dynamo0.1",
-                       kernel_path=os.path.join("test_files","dynamo0p1","kernels3"))
+                       kernel_path=os.path.join(root_path,
+                                                "test_files", "dynamo0p1",
+                                                "kernels3"))
 
 class TestGenerateGOcean1p0:
 
