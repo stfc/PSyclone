cc201d94bac08a56e66b765231170f3173ed9fc2
andrewporter <andrewporter@4bbb03d4-c1cc-11e1-9ca4-71d347b01581>
2015-08-18 16:12:05 +0000

    #272 PSyclone: WIP updating gocean1p0 tests now that we are generating code with constant loop bounds. 3 out of 4 tests still fail because generated code has changed.

--- src/tests/gocean1p0_test.py
+++ src/tests/gocean1p0_test.py
@@ -11,13 +11,19 @@ from psyGen import PSyFactory
 import os
 
 class TestPSyGOcean1p0API:
-    ''' Tests for PSy-layer code generation that are specific to the GOcean1.0 api. '''
+    '''Tests for PSy-layer code generation that are specific to the
+    GOcean 1.0 API.'''
 
     def test_field(self):
         ''' Tests that a kernel call with only fields produces correct code '''
-        ast, invokeInfo = parse(os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_files", "gocean1p0", "single_invoke.f90"), api="gocean1.0")
+        ast, invokeInfo = parse(os.path.join(os.path.\
+                                             dirname(os.path.\
+                                                     abspath(__file__)),
+                                             "test_files", "gocean1p0",
+                                             "single_invoke.f90"),
+                                api="gocean1.0")
         psy = PSyFactory("gocean1.0").create(invokeInfo)
-        generated_code = psy.gen
+        generated_code = str(psy.gen)
 
         expected_output = """  MODULE psy_single_invoke_test
     USE field_mod
@@ -29,20 +35,30 @@ class TestPSyGOcean1p0API:
       TYPE(r2d_field), intent(inout) :: cu_fld, p_fld, u_fld
       INTEGER j
       INTEGER i
-      DO j=cu_fld%internal%ystart,cu_fld%internal%ystop
-        DO i=cu_fld%internal%xstart,cu_fld%internal%xstop
+      INTEGER istop, jstop
+      !
+      ! Look-up loop bounds
+      istop = cu_fld%grid%simulation_domain%xstop
+      jstop = cu_fld%grid%simulation_domain%ystop
+      !
+      DO j=2,jstop
+        DO i=2,istop+1
           CALL compute_cu_code(i, j, cu_fld%data, p_fld%data, u_fld%data)
         END DO 
       END DO 
     END SUBROUTINE invoke_0_compute_cu
   END MODULE psy_single_invoke_test"""
 
-        assert str(generated_code).find(expected_output) != -1
+        assert generated_code.find(expected_output) != -1
 
     def test_two_kernels(self):
         ''' Tests that an invoke containing two kernel calls with only 
             fields as arguments produces correct code '''
-        ast, invokeInfo = parse(os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_files", "gocean1p0", "single_invoke_two_kernels.f90"), 
+        ast, invokeInfo = parse(os.path.join(os.path.\
+                                             dirname(os.path.\
+                                                     abspath(__file__)),
+                                             "test_files", "gocean1p0",
+                                             "single_invoke_two_kernels.f90"), 
                                 api="gocean1.0")
         psy = PSyFactory("gocean1.0").create(invokeInfo)
         generated_code = psy.gen
@@ -58,13 +74,19 @@ class TestPSyGOcean1p0API:
       TYPE(r2d_field), intent(inout) :: cu_fld, p_fld, u_fld, unew_fld, uold_fld
       INTEGER j
       INTEGER i
-      DO j=cu_fld%internal%ystart,cu_fld%internal%ystop
-        DO i=cu_fld%internal%xstart,cu_fld%internal%xstop
+      INTEGER istop, jstop
+      !
+      ! Look-up loop bounds
+      istop = cu_fld%grid%simulation_domain%xstop
+      jstop = cu_fld%grid%simulation_domain%ystop
+      !
+      DO j=2,jstop
+        DO i=2,istop
           CALL compute_cu_code(i, j, cu_fld%data, p_fld%data, u_fld%data)
         END DO 
       END DO 
-      DO j=1,SIZE(uold_fld%data, 2)
-        DO i=1,SIZE(uold_fld%data, 1)
+      DO j=1,jstop+1
+        DO i=1,istop+1
           CALL time_smooth_code(i, j, u_fld%data, unew_fld%data, uold_fld%data)
         END DO 
       END DO 
@@ -76,10 +98,14 @@ class TestPSyGOcean1p0API:
     def test_grid_property(self):
         ''' Tests that an invoke containing a kernel call requiring
             a property of the grid produces correct code '''
-        ast, invokeInfo = parse(os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_files", "gocean1p0", "single_invoke_grid_props.f90"), 
+        ast, invokeInfo = parse(os.path.join(os.path.\
+                                             dirname(os.path.\
+                                                     abspath(__file__)),
+                                             "test_files", "gocean1p0",
+                                             "single_invoke_grid_props.f90"), 
                                 api="gocean1.0")
         psy = PSyFactory("gocean1.0").create(invokeInfo)
-        generated_code = psy.gen
+        generated_code = str(psy.gen)
 
         expected_output = """  MODULE psy_single_invoke_with_grid_props_test
     USE field_mod
@@ -91,24 +117,35 @@ class TestPSyGOcean1p0API:
       TYPE(r2d_field), intent(inout) :: cu_fld, u_fld
       INTEGER j
       INTEGER i
-      DO j=cu_fld%internal%ystart,cu_fld%internal%ystop
-        DO i=cu_fld%internal%xstart,cu_fld%internal%xstop
+      INTEGER istop, jstop
+      !
+      ! Look-up loop bounds
+      istop = cu_fld%grid%simulation_domain%xstop
+      jstop = cu_fld%grid%simulation_domain%ystop
+      !
+      DO j=2,jstop
+        DO i=2,istop
           CALL next_sshu_code(i, j, cu_fld%data, u_fld%data, u_fld%grid%tmask, u_fld%grid%area_t, u_fld%grid%area_u)
         END DO 
       END DO 
     END SUBROUTINE invoke_0_next_sshu
   END MODULE psy_single_invoke_with_grid_props_test"""
 
-        assert str(generated_code).find(expected_output) != -1
+        print generated_code
+        assert generated_code.find(expected_output) != -1
 
 
     def test_scalar_int_arg(self):
         ''' Tests that an invoke containing a kernel call requiring
             an integer, scalar argument produces correct code '''
-        ast, invokeInfo = parse(os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_files", "gocean1p0", "single_invoke_scalar_int_arg.f90"), 
+        ast, invokeInfo = parse(os.path.join(os.path.\
+                                             dirname(os.path.\
+                                                     abspath(__file__)),
+                                             "test_files", "gocean1p0",
+                                             "single_invoke_scalar_int_arg.f90"), 
                                 api="gocean1.0")
         psy = PSyFactory("gocean1.0").create(invokeInfo)
-        generated_code = psy.gen
+        generated_code = str(psy.gen)
 
         expected_output = """  MODULE psy_single_invoke_scalar_int_test
     USE field_mod
@@ -121,6 +158,12 @@ class TestPSyGOcean1p0API:
       INTEGER, intent(inout) :: ncycle
       INTEGER j
       INTEGER i
+      INTEGER istop, jstop
+      !
+      ! Look-up loop bounds
+      istop = ssh_fld%grid%simulation_domain%xstop
+      jstop = ssh_fld%grid%simulation_domain%ystop
+      !
       DO j=ssh_fld%whole%ystart,ssh_fld%whole%ystop
         DO i=ssh_fld%whole%xstart,ssh_fld%whole%xstop
           CALL bc_ssh_code(i, j, ncycle, ssh_fld%data, ssh_fld%grid%tmask)
@@ -128,4 +171,5 @@ class TestPSyGOcean1p0API:
       END DO 
     END SUBROUTINE invoke_0_bc_ssh
   END MODULE psy_single_invoke_scalar_int_test"""
-        assert str(generated_code).find(expected_output) != -1
+        print generated_code
+        assert generated_code.find(expected_output) != -1
