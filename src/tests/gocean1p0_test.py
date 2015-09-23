7877237eac76fa8227ad2e9c89ad909651da083a
andrewporter <andrewporter@4bbb03d4-c1cc-11e1-9ca4-71d347b01581>
2015-09-23 14:36:34 +0000

    #272 PSyclone: add test for update of CT on internal points with SW offset

--- src/tests/gocean1p0_test.py
+++ src/tests/gocean1p0_test.py
@@ -417,7 +417,7 @@ def test_ne_offset_all_cf_points():
 
 
 def test_ne_offset_all_cf_points():
-    ''' Test that we can generate code for a kernel that expects a NE
+    ''' Test that we can generate code for a kernel that expects a SW
     offset and writes to a field on all CF points '''
     ast, invokeInfo = parse(os.path.\
                             join(os.path.\
@@ -428,7 +428,6 @@ def test_ne_offset_all_cf_points():
                             api="gocean1.0")
     psy = PSyFactory("gocean1.0").create(invokeInfo)
     generated_code = str(psy.gen)
-    print generated_code
 
     expected_output = """  MODULE psy_single_invoke_test
     USE field_mod
@@ -452,6 +451,47 @@ def test_ne_offset_all_cf_points():
         END DO 
       END DO 
     END SUBROUTINE invoke_0_compute_z
+  END MODULE psy_single_invoke_test"""
+
+    assert generated_code.find(expected_output) != -1
+
+
+def test_sw_offset_ct_points():
+    ''' Test that we can generate code for a kernel that expects a SW
+    offset and writes to a field on internal CT points '''
+    ast, invokeInfo = parse(os.path.\
+                            join(os.path.\
+                                 dirname(os.path.\
+                                         abspath(__file__)),
+                                 "test_files", "gocean1p0",
+                                 "test20_sw_offset_ct_updated_one_invoke.f90"), 
+                            api="gocean1.0")
+    psy = PSyFactory("gocean1.0").create(invokeInfo)
+    generated_code = str(psy.gen)
+    print generated_code
+
+    expected_output = """  MODULE psy_single_invoke_test
+    USE field_mod
+    USE kind_params_mod
+    IMPLICIT NONE
+    CONTAINS
+    SUBROUTINE invoke_0_compute_h(hfld, pfld, ufld, vfld)
+      USE kernel_sw_offset_ct_mod, ONLY: compute_h_code
+      TYPE(r2d_field), intent(inout) :: hfld, pfld, ufld, vfld
+      INTEGER j
+      INTEGER i
+      INTEGER istop, jstop
+      !
+      ! Look-up loop bounds
+      istop = hfld%grid%simulation_domain%xstop
+      jstop = hfld%grid%simulation_domain%ystop
+      !
+      DO j=2,jstop
+        DO i=2,istop
+          CALL compute_h_code(i, j, hfld%data, pfld%data, ufld%data, vfld%data)
+        END DO 
+      END DO 
+    END SUBROUTINE invoke_0_compute_h
   END MODULE psy_single_invoke_test"""
 
     assert generated_code.find(expected_output) != -1
---
+++ src/tests/test_files/gocean1p0/test20_sw_offset_ct_updated_one_invoke.f90
@@ -0,0 +1,51 @@
+!-------------------------------------------------------------------------------
+! (c) The copyright relating to this work is owned jointly by the Crown,
+! Met Office and NERC 2014.
+! However, it has been created with the help of the GungHo Consortium,
+! whose members are identified at https://puma.nerc.ac.uk/trac/GungHo/wiki
+!-------------------------------------------------------------------------------
+! Author A. Porter STFC Daresbury Lab
+! Funded by the GOcean project
+
+PROGRAM single_invoke_test
+
+  ! Fake Fortran program for testing aspects of
+  ! the PSyclone code generation system.
+
+  use kind_params_mod
+  use grid_mod
+  use field_mod
+  use kernel_sw_offset_ct_mod, only: compute_h
+  implicit none
+
+  type(grid_type), target :: model_grid
+  !> Pressure at current time step
+  type(r2d_field) :: p_fld
+  !> Velocity in x direction at current time step
+  type(r2d_field) :: u_fld
+  !> Mass flux in x direction at current time step
+  type(r2d_field) :: cu_fld
+
+  !> Loop counter for time-stepping loop
+  INTEGER :: ncycle
+
+  ! Create the model grid
+  model_grid = grid_type(ARAKAWA_C,                        &
+                         (/BC_PERIODIC,BC_PERIODIC,BC_NONE/) )
+
+  ! Create fields on this grid
+  pfld    = r2d_field(model_grid, T_POINTS)
+  hfld    = r2d_field(model_grid, T_POINTS)
+  ufld    = r2d_field(model_grid, U_POINTS)
+  vfld    = r2d_field(model_grid, v_POINTS)
+
+  !  ** Start of time loop ** 
+  DO ncycle=1,100
+    
+    call invoke( compute_h(hfld, pfld, ufld, vfld) )
+
+  END DO
+
+  !===================================================
+
+END PROGRAM single_invoke_test
