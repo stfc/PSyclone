80c5c53b4852d59151fa8a2575d4a0d4937f8adf
andrewporter <andrewporter@4bbb03d4-c1cc-11e1-9ca4-71d347b01581>
2015-09-23 13:54:54 +0000

    #272 PSyclone: add a test to cover generating code for a kernel with NE offset that writes to all points on CV

--- src/tests/gocean1p0_test.py
+++ src/tests/gocean1p0_test.py
@@ -334,3 +334,43 @@ def test_ne_offset_all_cu_points():
   END MODULE psy_single_invoke_test"""
 
     assert generated_code.find(expected_output) != -1
+
+
+def test_ne_offset_all_cv_points():
+    ''' Test that we can generate code for a kernel that expects a NE
+    offset and writes to a field on all CV points '''
+    ast, invokeInfo = parse(os.path.\
+                            join(os.path.\
+                                 dirname(os.path.\
+                                         abspath(__file__)),
+                                 "test_files", "gocean1p0",
+                                 "test17_ne_offset_cv_updated_one_invoke.f90"), 
+                            api="gocean1.0")
+    psy = PSyFactory("gocean1.0").create(invokeInfo)
+    generated_code = str(psy.gen)
+    print generated_code
+    expected_output = """  MODULE psy_single_invoke_test
+    USE field_mod
+    USE kind_params_mod
+    IMPLICIT NONE
+    CONTAINS
+    SUBROUTINE invoke_0_bc_solid_v(v_fld)
+      USE boundary_conditions_ne_offset_mod, ONLY: bc_solid_v_code
+      TYPE(r2d_field), intent(inout) :: v_fld
+      INTEGER j
+      INTEGER i
+      INTEGER istop, jstop
+      !
+      ! Look-up loop bounds
+      istop = v_fld%grid%simulation_domain%xstop
+      jstop = v_fld%grid%simulation_domain%ystop
+      !
+      DO j=1,jstop
+        DO i=1,istop+1
+          CALL bc_solid_v_code(i, j, v_fld%data, v_fld%grid%tmask)
+        END DO 
+      END DO 
+    END SUBROUTINE invoke_0_bc_solid_v
+  END MODULE psy_single_invoke_test"""
+
+    assert generated_code.find(expected_output) != -1
