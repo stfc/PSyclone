58fa641bf663649cfd2bcac6f3461d8b4f491b42
andrewporter <andrewporter@4bbb03d4-c1cc-11e1-9ca4-71d347b01581>
2015-09-23 13:08:21 +0000

    #272 PSyclone: add a test to cover generating code for a kernel with NE offset that writes to points on CF

--- src/tests/gocean1p0_test.py
+++ src/tests/gocean1p0_test.py
@@ -10,24 +10,21 @@ from parse import parse
 from psyGen import PSyFactory
 import os
 
-class TestPSyGOcean1p0API:
-    '''Tests for PSy-layer code generation that are specific to the
-    GOcean 1.0 API.'''
+'''Tests for PSy-layer code generation that are specific to the
+GOcean 1.0 API.'''
 
-    def test_field(self):
-        ''' Tests that a kernel call with only fields produces correct code '''
-        ast, invokeInfo = parse(os.path.join(os.path.\
-                                             dirname(os.path.\
-                                                     abspath(__file__)),
-                                             "test_files", "gocean1p0",
-                                             "single_invoke.f90"),
-                                api="gocean1.0")
-        psy = PSyFactory("gocean1.0").create(invokeInfo)
-        generated_code = str(psy.gen)
+def test_field():
+    ''' Tests that a kernel call with only fields produces correct code '''
+    ast, invokeInfo = parse(os.path.join(os.path.\
+                                         dirname(os.path.\
+                                                 abspath(__file__)),
+                                         "test_files", "gocean1p0",
+                                         "single_invoke.f90"),
+                            api="gocean1.0")
+    psy = PSyFactory("gocean1.0").create(invokeInfo)
+    generated_code = str(psy.gen)
 
-        print generated_code
-
-        expected_output = """  MODULE psy_single_invoke_test
+    expected_output = """  MODULE psy_single_invoke_test
     USE field_mod
     USE kind_params_mod
     IMPLICIT NONE
@@ -51,21 +48,22 @@ class TestPSyGOcean1p0API:
     END SUBROUTINE invoke_0_compute_cu
   END MODULE psy_single_invoke_test"""
 
-        assert generated_code.find(expected_output) != -1
+    assert generated_code.find(expected_output) != -1
 
-    def test_two_kernels(self):
-        ''' Tests that an invoke containing two kernel calls with only 
-            fields as arguments produces correct code '''
-        ast, invokeInfo = parse(os.path.join(os.path.\
-                                             dirname(os.path.\
-                                                     abspath(__file__)),
-                                             "test_files", "gocean1p0",
-                                             "single_invoke_two_kernels.f90"), 
-                                api="gocean1.0")
-        psy = PSyFactory("gocean1.0").create(invokeInfo)
-        generated_code = psy.gen
 
-        expected_output = """  MODULE psy_single_invoke_two_kernels
+def test_two_kernels():
+    ''' Tests that an invoke containing two kernel calls with only 
+    fields as arguments produces correct code '''
+    ast, invokeInfo = parse(os.path.join(os.path.\
+                                         dirname(os.path.\
+                                                 abspath(__file__)),
+                                         "test_files", "gocean1p0",
+                                         "single_invoke_two_kernels.f90"), 
+                            api="gocean1.0")
+    psy = PSyFactory("gocean1.0").create(invokeInfo)
+    generated_code = psy.gen
+
+    expected_output = """  MODULE psy_single_invoke_two_kernels
     USE field_mod
     USE kind_params_mod
     IMPLICIT NONE
@@ -95,21 +93,22 @@ class TestPSyGOcean1p0API:
     END SUBROUTINE invoke_0
   END MODULE psy_single_invoke_two_kernels"""
 
-        assert str(generated_code).find(expected_output) != -1
+    assert str(generated_code).find(expected_output) != -1
 
-    def test_grid_property(self):
-        ''' Tests that an invoke containing a kernel call requiring
-            a property of the grid produces correct code '''
-        ast, invokeInfo = parse(os.path.join(os.path.\
-                                             dirname(os.path.\
-                                                     abspath(__file__)),
-                                             "test_files", "gocean1p0",
-                                             "single_invoke_grid_props.f90"), 
-                                api="gocean1.0")
-        psy = PSyFactory("gocean1.0").create(invokeInfo)
-        generated_code = str(psy.gen)
 
-        expected_output = """  MODULE psy_single_invoke_with_grid_props_test
+def test_grid_property():
+    ''' Tests that an invoke containing a kernel call requiring
+    a property of the grid produces correct code '''
+    ast, invokeInfo = parse(os.path.join(os.path.\
+                                         dirname(os.path.\
+                                                 abspath(__file__)),
+                                         "test_files", "gocean1p0",
+                                         "single_invoke_grid_props.f90"), 
+                            api="gocean1.0")
+    psy = PSyFactory("gocean1.0").create(invokeInfo)
+    generated_code = str(psy.gen)
+
+    expected_output = """  MODULE psy_single_invoke_with_grid_props_test
     USE field_mod
     USE kind_params_mod
     IMPLICIT NONE
@@ -133,22 +132,22 @@ class TestPSyGOcean1p0API:
     END SUBROUTINE invoke_0_next_sshu
   END MODULE psy_single_invoke_with_grid_props_test"""
 
-        assert generated_code.find(expected_output) != -1
+    assert generated_code.find(expected_output) != -1
 
 
-    def test_scalar_int_arg(self):
-        ''' Tests that an invoke containing a kernel call requiring
-            an integer, scalar argument produces correct code '''
-        ast, invokeInfo = parse(os.path.join(os.path.\
-                                             dirname(os.path.\
-                                                     abspath(__file__)),
-                                             "test_files", "gocean1p0",
-                                             "single_invoke_scalar_int_arg.f90"), 
-                                api="gocean1.0")
-        psy = PSyFactory("gocean1.0").create(invokeInfo)
-        generated_code = str(psy.gen)
+def test_scalar_int_arg():
+    ''' Tests that an invoke containing a kernel call requiring
+    an integer, scalar argument produces correct code '''
+    ast, invokeInfo = parse(os.path.join(os.path.\
+                                         dirname(os.path.\
+                                                 abspath(__file__)),
+                                         "test_files", "gocean1p0",
+                                         "single_invoke_scalar_int_arg.f90"), 
+                            api="gocean1.0")
+    psy = PSyFactory("gocean1.0").create(invokeInfo)
+    generated_code = str(psy.gen)
 
-        expected_output = """  MODULE psy_single_invoke_scalar_int_test
+    expected_output = """  MODULE psy_single_invoke_scalar_int_test
     USE field_mod
     USE kind_params_mod
     IMPLICIT NONE
@@ -173,22 +172,22 @@ class TestPSyGOcean1p0API:
     END SUBROUTINE invoke_0_bc_ssh
   END MODULE psy_single_invoke_scalar_int_test"""
 
-        assert generated_code.find(expected_output) != -1
+    assert generated_code.find(expected_output) != -1
 
 
-    def test_scalar_float_arg(self):
-        ''' Tests that an invoke containing a kernel call requiring
-            a real, scalar argument produces correct code '''
-        ast, invokeInfo = parse(os.path.join(os.path.\
-                                             dirname(os.path.\
-                                                     abspath(__file__)),
-                                             "test_files", "gocean1p0",
-                                             "single_invoke_scalar_float_arg.f90"), 
-                                api="gocean1.0")
-        psy = PSyFactory("gocean1.0").create(invokeInfo)
-        generated_code = str(psy.gen)
+def test_scalar_float_arg():
+    ''' Tests that an invoke containing a kernel call requiring
+    a real, scalar argument produces correct code '''
+    ast, invokeInfo = parse(os.path.join(os.path.\
+                                         dirname(os.path.\
+                                                 abspath(__file__)),
+                                         "test_files", "gocean1p0",
+                                         "single_invoke_scalar_float_arg.f90"), 
+                            api="gocean1.0")
+    psy = PSyFactory("gocean1.0").create(invokeInfo)
+    generated_code = str(psy.gen)
 
-        expected_output = """  MODULE psy_single_invoke_scalar_float_test
+    expected_output = """  MODULE psy_single_invoke_scalar_float_test
     USE field_mod
     USE kind_params_mod
     IMPLICIT NONE
@@ -213,5 +212,45 @@ class TestPSyGOcean1p0API:
     END SUBROUTINE invoke_0_bc_ssh
   END MODULE psy_single_invoke_scalar_float_test"""
 
-        print generated_code
-        assert generated_code.find(expected_output) != -1
+    assert generated_code.find(expected_output) != -1
+
+
+def test_ne_offset_cf_points():
+    ''' Test that we can generate code for a kernel that expects a NE
+    offset and writes to a field on CF points '''
+    ast, invokeInfo = parse(os.path.\
+                            join(os.path.\
+                                 dirname(os.path.\
+                                         abspath(__file__)),
+                                 "test_files", "gocean1p0",
+                                 "test14_ne_offset_cf_updated_one_invoke.f90"), 
+                            api="gocean1.0")
+    psy = PSyFactory("gocean1.0").create(invokeInfo)
+    generated_code = str(psy.gen)
+
+    print generated_code
+    expected_output = """  MODULE psy_single_invoke_test
+    USE field_mod
+    USE kind_params_mod
+    IMPLICIT NONE
+    CONTAINS
+    SUBROUTINE invoke_0_compute_vort(vort_fld, p_fld, u_fld, v_fld)
+      USE kernel_ne_offset_cf_mod, ONLY: compute_vort_code
+      TYPE(r2d_field), intent(inout) :: vort_fld, p_fld, u_fld, v_fld
+      INTEGER j
+      INTEGER i
+      INTEGER istop, jstop
+      !
+      ! Look-up loop bounds
+      istop = vort_fld%grid%simulation_domain%xstop
+      jstop = vort_fld%grid%simulation_domain%ystop
+      !
+      DO j=1,jstop-1
+        DO i=1,istop-1
+          CALL compute_vort_code(i, j, vort_fld%data, p_fld%data, u_fld%data, v_fld%data)
+        END DO 
+      END DO 
+    END SUBROUTINE invoke_0_compute_vort
+  END MODULE psy_single_invoke_test"""
+
+    assert generated_code.find(expected_output) != -1
