88663cce9b8e7e0425e2b7e4d85fc119fbb01225
andrewporter <andrewporter@4bbb03d4-c1cc-11e1-9ca4-71d347b01581>
2015-09-28 08:14:24 +0000

    #272 PSyclone: add test for update of all points with ANY offset.

--- src/tests/gocean1p0_test.py
+++ src/tests/gocean1p0_test.py
@@ -652,3 +652,43 @@ def test_sw_offset_all_cv_points():
   END MODULE psy_single_invoke_test"""
 
     assert generated_code.find(expected_output) != -1
+
+
+def test_offset_any_all_points():
+    ''' Test that we can generate code for a kernel that will operate
+    with any offset and writes to a field on all points '''
+    ast, invokeInfo = parse(os.path.\
+                            join(os.path.\
+                                 dirname(os.path.\
+                                         abspath(__file__)),
+                                 "test_files", "gocean1p0",
+                                 "test24_any_offset_all_update_one_invoke.f90"), 
+                            api="gocean1.0")
+    psy = PSyFactory("gocean1.0").create(invokeInfo)
+    generated_code = str(psy.gen)
+
+    expected_output = """  MODULE psy_single_invoke_test
+    USE field_mod
+    USE kind_params_mod
+    IMPLICIT NONE
+    CONTAINS
+    SUBROUTINE invoke_0_copy(voldfld, vfld)
+      USE kernel_field_copy_mod, ONLY: field_copy_code
+      TYPE(r2d_field), intent(inout) :: voldfld, vfld
+      INTEGER i
+      INTEGER j
+      INTEGER istop, jstop
+      !
+      ! Look-up loop bounds
+      istop = voldfld%grid%simulation_domain%xstop
+      jstop = voldfld%grid%simulation_domain%ystop
+      !
+      DO j=1,jstop+1
+        DO i=1,istop+1
+          CALL field_copy_code(i, j, voldfld%data, vfld%data)
+        END DO 
+      END DO 
+    END SUBROUTINE invoke_0_copy
+  END MODULE psy_single_invoke_test"""
+    print generated_code
+    assert generated_code.find(expected_output) != -1
