8f4dc352e883849b8e7aa723f95735f3ca251a02
andrewporter <andrewporter@4bbb03d4-c1cc-11e1-9ca4-71d347b01581>
2016-08-05 14:42:36 +0000

    #686 PSyclone: record stencil extent and direction variables with name-space manager to prevent clashes with generated PSy variables

--- src/dynamo0p3.py
+++ src/dynamo0p3.py
@@ -758,8 +758,16 @@ class DynInvokeStencil(object):
                     if not arg.stencil.extent:
                         if not arg.stencil.extent_arg.is_literal():
                             if arg.stencil.extent_arg.text not in extent_names:
-                                extent_names.append(
-                                    arg.stencil.extent_arg.text)
+                                extent_names.append(arg.stencil.extent_arg.text)
+                                # Record this name with our name-space manager
+                                # so that it has a record of this variable
+                                # name. This avoids clashes with any
+                                # names we'll subsequently generate in
+                                # the PSy layer.
+                                self._name_space_manager.create_name(
+                                    root_name=arg.stencil.extent_arg.text,
+                                    context="PSyVars",
+                                    label=arg.name+"_stencil_size")
                                 self._unique_extent_args.append(arg)
 
         # a list of arguments that have a direction variable passed in
@@ -781,6 +789,14 @@ class DynInvokeStencil(object):
                            direction_names:
                             direction_names.append(
                                 arg.stencil.direction_arg.text)
+                            # Again, use our name-space manager to
+                            # avoid clashes with any variable names
+                            # we'll subsequently generate in the PSy
+                            # layer
+                            self._name_space_manager.create_name(
+                                root_name=arg.stencil.direction_arg.text,
+                                context="PSyVars",
+                                label=arg.name+"_stencil_direction")
                             self._unique_direction_args.append(arg)
 
         # list of stencil args with an extent variable passed in. The same
