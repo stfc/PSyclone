bdb31c56814fd8f99077cb5bc3afdcf34ad5f038
Andrew Porter <andrew.porter@stfc.ac.uk>
2017-05-11 10:02:47 +0100

    #21 Update copyright notice in test kernels

--- src/tests/test_files/dynamo0p3/columnwise_op_app_kernel_mod.F90
+++ src/tests/test_files/dynamo0p3/columnwise_op_app_kernel_mod.F90
@@ -1,39 +1,10 @@
-! -----------------------------------------------------------------------------
-! BSD 3-Clause License
-!
-! Copyright (c) 2017, Science and Technology Facilities Council
-! However, it has been created with the help of the GungHo Consortium,
+! Modifications copyright (c) 2017, Science and Technology Facilities Council
+!-------------------------------------------------------------------------------
+! (c) The copyright relating to this work is owned jointly by the Crown, 
+! Met Office and NERC 2014. 
+! However, it has been created with the help of the GungHo Consortium, 
 ! whose members are identified at https://puma.nerc.ac.uk/trac/GungHo/wiki
-! All rights reserved.
-!
-! Redistribution and use in source and binary forms, with or without
-! modification, are permitted provided that the following conditions are met:
-!
-! * Redistributions of source code must retain the above copyright notice, this
-!   list of conditions and the following disclaimer.
-!
-! * Redistributions in binary form must reproduce the above copyright notice,
-!   this list of conditions and the following disclaimer in the documentation
-!   and/or other materials provided with the distribution.
-!
-! * Neither the name of the copyright holder nor the names of its
-!   contributors may be used to endorse or promote products derived from
-!   this software without specific prior written permission.
-!
-! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-! LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-! FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-! COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-! INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-! BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-! LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-! LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-! POSSIBILITY OF SUCH DAMAGE.
-! -----------------------------------------------------------------------------
-! Author R. Ford and A. R. Porter, STFC Daresbury Lab
+!-------------------------------------------------------------------------------
 
 !> @brief Kernel which (incrementally) applies a columnwise assembled operator to a field
 
@@ -126,6 +97,20 @@ contains
                                            indirection_dofmap_to, &
                                            indirection_dofmap_from)
     implicit none
+    ! Arguments
+    integer(kind=i_def), intent(in) :: cell,  ncell_2d
+    integer(kind=i_def), intent(in) :: nrow, ncol, bandwidth
+    integer(kind=i_def), intent(in) :: undf1, ndf1
+    integer(kind=i_def), intent(in) :: undf2, ndf2
+    real(kind=r_def), dimension(undf1), intent(inout) :: lhs
+    real(kind=r_def), dimension(undf2), intent(in) :: x
+    real(kind=r_def), dimension(bandwidth,nrow,ncell_2d), intent(in) :: columnwise_matrix
+    integer(kind=i_def), dimension(ndf1), intent(in) :: map1
+    integer(kind=i_def), dimension(ndf2), intent(in) :: map2
+
+    integer(kind=i_def), intent(in) :: alpha, beta, gamma_m, gamma_p
+    integer(kind=i_def), dimension(nrow), intent(in) :: indirection_dofmap_to
+    integer(kind=i_def), dimension(ncol), intent(in) :: indirection_dofmap_from
 
   end subroutine columnwise_op_app_kernel_code
 
