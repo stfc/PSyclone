313cb5636a7c0590638225c0f1b76308a12f1613
Andrew Porter <andrew.porter@stfc.ac.uk>
2017-05-12 16:51:16 +0100

    #32 Change stored list of infrastructure modules to be just names, not file-names

--- src/tests/dynamo0p3_build.py
+++ src/tests/dynamo0p3_build.py
@@ -10,12 +10,12 @@ BASE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),
 # PSy code.  These must be listed in the order in which they must
 # be compiled.
 INFRASTRUCTURE_PATH = os.path.join(BASE_PATH, "infrastructure")
-INFRASTRUCTURE_MODULES = ["constants_mod.f90",
-                          "linked_list_data_mod.f90",
-                          "partition_mod.f90",
-                          "mesh_mod.f90",
-                          "stencil_dofmap_mod.f90",
-                          "function_space_mod.f90",
-                          "field_mod.f90",
-                          "quadrature_mod.f90",
-                          "operator_mod.f90"]
+INFRASTRUCTURE_MODULES = ["constants_mod",
+                          "linked_list_data_mod",
+                          "partition_mod",
+                          "mesh_mod",
+                          "stencil_dofmap_mod",
+                          "function_space_mod",
+                          "field_mod",
+                          "quadrature_mod",
+                          "operator_mod"]
