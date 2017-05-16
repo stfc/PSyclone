c31b1d82c4a779bae1daf14a25319ec04c2e123f
Andrew Porter <andrew.porter@stfc.ac.uk>
2017-05-16 09:09:40 +0100

    #32 Make testkern valid Fortran

--- src/tests/test_files/dynamo0p3/testkern.F90
+++ src/tests/test_files/dynamo0p3/testkern.F90
@@ -7,6 +7,9 @@
 ! Author R. Ford STFC Daresbury Lab
 
 module testkern
+  use argument_mod
+  use kernel_mod
+  use constants_mod
   type, extends(kernel_type) :: testkern_type
      type(arg_type), dimension(5) :: meta_args =    &
           (/ arg_type(gh_real, gh_read),     &
@@ -15,12 +18,20 @@ module testkern
              arg_type(gh_field,gh_read, w2), &
              arg_type(gh_field,gh_read, w3)  &
            /)
-     integer, parameter :: iterates_over = cells
+     integer :: iterates_over = cells
    contains
-     procedure() :: code => testkern_code
+     procedure, nopass :: code => testkern_code
   end type testkern_type
 contains
 
-  subroutine testkern_code()
+  subroutine testkern_code(nlayers, ascalar, fld1, fld2, fld3, fld4, &
+                           ndf_w1, undf_w1, map_w1, ndf_w2, undf_w2, map_w2, &
+                           ndf_w3, undf_w3, map_w3)
+    integer :: nlayers
+    real(kind=r_def) :: ascalar
+    real(kind=r_def), dimension(:) :: fld1, fld2, fld3, fld4
+    integer :: ndf_w1, undf_w1, ndf_w2, undf_w2, ndf_w3, undf_w3
+    integer, dimension(:) :: map_w1, map_w2, map_w3
+
   end subroutine testkern_code
 end module testkern
