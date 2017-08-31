6e745f8903a3fedc82de36cafee267d80addb6dd
TeranIvy <TeranIvy@users.noreply.github.com>
2017-08-31 10:20:41 +0100

    Issue #61, PR #71: Corrected BSD license in test files.

--- src/psyclone/tests/test_files/dynamo0p3/15.3.3_inc_aX_times_Y_builtin.f90
+++ src/psyclone/tests/test_files/dynamo0p3/15.3.3_inc_aX_times_Y_builtin.f90
@@ -2,8 +2,6 @@
 ! BSD 3-Clause License
 !
 ! Copyright (c) 2017, Science and Technology Facilities Council
-! However, it has been created with the help of the GungHo Consortium,
-! whose members are identified at https://puma.nerc.ac.uk/trac/GungHo/wiki
 ! All rights reserved.
 !
 ! Redistribution and use in source and binary forms, with or without
@@ -33,7 +31,7 @@
 ! ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 ! POSSIBILITY OF SUCH DAMAGE.
 ! -----------------------------------------------------------------------------
-! Modified I. Kavcic Met Office
+! Author I. Kavcic Met Office
 
 program single_invoke
 
