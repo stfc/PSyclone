db98c9e6ef319a077f4e72f743615eb1e7e3145e
Ubuntu 64-bit 16.04 <rupert.ford@stfc.ac.uk>
2017-10-02 15:00:14 +0100

    issue #52 pr #60. Changed the copyright notice appropriately.

--- src/psyclone/tests/dynamo0p3_basis_test.py
+++ src/psyclone/tests/dynamo0p3_basis_test.py
@@ -2,10 +2,6 @@
 # BSD 3-Clause License
 #
 # Copyright (c) 2017, Science and Technology Facilities Council
-# (c) The copyright relating to this work is owned jointly by the Crown,
-# Met Office and NERC 2016.
-# However, it has been created with the help of the GungHo Consortium,
-# whose members are identified at https://puma.nerc.ac.uk/trac/GungHo/wiki
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -35,7 +31,7 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 # -----------------------------------------------------------------------------
-# Author R. Ford and A. R. Porter, STFC Daresbury Lab
+# Author R. W. Ford and A. R. Porter, STFC Daresbury Lab
 
 ''' Module containing py.test tests for functionality related to quadrature
 and evaluators in the LFRic API '''
