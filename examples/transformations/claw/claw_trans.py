be40218705fa85a158b886fc93d0289c29f47655
Andrew Porter <andrew.porter@stfc.ac.uk>
2017-12-08 17:29:20 +0000

    #114 Add skeleton CLAW example

---
+++ examples/transformations/claw/claw_trans.py
@@ -0,0 +1,57 @@
+# -----------------------------------------------------------------------------
+# BSD 3-Clause License
+#
+# Copyright (c) 2017, Science and Technology Facilities Council
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# -----------------------------------------------------------------------------
+# Author: A. R. Porter, STFC Daresbury Lab
+
+
+def trans(psy, kernel_search_path, out_path):
+    pass
+
+def claw_trans():
+    pass
+
+if __name__ == "__main__":
+    import os
+    from psyclone.parse import parse
+    from psyclone.psyGen import PSyFactory
+    src_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                            "..", "..", "..", "src", "psyclone", "tests",
+                            "test_files", "gocean1p0")
+    _, info = parse(os.path.join(src_path, "single_invoke.f90"),
+                    api="gocean1.0")
+    psy = PSyFactory("gocean1.0").create(info)
+
+    trans(psy, src_path, os.path.abspath(__file__))
+
+    print "All done!"
