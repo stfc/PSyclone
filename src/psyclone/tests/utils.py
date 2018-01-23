92561169babdd1ac1f48e659a2dbab5f8d987543
hiker <henrichsjoerg@gmail.com>
2018-01-23 13:53:08 +1100

    Fixed 'pytest has no config member' pylint message (which is a false positive).

--- src/psyclone/tests/utils.py
+++ src/psyclone/tests/utils.py
@@ -44,7 +44,9 @@ FORTRAN_SUFFIXES = ["f90", "F90", "x90"]
 
 # Whether or not we run tests requiring code compilation is picked-up
 # from a command-line flag. (This is set-up in conftest.py.)
+# pylint: disable=no-member
 TEST_COMPILE = pytest.config.getoption("--compile")
+# pylint: enable=no-member
 # The following allows us to mark a test with @utils.COMPILE if it is
 # only to be run when the --compile option is passed to py.test
 COMPILE = pytest.mark.skipif(not TEST_COMPILE,
