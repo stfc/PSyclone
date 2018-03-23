289f64e79622afadef9aaaa6cf2024f29996ee65
Andrew Porter <andrew.porter@stfc.ac.uk>
2018-03-23 14:02:32 +0000

    #143 rm unnecessary output from tests and correct copyright

--- src/psyclone/tests/gocean1p0_test.py
+++ src/psyclone/tests/gocean1p0_test.py
@@ -1,8 +1,35 @@
-# ----------------------------------------------------------------------------
-# (c) The copyright relating to this work is owned jointly by the Crown,
-# Met Office and NERC 2015.
-# However, it has been created with the help of the GungHo Consortium,
-# whose members are identified at https://puma.nerc.ac.uk/trac/GungHo/wiki
+# -----------------------------------------------------------------------------
+# BSD 3-Clause License
+#
+# Copyright (c) 2017-2018, Science and Technology Facilities Council
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 # ----------------------------------------------------------------------------
 # Author A. R. Porter, STFC Daresbury Lab
 # Modified work Copyright (c) 2018 by J. Henrichs, Bureau of Meteorology
@@ -846,9 +873,7 @@ def test_goschedule_str():
         "EndLoop\n"
         "EndLoop\n"
         "End Schedule\n")
-
     sched_str = str(schedule)
-    print sched_str
     assert sched_str in expected_sched
 
     # Switch-off constant loop bounds
@@ -868,8 +893,6 @@ def test_goschedule_str():
         "EndLoop\n"
         "EndLoop\n"
         "End Schedule\n")
-
-    print sched_str
     assert sched_str in expected_sched
 
 
@@ -978,7 +1001,6 @@ def test_writetoread_dag(tmpdir, have_graphviz):
         assert os.path.isfile(dot_file)
         with open(dot_file, "r") as dfile:
             dot = dfile.read()
-        print dot
         assert dot.startswith("digraph")
         # write -> read means that the second loop can only begin once the
         # first loop is complete. Check that we have the correct forwards
