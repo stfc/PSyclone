028211aaa863b688830215777eef517e65fd5cc4
Andrew Porter <andrew.porter@stfc.ac.uk>
2018-09-03 20:57:30 +0100

    #209 correct copyright date in test files [skip ci]

--- src/psyclone/tests/nemo0p1/test_files/explicit_do.f90
+++ src/psyclone/tests/nemo0p1/test_files/explicit_do.f90
@@ -1,7 +1,7 @@
 ! -----------------------------------------------------------------------------
 ! BSD 3-Clause License
 !
-! Copyright (c) 2017, Science and Technology Facilities Council
+! Copyright (c) 2017-2018, Science and Technology Facilities Council.
 ! All rights reserved.
 !
 ! Redistribution and use in source and binary forms, with or without
