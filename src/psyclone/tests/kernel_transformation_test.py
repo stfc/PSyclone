a10b19fb8fab1165180a5040d878133b51cf6415
hiker <henrichsjoerg@gmail.com>
2018-10-10 17:19:38 +1100

    Revert "Fixed tests due to changes in fparser - no idea why it is only showing"
    This reverts commit 4ef5f9f2104d37121638c67c456e067d51abe75d.
    
    The problem seems to have been caused by me having stale/old pyc files
    in fparser.

--- src/psyclone/tests/kernel_transformation_test.py
+++ src/psyclone/tests/kernel_transformation_test.py
@@ -36,6 +36,7 @@
 ''' Module containing tests for kernel transformations. '''
 
 from __future__ import absolute_import
+import os
 import pytest
 from psyclone_test_utils import get_invoke
 from psyclone.transformations import TransformationError
@@ -78,7 +79,7 @@ def test_accroutine():
     directive to it. '''
     from psyclone.gocean1p0 import GOKern
     from psyclone.transformations import ACCRoutineTrans
-    from fparser.two import Fortran2003, utils
+    from fparser.two import Fortran2003
     _, invoke = get_invoke("nemolite2d_alg_mod.f90", api="gocean1.0", idx=0)
     sched = invoke.schedule
     kern = sched.children[0].children[0].children[0]
@@ -91,8 +92,8 @@ def test_accroutine():
     assert new_kern._fp2_ast
     assert isinstance(new_kern._fp2_ast, Fortran2003.Program)
     # Check AST contains directive
-    comments = utils.walk_ast(new_kern._fp2_ast.content,
-                              [Fortran2003.Comment])
+    comments = Fortran2003.walk_ast(new_kern._fp2_ast.content,
+                                    [Fortran2003.Comment])
     assert len(comments) == 1
     assert str(comments[0]) == "!$acc routine"
     # Check that directive is in correct place (end of declarations)
