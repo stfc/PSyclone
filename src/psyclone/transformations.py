36a80f4f9364f9951a941ef1b5f191f5a9dbc1ab
Andrew Porter <andrew.porter@stfc.ac.uk>
2018-10-23 09:30:57 +0100

    #210 fix imports of walk_ast

--- src/psyclone/transformations.py
+++ src/psyclone/transformations.py
@@ -2348,9 +2348,10 @@ class ACCRoutineTrans(Transformation):
         :raises TransformationError: if we fail to find the subroutine \
                                      corresponding to the kernel object.
         '''
-        from fparser.two.Fortran2003 import walk_ast, Subroutine_Subprogram, \
+        from fparser.two.Fortran2003 import Subroutine_Subprogram, \
             Subroutine_Stmt, Specification_Part, Type_Declaration_Stmt, \
             Implicit_Part, Comment
+        from fparser.two.utils import walk_ast
         from fparser.common.readfortran import FortranStringReader
         # Get the fparser2 AST of the kernel
         ast = kern.ast
