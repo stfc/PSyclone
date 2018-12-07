1cc0a50e34ea617ceb352d1c96c1e2444ce2b200
Andrew Porter <andrew.porter@stfc.ac.uk>
2018-12-07 14:51:21 +0000

    #244 add exp and sqrt keywords [skip ci]

--- src/psyclone/tests/nemo/transformations/openacc/data_directive_test.py
+++ src/psyclone/tests/nemo/transformations/openacc/data_directive_test.py
@@ -261,3 +261,17 @@ def test_replicated_loop():
             "    END DO\n"
             "  END DO\n"
             "  !$ACC END DATA") in gen_code
+
+
+def test_kind_parameter():
+    ''' Check that we don't attempt to put kind parameters into the list
+    of variables to copyin/out. '''
+    from fparser.two.parser import ParserFactory
+    from fparser.common.readfortran import FortranStringReader
+    parser = ParserFactory.create()
+    reader = FortranStringReader("program kind_param\n"
+                                 "sto_tmp(:, :) = 0._wp\n"
+                                 "end program kind_param\n")
+    code = parser(reader)
+    psy = PSyFactory(API, distributed_memory=False).create(code)
+    
