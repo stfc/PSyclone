642e891a780fdb720950fb4176792e3f773becae
Andrew Porter <andrew.porter@stfc.ac.uk>
2019-01-28 12:40:51 +0000

    Revert "#126 fix x-failing tests by correcting kernel argument lists in test Fortran" after accidentally committing straight to master branch.
    This reverts commit 42f130b972ee15e4ab094870f55cbac1e45ab8a9.

--- src/psyclone/tests/test_files/dynamo0p3/7_QR_field_array.f90
+++ src/psyclone/tests/test_files/dynamo0p3/7_QR_field_array.f90
@@ -1,49 +1,17 @@
-! -----------------------------------------------------------------------------
-! BSD 3-Clause License
-!
-! Copyright (c) 2017-2019, Science and Technology Facilities Council.
-! All rights reserved.
-!
-! Redistribution and use in source and binary forms, with or without
-! modification, are permitted provided that the following conditions are met:
-!
-! * Redistributions of source code must retain the above copyright notice, this
-!   list of conditions and the following disclaimer.
-!
-! * Redistributions in binary form must reproduce the above copyright notice,
-!   this list of conditions and the following disclaimer in the documentation
-!   and/or other materials provided with the distribution.
-!
-! * Neither the name of the copyright holder nor the names of its
-!   contributors may be used to endorse or promote products derived from
-!   this software without specific prior written permission.
-!
-! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-! AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-! IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-! DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-! FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-! DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-! SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-! OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-! OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-! -----------------------------------------------------------------------------
-! Authors: R. W. Ford and A. R. Porter, STFC Daresbury Lab
-
 program qr_field_array
 
   use testkern_qr, only: testkern_qr_type
   implicit none
   type(field_type)      :: f0, f1, f2, f3, f4
   type(quadrature_type) :: qr0(2,2), qr1(2,2)
-  real    :: ascal
   integer :: i, j, k(2), l
 
-  call invoke(                                            &
-       testkern_qr_type(f1,f2,f3,ascal,f4,l,qr0(i,j)),    &
-       testkern_qr_type(f1,f2,f3,ascal,f4,l,qr0(i,j+1)),  &
-       testkern_qr_type(f1,f2,f3,ascal,f4,l,qr1(i,k(l))), &
-       testkern_qr_type(f0,f2,f3,ascal,f4,l,qr0(i,j)) )
+  call invoke(                                    &
+       testkern_qr_type(f1,f2,f3,f4,qr0(i,j)),    &
+       testkern_qr_type(f1,f2,f3,f4,qr0(i,j+1)),  &
+       testkern_qr_type(f1,f2,f3,f4,qr1(i,k(l))), &
+       testkern_qr_type(f0,f2,f3,f4,qr0(i,j))     &
+       )
+
 
 end program qr_field_array
