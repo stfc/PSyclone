ac74eb3fef97693415525c51bcc8ad624f44db1c
Andrew Porter <andrew.porter@stfc.ac.uk>
2019-02-04 14:14:11 +0000

    #289 rm most top-level files from pages branch

--- examples/line_length/longlines.f90
+++
@@ -1,6 +0,0 @@
-program long_lines
-  use testkern_qr, only : testkern_qr_type
-  real(r_def) :: rdt
-  integer(i_def) :: iflag
-  call invoke(testkern_qr_type(f1, f2, f3, rdt, f4, iflag, qr))
-end program long_lines
