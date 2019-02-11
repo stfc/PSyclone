d87e5d3fb96c2f30fcbabf55c5c3a802c4d6c9a1
TeranIvy <TeranIvy@users.noreply.github.com>
2019-02-11 22:25:12 +0000

    I #139: Updated Dynamo examples

--- examples/dynamo/eg12/transpose_matrix_vector_kernel_mod.F90
+++ examples/dynamo/eg12/transpose_matrix_vector_kernel_mod.F90
@@ -38,7 +38,7 @@
 ! -----------------------------------------------------------------------------
 ! Modified by I. Kavcic, Met Office
 !
-module matrix_vector_kernel_mod
+module transpose_matrix_vector_kernel_mod
 use argument_mod,            only : arg_type,                               &
                                     GH_FIELD, GH_OPERATOR, GH_READ, GH_INC, &
                                     ANY_SPACE_1, ANY_SPACE_2,               &
@@ -52,16 +52,16 @@ implicit none
 ! Public types
 !-------------------------------------------------------------------------------
 
-type, public, extends(kernel_type) :: matrix_vector_kernel_type
+type, public, extends(kernel_type) :: transpose_matrix_vector_kernel_type
   private
   type(arg_type) :: meta_args(3) = (/                                  &
        arg_type(GH_FIELD,    GH_INC,  ANY_SPACE_1),                    &  
        arg_type(GH_FIELD,    GH_READ, ANY_SPACE_2),                    &
-       arg_type(GH_OPERATOR, GH_READ, ANY_SPACE_1, ANY_SPACE_2)        &
+       arg_type(GH_OPERATOR, GH_READ, ANY_SPACE_2, ANY_SPACE_1)        &
        /)
   integer :: iterates_over = CELLS
 contains
-  procedure, nopass ::matrix_vector_code
+  procedure, nopass ::transpose_matrix_vector_code
 end type
 
 !-------------------------------------------------------------------------------
@@ -69,71 +69,71 @@ end type
 !-------------------------------------------------------------------------------
 
 ! Overload the default structure constructor for function space
-interface matrix_vector_kernel_type
-   module procedure matrix_vector_kernel_constructor
+interface transpose_matrix_vector_kernel_type
+   module procedure transpose_matrix_vector_kernel_constructor
 end interface
 
 !-------------------------------------------------------------------------------
 ! Contained functions/subroutines
 !-------------------------------------------------------------------------------
-public matrix_vector_code
+public transpose_matrix_vector_code
 
 contains
 
-type(matrix_vector_kernel_type) function matrix_vector_kernel_constructor() result(self)
+type(transpose_matrix_vector_kernel_type) function transpose_matrix_vector_kernel_constructor() result(self)
   implicit none
   return
-end function matrix_vector_kernel_constructor
+end function transpose_matrix_vector_kernel_constructor
 
-!> @brief Computes lhs = matrix*x
+!> @brief Computes lhs = matrix^T*x where matrix^T is the transpose of the matrix
 !! @param[in] cell Horizontal cell index
 !! @param[in] nlayers Number of layers
-!! @param[inout] lhs Output lhs (A*x)
+!! @param[inout] lhs Output lhs (A^T*x)
 !! @param[in] x Input data
 !! @param[in] ncell_3d Total number of cells
-!! @param[in] matrix Local matrix assembly form of the operator A 
+!! @param[in] matrix Matrix values
 !! @param[in] ndf1 Number of degrees of freedom per cell for the output field
 !! @param[in] undf1 Unique number of degrees of freedom  for the output field
 !! @param[in] map1 Dofmap for the cell at the base of the column for the output field
 !! @param[in] ndf2 Number of degrees of freedom per cell for the input field
 !! @param[in] undf2 Unique number of degrees of freedom for the input field 
 !! @param[in] map2 Dofmap for the cell at the base of the column for the input field
-subroutine matrix_vector_code(cell,              &
-                              nlayers,           &
-                              lhs, x,            & 
-                              ncell_3d,          &
-                              matrix,            &
-                              ndf1, undf1, map1, &
-                              ndf2, undf2, map2)
+subroutine transpose_matrix_vector_code(cell,              &
+                                        nlayers,           &
+                                        lhs, x,            & 
+                                        ncell_3d,          &
+                                        matrix,            &
+                                        ndf1, undf1, map1, &
+                                        ndf2, undf2, map2)
  
   implicit none
 
   ! Arguments
-  integer(kind=i_def),                  intent(in) :: cell, nlayers, ncell_3d
-  integer(kind=i_def),                  intent(in) :: undf1, ndf1
-  integer(kind=i_def),                  intent(in) :: undf2, ndf2
-  integer(kind=i_def), dimension(ndf1), intent(in) :: map1
-  integer(kind=i_def), dimension(ndf2), intent(in) :: map2
-  real(kind=r_def), dimension(undf2),              intent(in)    :: x
-  real(kind=r_def), dimension(undf1),              intent(inout) :: lhs
-  real(kind=r_def), dimension(ndf1,ndf2,ncell_3d), intent(in)    :: matrix
+  integer(kind=i_def),                  intent(in)    :: cell, nlayers, ncell_3d
+  integer(kind=i_def),                  intent(in)    :: undf1, ndf1
+  integer(kind=i_def),                  intent(in)    :: undf2, ndf2
+  integer(kind=i_def), dimension(ndf1), intent(in)    :: map1
+  integer(kind=i_def), dimension(ndf2), intent(in)    :: map2
+  real(kind=r_def),    dimension(undf2),              intent(in)    :: x
+  real(kind=r_def),    dimension(undf1),              intent(inout) :: lhs
+  real(kind=r_def),    dimension(ndf2,ndf1,ncell_3d), intent(in)    :: matrix
 
   ! Internal variables
   integer(kind=i_def)               :: df, k, ik 
   real(kind=r_def), dimension(ndf2) :: x_e
   real(kind=r_def), dimension(ndf1) :: lhs_e
- 
+  
   do k = 0, nlayers-1
     do df = 1, ndf2  
       x_e(df) = x(map2(df)+k)
     end do
     ik = (cell-1)*nlayers + k + 1
-    lhs_e = matmul(matrix(:,:,ik),x_e)
+    lhs_e = matmul(transpose(matrix(:,:,ik)),x_e)
     do df = 1,ndf1
        lhs(map1(df)+k) = lhs(map1(df)+k) + lhs_e(df) 
     end do
   end do
  
-end subroutine matrix_vector_code
+end subroutine transpose_matrix_vector_code
 
-end module matrix_vector_kernel_mod
+end module transpose_matrix_vector_kernel_mod
