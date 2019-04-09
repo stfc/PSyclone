ed765b5aaa706ae4c83a5848092f4954c010ea4b
Andrew Porter <andrew.porter@stfc.ac.uk>
2019-04-09 16:37:38 +0100

    #289 add a landing page [skip ci]

---
+++ src/psyclone/doxy_main_page.py
@@ -0,0 +1,12 @@
+## @mainpage The PSyclone Domain-Specific Compiler
+#
+# @section intro_sec Introduction
+#
+# This is the internal API documentation for the PSyclone
+# Domain-Specific Compiler. For the user guide please see
+# [ReadTheDocs](https://psyclone.readthedocs.io/en/stable/).
+#
+# PSyclone itself is hosted on [GitHub](https://github.com/stfc/PSyclone)
+# and uses the [fparser](https://github.com/stfc/fparser) Fortran parser.
+
+
