6108aca9b6ff30bfac0505d1a07d27a23cd475c8
Sergi Siso <sergiesg@gmail.com>
2019-05-09 14:22:35 +0100

    #379 Fortran front-end does not allow undefined references into the AST

--- src/psyclone/psyGen.py
+++ src/psyclone/psyGen.py
@@ -5811,6 +5811,16 @@ class Fparser2ASTProcessor(object):
         :returns: PSyIR representation of node
         :rtype: :py:class:`psyclone.psyGen.Reference`
         '''
+        symbol_table = parent.root.symbol_table
+        try:
+            symbol_table.lookup(node.string)
+        except KeyError:
+            # TODO: Fix for nemolite2D, remove before merging to master
+            if node.string in ('visc', 'omega', 'd2r', 'g', 'rdt', 'cbfr',
+                               'wp'):
+                return Reference(node.string, parent)
+            raise GenerationError("Undeclared Reference: " + str(node.string))
+
         return Reference(node.string, parent)
 
     def _parenthesis_handler(self, node, parent):
@@ -5844,7 +5854,41 @@ class Fparser2ASTProcessor(object):
         '''
         from fparser.two import Fortran2003
 
-        reference_name = node.items[0].string
+        symbol_table = parent.root.symbol_table
+        reference_name = node.items[0].string.lower()
+
+        try:
+            symbol_table.lookup(reference_name)
+        except KeyError:
+            # Intrinsics are wrongly parsed as arrays by fparser2, we can
+            # fix the issue here and convert them to appropiate PSyIR nodes.
+            if reference_name == 'sign':
+                bop = BinaryOperation(BinaryOperator.SIGN, parent)
+                self.process_nodes(parent=bop, nodes=[node.items[1].items[0]],
+                                   nodes_parent=node)
+                self.process_nodes(parent=bop, nodes=[node.items[1].items[1]],
+                                   nodes_parent=node)
+                return bop
+            elif reference_name == 'sin':
+                uop = UnaryOperation(UnaryOperator.SIN, parent)
+                self.process_nodes(parent=uop, nodes=[node.items[1].items[0]],
+                                   nodes_parent=node)
+                return uop
+            elif reference_name == 'real':
+                uop = UnaryOperation(UnaryOperator.REAL, parent)
+                self.process_nodes(parent=uop, nodes=[node.items[1].items[0]],
+                                   nodes_parent=node)
+                return uop
+            elif reference_name == 'sqrt':
+                uop = UnaryOperation(UnaryOperator.SQRT, parent)
+                self.process_nodes(parent=uop, nodes=[node.items[1].items[0]],
+                                   nodes_parent=node)
+                return uop
+            else:
+                raise GenerationError("Undeclared Reference: " +
+                        str(reference_name))
+
+
         array = Array(reference_name, parent)
 
         if isinstance(node.items[1], Fortran2003.Section_Subscript_List):
@@ -6525,8 +6569,11 @@ UnaryOperator = Enum('UnaryOperator', [
     # Trigonometric Operators
     'COS', 'SIN', 'TAN', 'ACOS', 'ASIN', 'ATAN',
     # Other Maths Operators
-    'ABS'
+    'ABS',
+    # Casting Opertors
+    'REAL'
     ])
+
 
 BinaryOperator = Enum('BinaryOperator', [
     # Arithmetic Operators
