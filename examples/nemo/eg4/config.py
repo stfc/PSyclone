45ab7e3e4644bd371710eff62eb8fabf3541990e
Rupert Ford <rupert.ford@stfc.ac.uk>
2019-09-09 21:49:13 +0100

    pr #466. Added files required to run Dawn script.

---
+++ examples/nemo/eg4/config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+##===-----------------------------------------------------------------------------*- Python -*-===##
+##                          _                      
+##                         | |                     
+##                       __| | __ ___      ___ ___  
+##                      / _` |/ _` \ \ /\ / / '_  | 
+##                     | (_| | (_| |\ V  V /| | | |
+##                      \__,_|\__,_| \_/\_/ |_| |_| - Compiler Toolchain
+##
+##
+##  This file is distributed under the MIT License (MIT). 
+##  See LICENSE.txt for details.
+##
+##===------------------------------------------------------------------------------------------===##
+
+__dawn_versioninfo__ = (0, 0, 1)
+__dawn_install_module__ = '/home/rupert/proj/dawn/bundle/build/dawn-prefix/src/dawn-build/python/'
+__dawn_install_dawnclib__ = '/home/rupert/proj/dawn/bundle/install/lib/libDawnC.so'
