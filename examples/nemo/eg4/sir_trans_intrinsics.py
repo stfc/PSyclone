b5ca6187912a50fc30d77aaf79122cba5adfe7fe
Rupert Ford <rupert.ford@stfc.ac.uk>
2019-10-17 17:27:49 +0100

    pr #541. Working towards sign transformation.

--- examples/nemo/eg4/sir_trans_intrinsics.py
+++ examples/nemo/eg4/sir_trans_intrinsics.py
@@ -48,6 +48,125 @@ from psyclone.nemo import NemoKern
 from psyclone.psyGen import UnaryOperation, BinaryOperation, NaryOperation, Assignment, Reference, Literal, IfBlock, Schedule
 import copy
 
+
+def replace_abs(oper, key):
+    ''' xxx '''
+    # R=ABS(X) => IF (X<0.0) R=X*-1.0 ELSE R=X 
+    # TODO: There is an assumption that operation is child of assignment
+    oper_parent = oper.parent
+    assignment = oper.ancestor(Assignment)
+
+    # Replace operation with a temporary (res_X).
+    oper_parent.children[oper.position] = Reference("res_{0}".format(key), parent=oper_parent)
+
+    # Assign content of operation to a temporary (tmp_X)
+    new_assignment = Assignment(parent=assignment.parent)
+    lhs = Reference("tmp_{0}".format(key), parent=new_assignment)
+    rhs = oper.children[0]
+    rhs.parent = new_assignment
+    new_assignment.children = [lhs, rhs]
+    assignment.parent.children.insert(assignment.position, new_assignment)
+
+    # Set res_X to the absolute value of tmp_X
+    if_stmt = IfBlock(parent=assignment.parent)
+    if_condition = BinaryOperation(BinaryOperation.Operator.GT, parent=if_stmt)
+    lhs = Reference("tmp_{0}".format(key), parent=if_condition)
+    rhs = Literal("0.0", parent=if_condition)
+    if_condition.children = [lhs, rhs]
+    then_schedule = Schedule(parent=if_stmt)
+    then_body = Assignment(parent=then_schedule)
+    then_body_lhs = Reference("res_{0}".format(key), parent=then_body)
+    then_body_rhs = Reference("tmp_{0}".format(key), parent=then_body)
+    then_body.children = [then_body_lhs, then_body_rhs]
+    then_schedule.children = [then_body]                    
+    else_schedule = Schedule(parent=if_stmt)
+    else_body = Assignment(parent=else_schedule)
+    else_body_lhs = Reference("res_{0}".format(key), parent=else_body)
+    else_body_rhs = BinaryOperation(BinaryOperation.Operator.MUL, parent=else_body)
+    lhs_child = Reference("tmp_{0}".format(key), parent=else_body_rhs)
+    rhs_child = Literal("-1.0", parent=else_body_rhs)
+    else_body_rhs.children = [lhs_child, rhs_child]
+    else_body.children = [else_body_lhs, else_body_rhs]
+    else_schedule.children = [else_body]
+    if_stmt.children = [if_condition, then_schedule, else_schedule]
+    assignment.parent.children.insert(assignment.position, if_stmt)
+
+    #new_assignment_mult = Assignment(parent=assignment.parent)
+    #lhs = Reference("tmp1_{0}".format(key), parent=new_assignment_mult)
+    #rhs = BinaryOperation(BinaryOperation.Operator.MUL, parent=new_assignment_mult)
+    #new_assignment_mult.children = [lhs, rhs]
+    #lhs_child = copy.copy(oper.children[0])
+    #rhs_child = Literal("-1.0", parent=rhs)
+    #rhs.children = [lhs_child, rhs_child]
+    #assignment.parent.children.insert(assignment.position, new_assignment_mult)
+
+    # This is essentially a max so reuse
+    #if_stmt = IfBlock(parent=assignment.parent)
+    #if_condition = BinaryOperation(BinaryOperation.Operator.GT, parent=if_stmt)
+    #lhs = Reference("tmp0_{0}".format(key), parent=if_condition)
+    #rhs = Reference("tmp1_{0}".format(key), parent=if_condition)
+    #if_condition.children = [lhs, rhs]
+    #then_schedule = Schedule(parent=if_stmt)
+    #then_body = Assignment(parent=then_schedule)
+    #then_body_lhs = Reference("tmp2_{0}".format(key), parent=then_body)
+    #then_body_rhs = Reference("tmp0_{0}".format(key), parent=then_body)
+    #then_body.children = [then_body_lhs, then_body_rhs]
+    #then_schedule.children = [then_body]                    
+    #else_schedule = Schedule(parent=if_stmt)
+    #else_body = Assignment(parent=else_schedule)
+    #else_body_lhs = Reference("tmp2_{0}".format(key), parent=else_body)
+    #else_body_rhs = Reference("tmp1_{0}".format(key), parent=else_body)
+    #else_body.children = [else_body_lhs, else_body_rhs]
+    #else_schedule.children = [else_body]
+    #if_stmt.children = [if_condition, then_schedule, else_schedule]
+    #assignment.parent.children.insert(assignment.position, if_stmt)
+
+def replace_sign(oper, key):
+    ''' xxx '''
+    # R=SIGN(A,B) if A<0 then (if B<0 R=B else R=B*-1) else ((if B>0 R=B else R=B*-1))
+    # [USE THIS ONE] R=SIGN(A,B) => R=ABS(B); if A<0.0 R=R*-1.0
+    oper_parent = oper.parent
+    assignment = oper.ancestor(Assignment)
+
+    res_var = "XXXRES_{0}".format(key)
+    tmp_var = "XXXTMP_{0}".format(key)
+
+    # Replace operation with a temporary (res_X).
+    oper_parent.children[oper.position] = Reference(res_var, parent=oper_parent)
+
+    # Set the result to the ABS value of the second argument of SIGN
+    new_assignment = Assignment(parent=assignment.parent)
+    lhs = Reference(res_var, parent=new_assignment)
+    rhs = UnaryOperation(UnaryOperation.Operator.ABS, parent=new_assignment)
+    rhs_child = oper.children[1]
+    rhs_child.parent = rhs
+    rhs.children = [rhs_child]
+    new_assignment.children = [lhs, rhs]
+    assignment.parent.children.insert(assignment.position, new_assignment)
+
+    # Assign the 1st argument to a temporary in case it is a complex expression.
+    new_assignment = Assignment(parent=assignment.parent)
+    lhs = Reference(tmp_var, parent=new_assignment)
+    rhs = oper.children[0]
+    rhs.parent = new_assignment
+    new_assignment.children = [lhs, rhs]
+    assignment.parent.children.insert(assignment.position, new_assignment)
+
+    # Negate the result if the first argument is negative, otherwise do nothing
+    if_stmt = IfBlock(parent=assignment.parent)
+    if_condition = BinaryOperation(BinaryOperation.Operator.LT, parent=if_stmt)
+    lhs = Reference(tmp_var, parent=if_condition)
+    rhs = Literal("0.0", parent=if_condition)
+    if_condition.children = [lhs, rhs]
+    then_schedule = Schedule(parent=if_stmt)
+    then_body = Assignment(parent=then_schedule)
+    then_body_lhs = Reference(res_var, parent=then_body)
+    then_body_rhs = Reference("XXXRESTIMESMINUS1_{0}".format(key), parent=then_body)
+    then_body.children = [then_body_lhs, then_body_rhs]
+    then_schedule.children = [then_body]                    
+    if_stmt.children = [if_condition, then_schedule]
+    assignment.parent.children.insert(assignment.position, if_stmt)
+    
 def trans(psy):
     '''Transformation routine for use with PSyclone. Applies the PSyIR2SIR
     transform to the supplied invokes. This transformation is limited
@@ -65,7 +184,7 @@ def trans(psy):
     # schedule. Note, there is no algorithm layer in the NEMO API so
     # the invokes represent all of the original code.
     for invoke in psy.invokes.invoke_list:
-        key=0
+        key = 0
         sched = invoke.schedule
         for kernel in sched.walk(NemoKern):
 
@@ -76,82 +195,13 @@ def trans(psy):
             for oper in kernel_schedule.walk(UnaryOperation):
                 if oper.operator == UnaryOperation.Operator.ABS:
                     print ("FOUND ABS")
-                    # R=ABS(X) => IF (X<0.0) R=X*-1.0 ELSE R=X 
-                    # TODO: There is an assumption that operation is child of assignment
-                    oper_parent = oper.parent
-                    assignment = oper.ancestor(Assignment)
-
-                    # Replace operation with a temporary (res_X).
-                    oper_parent.children[oper.position] = Reference("res_{0}".format(key), parent=oper_parent)
-
-                    # Assign content of operation to a temporary (tmp_X)
-                    new_assignment = Assignment(parent=assignment.parent)
-                    lhs = Reference("tmp_{0}".format(key), parent=new_assignment)
-                    rhs = oper.children[0]
-                    rhs.parent = new_assignment
-                    new_assignment.children = [lhs, rhs]
-                    assignment.parent.children.insert(assignment.position, new_assignment)
-
-                    # Set res_X to the absolute value of tmp_X
-                    if_stmt = IfBlock(parent=assignment.parent)
-                    if_condition = BinaryOperation(BinaryOperation.Operator.GT, parent=if_stmt)
-                    lhs = Reference("tmp_{0}".format(key), parent=if_condition)
-                    rhs = Literal("0.0", parent=if_condition)
-                    if_condition.children = [lhs, rhs]
-                    then_schedule = Schedule(parent=if_stmt)
-                    then_body = Assignment(parent=then_schedule)
-                    then_body_lhs = Reference("res_{0}".format(key), parent=then_body)
-                    then_body_rhs = Reference("tmp_{0}".format(key), parent=then_body)
-                    then_body.children = [then_body_lhs, then_body_rhs]
-                    then_schedule.children = [then_body]                    
-                    else_schedule = Schedule(parent=if_stmt)
-                    else_body = Assignment(parent=else_schedule)
-                    else_body_lhs = Reference("res_{0}".format(key), parent=else_body)
-                    else_body_rhs = BinaryOperation(BinaryOperation.Operator.MUL, parent=else_body)
-                    lhs_child = Reference("tmp_{0}".format(key), parent=else_body_rhs)
-                    rhs_child = Literal("-1.0", parent=else_body_rhs)
-                    else_body_rhs.children = [lhs_child, rhs_child]
-                    else_body.children = [else_body_lhs, else_body_rhs]
-                    else_schedule.children = [else_body]
-                    if_stmt.children = [if_condition, then_schedule, else_schedule]
-                    assignment.parent.children.insert(assignment.position, if_stmt)
-
-                    #new_assignment_mult = Assignment(parent=assignment.parent)
-                    #lhs = Reference("tmp1_{0}".format(key), parent=new_assignment_mult)
-                    #rhs = BinaryOperation(BinaryOperation.Operator.MUL, parent=new_assignment_mult)
-                    #new_assignment_mult.children = [lhs, rhs]
-                    #lhs_child = copy.copy(oper.children[0])
-                    #rhs_child = Literal("-1.0", parent=rhs)
-                    #rhs.children = [lhs_child, rhs_child]
-                    #assignment.parent.children.insert(assignment.position, new_assignment_mult)
-
-                    # This is essentially a max so reuse
-                    #if_stmt = IfBlock(parent=assignment.parent)
-                    #if_condition = BinaryOperation(BinaryOperation.Operator.GT, parent=if_stmt)
-                    #lhs = Reference("tmp0_{0}".format(key), parent=if_condition)
-                    #rhs = Reference("tmp1_{0}".format(key), parent=if_condition)
-                    #if_condition.children = [lhs, rhs]
-                    #then_schedule = Schedule(parent=if_stmt)
-                    #then_body = Assignment(parent=then_schedule)
-                    #then_body_lhs = Reference("tmp2_{0}".format(key), parent=then_body)
-                    #then_body_rhs = Reference("tmp0_{0}".format(key), parent=then_body)
-                    #then_body.children = [then_body_lhs, then_body_rhs]
-                    #then_schedule.children = [then_body]                    
-                    #else_schedule = Schedule(parent=if_stmt)
-                    #else_body = Assignment(parent=else_schedule)
-                    #else_body_lhs = Reference("tmp2_{0}".format(key), parent=else_body)
-                    #else_body_rhs = Reference("tmp1_{0}".format(key), parent=else_body)
-                    #else_body.children = [else_body_lhs, else_body_rhs]
-                    #else_schedule.children = [else_body]
-                    #if_stmt.children = [if_condition, then_schedule, else_schedule]
-                    #assignment.parent.children.insert(assignment.position, if_stmt)
-
+                    replace_abs(oper, key)
                     key += 1
             for oper in kernel_schedule.walk(BinaryOperation):
                 if oper.operator == BinaryOperation.Operator.SIGN:
                     print ("FOUND SIGN")
-                    # R=SIGN(A,B) if A<0 then (if B<0 R=B else R=B*-1) else ((if B>0 R=B else R=B*-1))
-                    # R=SIGN(A,B) R=ABS(B); if A<0 R=R*-1
+                    replace_sign(oper, key)
+                    key += 1
             for oper in kernel_schedule.walk(BinaryOperation):
                 if oper.operator == BinaryOperation.Operator.MIN:
                     print ("FOUND BINARY MIN")
@@ -165,3 +215,4 @@ def trans(psy):
         print(kern)
         exit(1)
     return psy
+
