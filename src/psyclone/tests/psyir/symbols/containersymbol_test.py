69f57119d9da15fe63f4b13245cda5a6fae0bfc0
Andrew Porter <andrew.porter@stfc.ac.uk>
2019-11-18 11:39:32 +0000

    #545 correct typos

--- src/psyclone/tests/psyir/symbols/containersymbol_test.py
+++ src/psyclone/tests/psyir/symbols/containersymbol_test.py
@@ -149,7 +149,7 @@ def test_containersymbol_fortranmodule_interface(monkeypatch, tmpdir):
             "'fake_module.[f|F]90') not found in any of the include_paths "
             "directories " in str(error.value))
 
-    # Try importing and existing Fortran module
+    # Try importing an existing Fortran module
     create_dummy_module(path)
     container = fminterface.import_container("dummy_module")
     assert isinstance(container, Container)
