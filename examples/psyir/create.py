243f24379c2eebc49997dea53ad2ec4341e7e8cf
Rupert Ford <rupert.ford@stfc.ac.uk>
2019-11-28 16:19:35 +0000

    pr #601. Completed the example.

--- examples/psyir/create.py
+++ examples/psyir/create.py
@@ -1,58 +1,117 @@
-''' XXX '''
+# -----------------------------------------------------------------------------
+# BSD 3-Clause License
+#
+# Copyright (c) 2019, Science and Technology Facilities Council
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# -----------------------------------------------------------------------------
+# Author: R. W. Ford, STFC Daresbury Lab
+'''A simple Python script showing how to create a PSyIR tree using the
+create methods. In order to use it you must first install
+PSyclone. See README.md in the top-level psyclone directory.
+
+Once you have psyclone installed, this script may be run by doing:
+
+>>> python create.py
+
+This should output a Fortran representation of the PSyIR and part of a
+C representation of the PSyIR.
+
+'''
+from __future__ import print_function
 from psyclone.psyGen import Reference, Literal, UnaryOperation, \
     BinaryOperation, NaryOperation, Assignment, IfBlock, Loop, \
     KernelSchedule, Container
 from psyclone.psyir.symbols import DataSymbol, SymbolTable, ArgumentInterface
 from psyclone.psyir.backend.fortran import FortranWriter
+from psyclone.psyir.backend.c import CWriter
 
 # Symbol table and symbols
-symbol_table = SymbolTable()
-arg1 = DataSymbol("tmp1", "real", interface=ArgumentInterface(
+SYMBOL_TABLE = SymbolTable()
+ARG1 = DataSymbol("tmp1", "real", interface=ArgumentInterface(
     ArgumentInterface.Access.READWRITE))
-symbol_table.add(arg1)
-symbol_table.add(DataSymbol("tmp2", "real"))
-symbol_table.add(DataSymbol("i", "integer"))
-symbol_table.specify_argument_list([arg1])
+SYMBOL_TABLE.add(ARG1)
+SYMBOL_TABLE.add(DataSymbol("tmp2", "real"))
+SYMBOL_TABLE.add(DataSymbol("i", "integer"))
+SYMBOL_TABLE.specify_argument_list([ARG1])
 
 # Nodes which do not have Nodes as children
-zero = Literal("0.0")
-one = Literal("1.0")
-tmp1 = Reference("tmp1")
-tmp2 = Reference("tmp2")
+ZERO = Literal("0.0")
+ONE = Literal("1.0")
+INT_ZERO = Literal("0")
+INT_ONE = Literal("1")
+TMP1 = Reference("tmp1")
+TMP2 = Reference("tmp2")
 
-# Unary operation
-oper = UnaryOperation.Operator.SIN
-unaryoperation = UnaryOperation.create(oper,tmp2)
+# Unary OPERation
+OPER = UnaryOperation.Operator.SIN
+UNARYOPERATION = UnaryOperation.create(OPER, TMP2)
 
-# Binary operation
-oper = BinaryOperation.Operator.ADD
-binaryoperation = BinaryOperation.create(oper, one, unaryoperation)
+# Binary OPERation
+OPER = BinaryOperation.Operator.ADD
+BINARYOPERATION = BinaryOperation.create(OPER, ONE, UNARYOPERATION)
 
-# Nary operation
-oper = NaryOperation.Operator.MAX
-naryoperation = NaryOperation.create(oper, [tmp1, tmp2, one])
+# Nary OPERation
+OPER = NaryOperation.Operator.MAX
+NARYOPERATION = NaryOperation.create(OPER, [TMP1, TMP2, ONE])
 
 # Assignments
-assign1 = Assignment.create(tmp1, zero)
-assign2 = Assignment.create(tmp2, zero)
-assign3 = Assignment.create(tmp2, binaryoperation)
-assign4 = Assignment.create(tmp2, naryoperation)
+ASSIGN1 = Assignment.create(TMP1, ZERO)
+ASSIGN2 = Assignment.create(TMP2, ZERO)
+ASSIGN3 = Assignment.create(TMP2, BINARYOPERATION)
+ASSIGN4 = Assignment.create(TMP1, TMP2)
+ASSIGN5 = Assignment.create(TMP1, NARYOPERATION)
 
 # If statement
-if_condition = BinaryOperation.create(BinaryOperation.Operator.EQ, tmp1, zero)
-ifblock = IfBlock.create(if_condition, [assign3, assign4])
+IF_CONDITION = BinaryOperation.create(BinaryOperation.Operator.GT, TMP1, ZERO)
+IFBLOCK = IfBlock.create(IF_CONDITION, [ASSIGN3, ASSIGN4])
 
 # Loop
-loop = Loop.create("i", zero, one, one, [ifblock])
+LOOP = Loop.create("i", INT_ZERO, INT_ONE, INT_ONE, [IFBLOCK])
 
 # KernelSchedule
-kernel_schedule = KernelSchedule.create("work", symbol_table,
-                                        [assign2, loop])
+KERNEL_SCHEDULE = KernelSchedule.create("work", SYMBOL_TABLE,
+                                        [ASSIGN2, LOOP, ASSIGN5])
 
 # Container
-container_symbol_table = SymbolTable()
-container = Container.create("container", container_symbol_table, [kernel_schedule])
+CONTAINER_SYMBOL_TABLE = SymbolTable()
+CONTAINER = Container.create("CONTAINER", CONTAINER_SYMBOL_TABLE,
+                             [KERNEL_SCHEDULE])
 
-writer = FortranWriter()
-result = writer(container)
-print (str(result))
+# Write out the code as Fortran
+WRITER = FortranWriter()
+RESULT = WRITER(CONTAINER)
+print (RESULT)
+
+# Write out the code as C. At the moment NaryOPERator, KernelSchedule
+# and CONTAINER are not supported in the C backend so the full example
+# can't be output.
+WRITER = CWriter()
+RESULT = WRITER(LOOP)
+print (RESULT)
