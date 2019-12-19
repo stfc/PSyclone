df4936085ec93083b9fab7b966b9ef82abca8135
Sergi Siso <sergiesg@gmail.com>
2019-12-19 10:02:51 +0000

    #190 Revert generic KernelArgument modification

--- src/psyclone/psyGen.py
+++ src/psyclone/psyGen.py
@@ -4920,10 +4920,11 @@ class Argument(object):
 
     @property
     def type(self):
-        '''Return the type of the argument. API's that do not have this concept
-        can use this baseclass version which just returns "scalar" in all
-        cases. API's with this concept can override this method. '''
-        return "scalar"
+        '''Return the type of the argument. API's that do not have this
+        concept (such as gocean0.1 and dynamo0.1) can use this
+        baseclass version which just returns "field" in all
+        cases. API's with this concept can override this method '''
+        return "field"
 
     @property
     def call(self):
