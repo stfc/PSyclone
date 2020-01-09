5d5fdf39e1f6ecdfde63830ede49f84e73bc357b
Sergi Siso <sergiesg@gmail.com>
2020-01-09 14:43:42 +0000

    #585 Add missing quotes in error message unit-test

--- src/psyclone/tests/psyir/frontend/literals_test.py
+++ src/psyclone/tests/psyir/frontend/literals_test.py
@@ -82,7 +82,7 @@ def test_handling_invalid_logic_literal(f2008_parser):
     with pytest.raises(GenerationError) as error:
         processor.process_nodes(fake_parent, [astmt], None)
     assert "Exptected to find '.true.' or '.false' as fparser2 logical " \
-        "literal, but found invalid instead." in str(error.value)
+        "literal, but found 'invalid' instead." in str(error.value)
 
 
 def test_number_handler():
