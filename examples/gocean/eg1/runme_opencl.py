f0b70902ba978a63f254063aab3b896ecac2c5c7
Sergi Sisó <sergiesg@gmail.com>
2020-06-15 13:12:01 +0100

    790 Update example/gocean/eg1 with a runme_opencl with the same format as the other runme scripts.

--- examples/gocean/eg1/runme_opencl.py
+++ examples/gocean/eg1/runme_opencl.py
@@ -1,29 +1,82 @@
-def trans(psy):
-    ''' Python script intended to be passed to PSyclone's generate()
-    function via the -s option. Applies OpenCL to the Schedule so
-    that PSyclone will generate an OpenCL PSy layer. '''
+# -----------------------------------------------------------------------------
+# BSD 3-Clause License
+#
+# Copyright (c) 2020, Science and Technology Facilities Council
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# -----------------------------------------------------------------------------
+# Authors: S. Siso, STFC Daresbury Lab
 
-    from psyclone.psyGen import TransInfo
-    tinfo = TransInfo()
-    globaltrans = tinfo.get_trans_name('KernelGlobalsToArguments')
-    cltrans = tinfo.get_trans_name('OCLTrans')
+'''A simple test script showing the introduction of OpenCL with PSyclone.
+In order to use it you must first install PSyclone like so:
 
-    for invoke in psy.invokes.invoke_list:
-        print("Converting to OpenCL invoke: " + invoke.name)
-        schedule = invoke.schedule
+ >>> pip install --user psyclone
 
-        # Skip invoke_2
-        if invoke.name == "invoke_2":
-            continue
+(or see the Getting Going section in ../../psyclone.pdf.) Once PSyclone is
+installed, this script may be run by doing:
 
-        # Remove the globals from inside each kernel
-        for kern in schedule.kernels():
-            print("Remove glovals from kernel: " + kern.name)
-            # if kern.name == "time_smooth_code":
-            #    import pdb; pdb.set_trace()
-            globaltrans.apply(kern)
+ >>> python runme_opencl.py
 
-        # Transform invoke to OpenCL
-        cltrans.apply(schedule)
+This should generate a lot of output, ending with the generated
+Fortran invoke subroutines using FortCl to interface with the OpenCL
+runtime. Additionally it should create a .cl file for each of the kernels.
 
-    return psy
+'''
+
+from __future__ import print_function
+from psyclone.parse.algorithm import parse
+from psyclone.psyGen import PSyFactory, TransInfo
+
+API = "gocean1.0"
+_, INVOKEINFO = parse("shallow_alg.f90", api=API)
+PSY = PSyFactory(API).create(INVOKEINFO)
+print(PSY.gen)
+
+TRANS_INFO = TransInfo()
+print(TRANS_INFO.list)
+GLOBAL_TRANS = TRANS_INFO.get_trans_name('KernelGlobalsToArguments')
+CL_TRANS = TRANS_INFO.get_trans_name('OCLTrans')
+
+for invoke in PSY.invokes.invoke_list:
+    print("Converting to OpenCL the invoke: " + invoke.name)
+    schedule = invoke.schedule
+
+    # Skip invoke_2
+    if invoke.name == "invoke_2":
+        continue
+
+    # Remove the globals from inside each kernel
+    for kern in schedule.kernels():
+        print("Remove glovals from kernel: " + kern.name)
+        GLOBAL_TRANS.apply(kern)
+
+    # Transform invoke to OpenCL
+    CL_TRANS.apply(schedule)
+
+print(PSY.gen)
