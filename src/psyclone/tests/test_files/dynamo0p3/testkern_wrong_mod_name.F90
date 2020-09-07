37faf32ace3de928d8b49ed7ed4f1849f423020c
TeranIvy <TeranIvy@users.noreply.github.com>
2020-09-07 17:59:01 +0100

    I #817: Changed metadata in examples of incorrect metadata

--- src/psyclone/tests/test_files/dynamo0p3/testkern_wrong_mod_name.F90
+++ src/psyclone/tests/test_files/dynamo0p3/testkern_wrong_mod_name.F90
@@ -44,12 +44,12 @@ module testkern
   implicit none
 
   type, extends(kernel_type) :: testkern_type
-     type(arg_type), dimension(5) :: meta_args = &
-          (/ arg_type(gh_real,  gh_read),        &
-             arg_type(gh_field, gh_inc,  w1),    &
-             arg_type(gh_field, gh_read, w2),    &
-             arg_type(gh_field, gh_read, w2),    &
-             arg_type(gh_field, gh_read, w3)     &
+     type(arg_type), dimension(5) :: meta_args =        &
+          (/ arg_type(gh_scalar, gh_real, gh_read),     &
+             arg_type(gh_field,  gh_real, gh_inc,  w1), &
+             arg_type(gh_field,  gh_real, gh_read, w2), &
+             arg_type(gh_field,  gh_real, gh_read, w2), &
+             arg_type(gh_field,  gh_real, gh_read, w3)  &
            /)
      integer :: iterates_over = cells
    contains
