cb18d44c6a939e20e7747dd3a1df477846e91495
TeranIvy <TeranIvy@users.noreply.github.com>
2020-09-07 17:53:24 +0100

    I #817: Changed metadata in ref_elem test examples

--- src/psyclone/tests/test_files/dynamo0p3/testkern_ref_elem_out_mod.F90
+++ src/psyclone/tests/test_files/dynamo0p3/testkern_ref_elem_out_mod.F90
@@ -42,12 +42,12 @@ module testkern_ref_elem_out_mod
   implicit none
 
   type, extends(kernel_type) :: testkern_ref_elem_out_type
-     type(arg_type), dimension(5) :: meta_args = &
-          (/ arg_type(gh_real,  gh_read),        &
-             arg_type(gh_field, gh_inc,  w1),    &
-             arg_type(gh_field, gh_read, w2),    &
-             arg_type(gh_field, gh_read, w2),    &
-             arg_type(gh_field, gh_read, w3)     &
+     type(arg_type), dimension(5) :: meta_args =        &
+          (/ arg_type(gh_scalar, gh_real, gh_read),     &
+             arg_type(gh_field,  gh_real, gh_inc,  w1), &
+             arg_type(gh_field,  gh_real, gh_read, w2), &
+             arg_type(gh_field,  gh_real, gh_read, w2), &
+             arg_type(gh_field,  gh_real, gh_read, w3)  &
              /)
      type(reference_element_data_type), dimension(3) ::                        &
           meta_reference_element =                                             &
