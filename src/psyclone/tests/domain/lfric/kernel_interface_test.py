71fb788a1ba6895479f6abc2d3024ba4adb2bc05
rupertford <rupert.ford@stfc.ac.uk>
2020-10-05 01:30:54 +0100

    pr #901. Added more kernel interface tests.

--- src/psyclone/tests/domain/lfric/kernel_interface_test.py
+++ src/psyclone/tests/domain/lfric/kernel_interface_test.py
@@ -41,11 +41,13 @@ from __future__ import absolute_import
 import os
 import pytest
 from psyclone.psyir.symbols import SymbolTable, ArgumentInterface
-from psyclone.domain.lfric import KernelInterface
+from psyclone.domain.lfric import KernelInterface, FunctionSpace
 from psyclone.domain.lfric.psyir import \
     MeshHeightDataSymbol, CellPositionDataSymbol, \
     RealVectorFieldDataDataSymbol, NumberOfUniqueDofsDataSymbol, \
-    RealFieldDataDataSymbol
+    RealFieldDataDataSymbol, NumberOfDofsDataSymbol, \
+    OperatorDataSymbol, NumberOfCellsDataSymbol, \
+    LfricIntegerScalarDataSymbol, DofMapDataSymbol
 from psyclone.psyir.frontend.fparser2 import INTENT_MAPPING
 from psyclone.psyGen import PSyFactory
 from psyclone.parse.algorithm import parse
@@ -123,10 +125,10 @@ def test_cell_map():
 
 
 def test_field_vector():
-    '''Test that the KernelInterface class field_vector. We want to check
-    that the correct symbol is referenced for the dimension of the
-    vector field symbols so the simplest solution is to use one of the
-    Fortran test examples.
+    '''Test the KernelInterface class field_vector method. We want to
+    check that the correct symbol is referenced for the dimension of
+    the vector field symbols so the simplest solution is to use one of
+    the Fortran test examples.
 
     '''
     kernel_interface = KernelInterface(None)
@@ -143,6 +145,9 @@ def test_field_vector():
     undf_tag = "undf_{0}".format(vector_arg.function_space.orig_name)
     undf_symbol = kernel_interface._symbol_table.lookup(undf_tag)
     assert isinstance(undf_symbol, NumberOfUniqueDofsDataSymbol)
+    assert isinstance(undf_symbol.interface, ArgumentInterface)
+    assert (undf_symbol.interface.access ==
+            kernel_interface._read_access.access)
 
     # vector fields declared, added to argument list, correct function
     # space specified and dimensioned correctly
@@ -160,10 +165,10 @@ def test_field_vector():
 
 
 def test_field():
-    '''Test that the KernelInterface class field. We want to check
-    that the correct symbol is referenced for the dimension of the
-    field symbol so the simplest solution is to use one of the
-    Fortran test examples.
+    '''Test the KernelInterface class field method. We want to check that
+    the correct symbol is referenced for the dimension of the field
+    symbol so the simplest solution is to use one of the Fortran test
+    examples.
 
     '''
     kernel_interface = KernelInterface(None)
@@ -180,6 +185,9 @@ def test_field():
     undf_tag = "undf_{0}".format(field_arg.function_space.orig_name)
     undf_symbol = kernel_interface._symbol_table.lookup(undf_tag)
     assert isinstance(undf_symbol, NumberOfUniqueDofsDataSymbol)
+    assert isinstance(undf_symbol.interface, ArgumentInterface)
+    assert (undf_symbol.interface.access ==
+            kernel_interface._read_access.access)
 
     # field declared, added to argument list, correct function
     # space specified and dimensioned correctly
@@ -226,9 +234,66 @@ def test_stencil():
     kernel_interface = KernelInterface(None)
     kernel_interface.stencil(None)
 
-# TBD
-# def test_operator():
-#     pass
+
+def test_operator():
+    '''Test the KernelInterface class operator method. We want to check
+    that the correct symbol is referenced for the dimension of the
+    operator symbol so the simplest solution is to use one of the Fortran
+    test examples.
+
+    '''
+    kernel_interface = KernelInterface(None)
+    _, invoke_info = parse(os.path.join(BASE_PATH, "10_operator.f90"),
+                           api="dynamo0.3")
+    psy = PSyFactory("dynamo0.3",
+                     distributed_memory=False).create(invoke_info)
+    schedule = psy.invokes.invoke_list[0].schedule
+    kernel = schedule[0].loop_body[0]
+    operator_arg = kernel.args[0]
+    kernel_interface.operator(operator_arg)
+
+    # fs_from symbol declared
+    fs_from_name = operator_arg.function_space_from.orig_name
+    fs_from_tag = "ndf_{0}".format(fs_from_name)
+    fs_from_symbol = kernel_interface._symbol_table.lookup(fs_from_tag)
+    assert isinstance(fs_from_symbol, NumberOfDofsDataSymbol)
+    assert fs_from_symbol.fs == fs_from_name
+    assert isinstance(fs_from_symbol.interface, ArgumentInterface)
+    assert (fs_from_symbol.interface.access ==
+            kernel_interface._read_access.access)
+
+    # fs_to symbol declared
+    fs_to_name = operator_arg.function_space_from.orig_name
+    fs_to_tag = "ndf_{0}".format(fs_to_name)
+    fs_to_symbol = kernel_interface._symbol_table.lookup(fs_to_tag)
+    assert isinstance(fs_to_symbol, NumberOfDofsDataSymbol)
+    assert fs_to_symbol.fs == fs_to_name
+    assert isinstance(fs_to_symbol.interface, ArgumentInterface)
+    assert (fs_to_symbol.interface.access ==
+            kernel_interface._read_access.access)
+
+    # ncells symbol declared
+    ncells_symbol = kernel_interface._symbol_table.lookup("ncell_3d")
+    assert isinstance(ncells_symbol, NumberOfCellsDataSymbol)
+    assert isinstance(ncells_symbol.interface, ArgumentInterface)
+    assert (ncells_symbol.interface.access ==
+            kernel_interface._read_access.access)
+
+    # operator declared, added to argument list, correct function
+    # spaces specified and dimensioned correctly
+    tag = operator_arg.name
+    symbol = kernel_interface._symbol_table.lookup(tag)
+    assert isinstance(symbol, OperatorDataSymbol)
+    assert isinstance(symbol.interface, ArgumentInterface)
+    assert (symbol.interface.access ==
+            ArgumentInterface(INTENT_MAPPING[operator_arg.intent]).access)
+    assert kernel_interface._arglist[-1] is symbol
+    assert symbol.fs_from == operator_arg.function_space_from.orig_name
+    assert symbol.fs_to == operator_arg.function_space_to.orig_name
+    assert len(symbol.shape) == 3
+    assert symbol.shape[0] is fs_from_symbol
+    assert symbol.shape[1] is fs_to_symbol
+    assert symbol.shape[2] is ncells_symbol
 
 
 @pytest.mark.xfail(reason="Issue #928: this callback is not yet implemented")
@@ -240,11 +305,46 @@ def test_cma_operator():
     kernel_interface = KernelInterface(None)
     kernel_interface.cma_operator(None)
 
-# TBD
-# def test_scalar():
-#     pass
-# def test_fs_common():
-#     pass
+
+def test_scalar():
+    '''Test that the KernelInterface class scalar method adds the
+    expected class to the symbol table and the _arglist list.
+
+    '''
+    kernel_interface = KernelInterface(None)
+    _, invoke_info = parse(os.path.join(
+        BASE_PATH, "1.6.1_single_invoke_1_int_scalar.f90"),
+                           api="dynamo0.3")
+    psy = PSyFactory("dynamo0.3",
+                     distributed_memory=False).create(invoke_info)
+    schedule = psy.invokes.invoke_list[0].schedule
+    kernel = schedule[0].loop_body[0]
+    scalar_arg = kernel.args[1]
+    kernel_interface = KernelInterface(None)
+    kernel_interface.scalar(scalar_arg)
+    symbol = kernel_interface._symbol_table.lookup(scalar_arg.name)
+    assert isinstance(symbol, LfricIntegerScalarDataSymbol)
+    assert isinstance(symbol.interface, ArgumentInterface)
+    assert (symbol.interface.access ==
+            INTENT_MAPPING[scalar_arg.intent])
+    assert kernel_interface._arglist[-1] is symbol
+
+
+def test_fs_common():
+    '''Test that the KernelInterface class fs_common method adds the
+    expected class to the symbol table and the _arglist list.
+
+    '''
+    kernel_interface = KernelInterface(None)
+    function_space = FunctionSpace("w3", None)
+    kernel_interface.fs_common(function_space)
+    fs_name = function_space.orig_name
+    symbol = kernel_interface._symbol_table.lookup("ndf_{0}".format(fs_name))
+    assert isinstance(symbol, NumberOfDofsDataSymbol)
+    assert isinstance(symbol.interface, ArgumentInterface)
+    assert (symbol.interface.access ==
+            kernel_interface._read_access.access)
+    assert kernel_interface._arglist[-1] is symbol
 
 
 @pytest.mark.xfail(reason="Issue #928: this callback is not yet implemented")
@@ -256,9 +356,44 @@ def test_fs_intergrid():
     kernel_interface = KernelInterface(None)
     kernel_interface.fs_intergrid(None)
 
-# TBD
-# def test_fs_compulsory_field():
-#     pass
+
+def test_fs_compulsory_field():
+    '''Test that the KernelInterface class fs_compulsory_field method adds
+    the expected classes to the symbol table and the _arglist list.
+
+    '''
+    kernel_interface = KernelInterface(None)
+    function_space = FunctionSpace("w3", None)
+    kernel_interface.fs_compulsory_field(function_space)
+    fs_name = function_space.orig_name
+
+    # undf declared and added to argument list
+    symbol = kernel_interface._symbol_table.lookup("undf_{0}".format(fs_name))
+    assert isinstance(symbol, NumberOfUniqueDofsDataSymbol)
+    assert isinstance(symbol.interface, ArgumentInterface)
+    assert (symbol.interface.access ==
+            kernel_interface._read_access.access)
+    assert kernel_interface._arglist[-2] is symbol
+
+    # ndf declared
+    ndf_symbol = kernel_interface._symbol_table.lookup(
+        "ndf_{0}".format(fs_name))
+    assert isinstance(ndf_symbol, NumberOfDofsDataSymbol)
+    assert isinstance(ndf_symbol.interface, ArgumentInterface)
+    assert (ndf_symbol.interface.access ==
+            kernel_interface._read_access.access)
+
+    # dofmap declared, added to argument list, correct function
+    # space specified and dimensioned correctly
+    tag = "dofmap_{0}".format(fs_name)
+    symbol = kernel_interface._symbol_table.lookup(tag)
+    assert isinstance(symbol, DofMapDataSymbol)
+    assert isinstance(symbol.interface, ArgumentInterface)
+    assert symbol.interface.access == kernel_interface._read_access.access
+    assert kernel_interface._arglist[-1] is symbol
+    assert symbol.fs == fs_name
+    assert len(symbol.shape) == 1
+    assert symbol.shape[0] is ndf_symbol
 
 
 @pytest.mark.xfail(reason="Issue #928: this callback is not yet implemented")
