4695eb6389f30d43c615a583c5258dfdeb985dab
TeranIvy <TeranIvy@users.noreply.github.com>
2021-01-09 00:56:57 +0000

    PR #1045: Updated metadata in lfric/eg7 [skip ci]

--- examples/lfric/eg7/columnwise_op_mul_kernel_mod.F90
+++ examples/lfric/eg7/columnwise_op_mul_kernel_mod.F90
@@ -8,7 +8,7 @@
 ! -----------------------------------------------------------------------------
 ! BSD 3-Clause License
 !
-! Modifications copyright (c) 2017-2020, Science and Technology Facilities Council
+! Modifications copyright (c) 2017-2021, Science and Technology Facilities Council
 ! All rights reserved.
 !
 ! Redistribution and use in source and binary forms, with or without
@@ -44,8 +44,8 @@ module columnwise_op_mul_kernel_mod
 
 use kernel_mod,              only : kernel_type
 use argument_mod,            only : arg_type,                               &
-                                    GH_COLUMNWISE_OPERATOR,                 &
-                                    GH_READ, GH_WRITE, GH_INC,              &
+                                    GH_COLUMNWISE_OPERATOR, GH_REAL,        &
+                                    GH_READ, GH_READWRITE,                  &
                                     ANY_SPACE_1, ANY_SPACE_2, ANY_SPACE_3,  &
                                     CELL_COLUMN
 
@@ -61,10 +61,10 @@ private
 
 type, public, extends(kernel_type) :: columnwise_op_mul_kernel_type
    private
-  type(arg_type) :: meta_args(3) = (/                                             &
-       arg_type(GH_COLUMNWISE_OPERATOR, GH_READ,      ANY_SPACE_1, ANY_SPACE_2),  &
-       arg_type(GH_COLUMNWISE_OPERATOR, GH_READ,      ANY_SPACE_2, ANY_SPACE_3),  &
-       arg_type(GH_COLUMNWISE_OPERATOR, GH_READWRITE, ANY_SPACE_1, ANY_SPACE_3)   &
+  type(arg_type) :: meta_args(3) = (/                                                     &
+       arg_type(GH_COLUMNWISE_OPERATOR, GH_REAL, GH_READ,      ANY_SPACE_1, ANY_SPACE_2), &
+       arg_type(GH_COLUMNWISE_OPERATOR, GH_REAL, GH_READ,      ANY_SPACE_2, ANY_SPACE_3), &
+       arg_type(GH_COLUMNWISE_OPERATOR, GH_REAL, GH_READWRITE, ANY_SPACE_1, ANY_SPACE_3)  &
        /)
   integer :: operates_on = CELL_COLUMN
 contains
@@ -81,32 +81,32 @@ contains
   !> @brief The subroutine which is called directly from the PSY layer and
   !> calculates op_C = op_C + op_A * op_B
   !>
-  !> @param [in] cell the horizontal cell index
-  !> @param [in] ncell_2d total number of cells in 2d grid
-  !> @param [in] columnwise_matrix_A banded matrix op_A
-  !> @param [in] nrow_A number of rows in the banded matrix A
-  !> @param [in] ncol_A number of columns in the banded matrix A
-  !> @param [in] bandwidth_A bandwidth of the banded matrix
-  !> @param [in] alpha_A banded matrix parameter \f$\alpha\f$
-  !> @param [in] beta_A banded matrix parameter \f$\beta\f$
-  !> @param [in] gamma_m_A banded matrix parameter \f$\gamma_-\f$
-  !> @param [in] gamma_p_A banded matrix parameter \f$\gamma_+\f$
-  !> @param [in] columnwise_matrix_B banded matrix op_B
-  !> @param [in] nrow_B number of rows in the banded matrix B
-  !> @param [in] ncol_B number of columns in the banded matrix B
-  !> @param [in] bandwidth_B bandwidth of the banded matrix
-  !> @param [in] alpha_B banded matrix parameter \f$\alpha\f$
-  !> @param [in] beta_B banded matrix parameter \f$\beta\f$
-  !> @param [in] gamma_m_B banded matrix parameter \f$\gamma_-\f$
-  !> @param [in] gamma_p_B banded matrix parameter \f$\gamma_+\f$
-  !> @param [in,out] columnwise_matrix_C banded matrix op_C
-  !> @param [in] nrow_C number of rows in the banded matrix C
-  !> @param [in] ncol_C number of columns in the banded matrix C
-  !> @param [in] bandwidth_C bandwidth of the banded matrix
-  !> @param [in] alpha_C banded matrix parameter \f$\alpha\f$
-  !> @param [in] beta_C banded matrix parameter \f$\beta\f$
-  !> @param [in] gamma_m_C banded matrix parameter \f$\gamma_-\f$
-  !> @param [in] gamma_p_C banded matrix parameter \f$\gamma_+\f$
+  !> @param [in] cell Horizontal cell index
+  !> @param [in] ncell_2d Total number of cells in 2d grid
+  !> @param [in] columnwise_matrix_A Banded matrix op_A
+  !> @param [in] nrow_A Number of rows in the banded matrix A
+  !> @param [in] ncol_A Number of columns in the banded matrix A
+  !> @param [in] bandwidth_A Bandwidth of the banded matrix
+  !> @param [in] alpha_A Banded matrix parameter \f$\alpha\f$
+  !> @param [in] beta_A Banded matrix parameter \f$\beta\f$
+  !> @param [in] gamma_m_A Banded matrix parameter \f$\gamma_-\f$
+  !> @param [in] gamma_p_A Banded matrix parameter \f$\gamma_+\f$
+  !> @param [in] columnwise_matrix_B Banded matrix op_B
+  !> @param [in] nrow_B Number of rows in the banded matrix B
+  !> @param [in] ncol_B Number of columns in the banded matrix B
+  !> @param [in] bandwidth_B Bandwidth of the banded matrix
+  !> @param [in] alpha_B Banded matrix parameter \f$\alpha\f$
+  !> @param [in] beta_B Banded matrix parameter \f$\beta\f$
+  !> @param [in] gamma_m_B Banded matrix parameter \f$\gamma_-\f$
+  !> @param [in] gamma_p_B Banded matrix parameter \f$\gamma_+\f$
+  !> @param [in,out] columnwise_matrix_C Banded matrix op_C
+  !> @param [in] nrow_C Number of rows in the banded matrix C
+  !> @param [in] ncol_C Number of columns in the banded matrix C
+  !> @param [in] bandwidth_C Bandwidth of the banded matrix
+  !> @param [in] alpha_C Banded matrix parameter \f$\alpha\f$
+  !> @param [in] beta_C Banded matrix parameter \f$\beta\f$
+  !> @param [in] gamma_m_C Banded matrix parameter \f$\gamma_-\f$
+  !> @param [in] gamma_p_C Banded matrix parameter \f$\gamma_+\f$
   subroutine columnwise_op_mul_kernel_code(cell,                      &
                                            ncell_2d,                  &
                                            columnwise_matrix_A,       &
@@ -141,7 +141,7 @@ contains
     integer(kind=i_def), intent(in) :: bandwidth_A, bandwidth_B, bandwidth_C
     real(kind=r_def), dimension(bandwidth_A,nrow_A,ncell_2d), intent(in) :: columnwise_matrix_A
     real(kind=r_def), dimension(bandwidth_B,nrow_B,ncell_2d), intent(in) :: columnwise_matrix_B
-    real(kind=r_def), dimension(bandwidth_C,nrow_C,ncell_2d), intent(out) :: columnwise_matrix_C
+    real(kind=r_def), dimension(bandwidth_C,nrow_C,ncell_2d), intent(inout) :: columnwise_matrix_C
 
     integer(kind=i_def), intent(in) :: alpha_A, beta_A, gamma_m_A, gamma_p_A
     integer(kind=i_def), intent(in) :: alpha_B, beta_B, gamma_m_B, gamma_p_B
