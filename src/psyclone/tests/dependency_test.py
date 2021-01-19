dba9716eb8462366b2c9021ccea70ca5d39462cc
TeranIvy <TeranIvy@users.noreply.github.com>
2021-01-19 10:54:03 +0000

    I #960: Updated part of tests

--- src/psyclone/tests/dependency_test.py
+++ src/psyclone/tests/dependency_test.py
@@ -1,7 +1,7 @@
 # -----------------------------------------------------------------------------
 # BSD 3-Clause License
 #
-# Copyright (c) 2019-2020, Science and Technology Facilities Council.
+# Copyright (c) 2019-2021, Science and Technology Facilities Council.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -565,7 +565,6 @@ def test_lfric_various_basis():
     _, invoke_info = get_invoke("10.3_operator_different_spaces.f90",
                                 "dynamo0.3", idx=0)
     var_info = str(VariablesAccessInfo(invoke_info.schedule))
-    assert "orientation_w2: READ" in var_info
     assert "basis_w3_qr: READ" in var_info
     assert "diff_basis_w0_qr: READ" in var_info
     assert "diff_basis_w2_qr: READ" in var_info
@@ -666,31 +665,6 @@ def test_lfric_stub_args2():
     assert "nfaces_qr_face: READ" in var_info
     assert "np_xyz_qr_face: READ" in var_info
     assert "weights_xyz_qr_face: READ" in var_info
-
-
-def test_lfric_stub_args3():
-    '''Check variable usage detection for cell position, operator
-
-    '''
-    from psyclone.dynamo0p3 import DynKernMetadata, DynKern
-    from psyclone.domain.lfric import KernStubArgList
-    ast = get_ast("dynamo0.3", "dummy_orientation_mod.f90")
-    metadata = DynKernMetadata(ast)
-    kernel = DynKern()
-    kernel.load_meta(metadata)
-    var_accesses = VariablesAccessInfo()
-    create_arg_list = KernStubArgList(kernel)
-    create_arg_list.generate(var_accesses=var_accesses)
-    var_info = str(var_accesses)
-    assert "cell: READ" in var_info
-    assert "op_2_ncell_3d: READ" in var_info
-    assert "op_2: READ" in var_info
-    assert "op_4_ncell_3d: READ" in var_info
-    assert "op_4: READ" in var_info
-    assert "orientation_w0: READ" in var_info
-    assert "orientation_w1: READ" in var_info
-    assert "orientation_w2: READ" in var_info
-    assert "orientation_w3: READ" in var_info
 
 
 def test_lfric_stub_boundary_dofs():
