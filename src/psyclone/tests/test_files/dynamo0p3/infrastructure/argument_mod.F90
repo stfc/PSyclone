77b51c2ebc81fb1860b1b0f15c9b48b73e20c70b
andrewcoughtrie <24609575+andrewcoughtrie@users.noreply.github.com>
2021-01-20 17:33:02 +0000

    #194 - Update copyright date in argument_mod.F90

--- src/psyclone/tests/test_files/dynamo0p3/infrastructure/argument_mod.F90
+++ src/psyclone/tests/test_files/dynamo0p3/infrastructure/argument_mod.F90
@@ -9,7 +9,7 @@
 
 ! BSD 3-Clause License
 !
-! Modifications copyright (c) 2017-2020, Science and Technology
+! Modifications copyright (c) 2020-2021, Science and Technology
 ! Facilities Council.
 ! All rights reserved.
 !
