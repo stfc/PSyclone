b3457cd3e4b499e8f417f263a46ae5d1ef1a7d0a
rupertford <rupert.ford@stfc.ac.uk>
2021-01-22 22:21:33 +0000

    pr #1068. Updated copyright date.

--- src/psyclone/parse/algorithm.py
+++ src/psyclone/parse/algorithm.py
@@ -1,7 +1,7 @@
 # -----------------------------------------------------------------------------
 # BSD 3-Clause License
 #
-# Copyright (c) 2019-2020, Science and Technology Facilities Council.
+# Copyright (c) 2019-2021, Science and Technology Facilities Council.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
