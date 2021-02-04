f8ab5280a77f6ff180ef51776d793f1d70c26baf
Sergi Sisó <sergiesg@gmail.com>
2021-02-04 13:24:25 +0000

    #1087 Update copyrights touched in this PR

--- src/psyclone/tests/psyir/transformations/intrinsics/matmul2code_trans_test.py
+++ src/psyclone/tests/psyir/transformations/intrinsics/matmul2code_trans_test.py
@@ -1,7 +1,7 @@
 # -----------------------------------------------------------------------------
 # BSD 3-Clause License
 #
-# Copyright (c) 2020, Science and Technology Facilities Council
+# Copyright (c) 2020-2021, Science and Technology Facilities Council
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -31,7 +31,7 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 # -----------------------------------------------------------------------------
-# Author R. W. Ford, STFC Daresbury Lab
+# Authors R. W. Ford and S. Siso, STFC Daresbury Lab
 
 '''Module containing tests for the matmul2code transformation.'''
 
