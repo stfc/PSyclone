eea3e34d72e6c694a2b347a9428b44d345205e23
rupertford <rupert.ford@stfc.ac.uk>
2021-04-27 21:29:46 +0100

    pr #1228. Removing Python2 string issues.

--- src/psyclone/domain/common/algorithm/psyir.py
+++ src/psyclone/domain/common/algorithm/psyir.py
@@ -36,6 +36,8 @@
 '''This module contains PSyclone Algorithm-layer-specific PSyIR classes.
 
 '''
+import six
+
 from psyclone.psyir.nodes import Call, Reference, DataNode, Literal, \
     ArrayReference
 from psyclone.psyir.symbols import TypeSymbol, ContainerSymbol, \
@@ -80,11 +82,15 @@ class AlgorithmInvokeCall(Call):
             raise ValueError(
                 "AlgorithmInvokeCall index argument should be a non-negative "
                 "integer but found {0}.".format(index))
-        if description and not isinstance(description, str):
+        
+        if description and not isinstance(description, six.text_type):
             raise TypeError(
                 "AlgorithmInvokeCall description argument should be a str but "
                 "found '{0}'.".format(type(description).__name__))
 
+        if description:
+            # Avoid unicode issues with Python2
+            description = str(description)
         self._index = index
         self.psylayer_routine_symbol = None
         self._description = description
