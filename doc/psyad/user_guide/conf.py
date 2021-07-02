97cabdf69fb387830d3e3c632e55e7f52013d3b5
Andy Porter <andrew.porter@stfc.ac.uk>
2021-07-02 09:11:48 +0100

    #1297 document example and switch to RTD html theme

--- doc/psyad/user_guide/conf.py
+++ doc/psyad/user_guide/conf.py
@@ -30,8 +30,7 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,9 +46,13 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
+
+# Configuration for intersphinx: allows us to link to the PSyclone User Guide.
+intersphinx_mapping = {
+    'user_guide': ('https://psyclone.readthedocs.io/en/latest/', None)}
