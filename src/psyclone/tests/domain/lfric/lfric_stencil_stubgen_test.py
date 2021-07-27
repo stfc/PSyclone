25f5e9a3ac6d2b81aaf692e8821708081a49f247
Sergi Sisó <sergiesg@gmail.com>
2021-07-27 13:54:04 +0100

    #1359 Add tear down logic to the test fixtures

--- src/psyclone/tests/domain/lfric/lfric_stencil_stubgen_test.py
+++ src/psyclone/tests/domain/lfric/lfric_stencil_stubgen_test.py
@@ -58,6 +58,8 @@ TEST_API = "dynamo0.3"
 def setup():
     '''Make sure that all tests here use dynamo0.3 as API.'''
     Config.get().api = "dynamo0.3"
+    yield()
+    Config._instance = None
 
 
 def test_stub_stencil_extent():
