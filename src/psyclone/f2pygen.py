11f70ae1316c16158f01fe2e3b469fdcd3f103bd
Sergi Sisó <sergiesg@gmail.com>
2021-07-28 08:44:22 +0100

    #1361 Update copyrights

--- src/psyclone/f2pygen.py
+++ src/psyclone/f2pygen.py
@@ -31,7 +31,7 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 # -----------------------------------------------------------------------------
-# Authors R. W. Ford and A. R. Porter, STFC Daresbury Lab
+# Authors R. W. Ford, A. R. Porter and S. Siso, STFC Daresbury Lab
 
 ''' Fortran code-generation library. This wraps the f2py fortran parser to
     provide routines which can be used to generate fortran code. '''
