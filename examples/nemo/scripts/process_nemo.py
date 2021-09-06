29fb5e8e4ceea14a4d0150fc159b5e90c2aec5b1
Andrew Porter <andrew.porter@stfc.ac.uk>
2021-09-06 11:42:20 +0100

    #1147 add obs, iom and zdfosm files to those we only profile

--- examples/nemo/scripts/process_nemo.py
+++ examples/nemo/scripts/process_nemo.py
@@ -53,7 +53,7 @@ import os
 import sys
 
 # Files that we will only add profiling to
-PROFILE_ONLY = ["bdyini.f90",
+PROFILE_ONLY = ["bdyini.f90", "bdydta.f90", "bdyvol.f90",
                 "diaobs.f90",
                 "diawri.f90",  # Unused & has Return in profile region
                 "dommsk.f90",
@@ -66,11 +66,13 @@ PROFILE_ONLY = ["bdyini.f90",
                 "iceforcing.f90",
                 "iceistate.f90",
                 "icethd_ent.f90", "icethd_zdf.f90",
-                "icethd_dh.f90", "iom.f90",
+                "icethd_dh.f90", "iom.f90", "iom_nf90.f90"
                 "obs_inter_h2d.f90", "obs_grid.f90", "obs_averg_h2d.f90",
                 "obs_profiles_def.f90", "obs_sort.f90", "obs_types.f90",
+                "obs_surf_def.f90", "obs_read_prof.f90", "obs_read_surf.f90",
+                "obs_write.f90",
                 "stopar.f90",
-                "tide_mod.f90"]
+                "tide_mod.f90", "zdfosm.f90"]
 
 # Files that we won't touch at all
 EXCLUDED_FILES = [
@@ -133,7 +135,6 @@ if __name__ == "__main__":
             extra_args = ["-p", "invokes",
                           "-oalg", "/dev/null",
                           "-opsy", out_file, ffile]
-            continue
         elif file_name in EXCLUDED_FILES:
             print("Skipping {0} entirely.".format(ffile))
             continue
