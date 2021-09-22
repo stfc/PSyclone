95e22d90ad3e9543d4e03fde60b8224ec8cb315b
Andrew Porter <andrew.porter@stfc.ac.uk>
2021-09-22 09:43:23 +0100

    #1147 rm tests put back during merge

--- src/psyclone/tests/nemo/transformations/openacc/data_directive_test.py
+++ src/psyclone/tests/nemo/transformations/openacc/data_directive_test.py
@@ -45,7 +45,6 @@ import os
 import pytest
 
 from fparser.common.readfortran import FortranStringReader
-from fparser.two import Fortran2003
 from psyclone.errors import InternalError
 from psyclone.gocean1p0 import GOACCEnterDataDirective
 from psyclone.psyGen import PSyFactory, TransInfo
@@ -124,56 +123,6 @@ def test_data_no_gen_code():
         schedule.children[0].gen_code(schedule)
     assert ("ACCDataDirective.gen_code should not have "
             "been called" in str(err.value))
-
-
-def test_add_region_invalid_data_move():
-    ''' Check that _add_region() raises the expected error if an invalid
-    value for data_movement is supplied. '''
-    _, invoke_info = get_invoke("explicit_do.f90", api=API, idx=0)
-    schedule = invoke_info.schedule
-    acc_trans = TransInfo().get_trans_name('ACCDataTrans')
-    acc_trans.apply(schedule.children)
-    datadir = schedule.children[0]
-    with pytest.raises(InternalError) as err:
-        datadir._add_region("DATA", "END DATA", data_movement="invalid")
-    assert ("optional data_movement argument must be one of ['present', "
-            "'analyse'] but got 'invalid'" in str(err.value))
-
-
-def test_add_region(parser):
-    ''' Check that add_region works as expected. '''
-    reader = FortranStringReader(EXPLICIT_DO)
-    code = parser(reader)
-    psy = PSyFactory(API, distributed_memory=False).create(code)
-    schedule = psy.invokes.get('explicit_do').schedule
-    acc_trans = TransInfo().get_trans_name('ACCDataTrans')
-    acc_trans.apply(schedule.children)
-    datadir = schedule.children[0]
-    datadir._add_region("data", "end data")
-    assert isinstance(datadir._ast, Fortran2003.Comment)
-    assert str(datadir._ast).lower() == "!$acc data"
-    assert isinstance(datadir._ast_end, Fortran2003.Comment)
-    assert str(datadir._ast_end).lower() == "!$acc end data"
-
-
-def test_add_region_comment_err(parser):
-    ''' Check that _add_region rejects begin and end strings that contain
-    comments that are not directives. '''
-    reader = FortranStringReader(EXPLICIT_DO)
-    code = parser(reader)
-    psy = PSyFactory(API, distributed_memory=False).create(code)
-    schedule = psy.invokes.get('explicit_do').schedule
-    acc_trans = TransInfo().get_trans_name('ACCDataTrans')
-    acc_trans.apply(schedule.children)
-    datadir = schedule.children[0]
-    with pytest.raises(InternalError) as err:
-        datadir._add_region("!data", "!end data")
-    assert ("start_text must be a plain label without directive or comment "
-            "characters but got: '!data'" in str(err.value))
-    with pytest.raises(InternalError) as err:
-        datadir._add_region("data", "!end data")
-    assert ("end_text must be a plain label without directive or comment "
-            "characters but got: '!end data'" in str(err.value))
 
 
 def test_explicit_directive(parser):
