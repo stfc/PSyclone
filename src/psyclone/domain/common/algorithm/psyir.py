d89d83921434ec21169e77c87dcfe9c4e2fe385f
rupertford <rupert.ford@stfc.ac.uk>
2021-12-06 12:41:31 +0000

    pr #1228. Added six back into psyir.py as it is still used.

--- src/psyclone/domain/common/algorithm/psyir.py
+++ src/psyclone/domain/common/algorithm/psyir.py
@@ -38,7 +38,7 @@
 
 '''
 from __future__ import absolute_import
-
+import six
 from psyclone.core import SymbolicMaths
 from psyclone.psyir.nodes import Call, Reference, DataNode, Literal, \
     ArrayReference, Routine, FileContainer
