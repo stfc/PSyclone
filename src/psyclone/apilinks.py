f400edecc2d4c754c9aca920cd578778dc58af07
Andrew Porter <andrew.porter@stfc.ac.uk>
2022-02-03 15:00:15 +0000

    #1410 tidy implementation and use intersphinx mapping to achieve same thing for dev guide

--- src/psyclone/apilinks.py
+++ src/psyclone/apilinks.py
@@ -1,18 +1,43 @@
 from docutils import nodes, utils
 
+'''
+This module defines a Sphinx plugin which enables the use of a
+':ref_guide:' role within Sphinx documentation. The link targets
+are then generated using the 'ref_guide_base' (configuration
+variable) as a base. This is set in the 'conf.py' of the documentation
+and enables link targets to be configured at build time.
+
+'''
+
 
 def setup(app):
-    def api_link_role(role, rawtext, text, lineno, inliner, options={},
+    '''
+    Sets-up this Sphinx plugin.
+
+    '''
+    def ref_link_role(rawtext, text, lineno, inliner, options={},
                       content=[]):
-        #import pdb; pdb.set_trace()
+        '''
+        Handler routine called when the ':ref_guide:' role is encountered.
+
+        Modifies the supplied link text by pre-pending the text stored in
+        the 'ref_guide_base' Sphinx configuration variable.
+
+        '''
         items = text.split()
-        ref = app.config.apilinks_base + items[-1]
-        if len(items) == 2:
-            name = items[0]
+        ref = app.config.ref_guide_base + items[-1]
+        if len(items) > 1:
+            # The link had a name associated with it so reconstruct it.
+            name = ' '.join(items[:-1])
         else:
             name = utils.unescape(ref)
-
+        # Construct the new reference.
         node = nodes.reference(rawtext, name, refuri=ref, **options)
         return [node], []
-    app.add_config_value('apilinks_base', 'http://localhost/', False)
-    app.add_role('apilink', api_link_role)
+
+    # Define a new Sphinx configuration variable that will be used to
+    # store the base URL to use when generating links.
+    app.add_config_value('ref_guide_base', 'http://localhost/', False)
+
+    # Define the new role and assign it our new handler.
+    app.add_role('ref_guide', ref_link_role)
