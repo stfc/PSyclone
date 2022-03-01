4d5c9ed221fba3aceb43924ef8d7e9bb2bdf5389
Joerg Henrichs <henrichsjoerg@gmail.com>
2022-03-01 11:59:31 +1100

    #1623 Updated solution to be identical with current gol-lib.

--- tutorial/training/2.4-GameOfLife-psyclone/solution/output_field_mod.f90
+++ tutorial/training/2.4-GameOfLife-psyclone/solution/output_field_mod.f90
@@ -6,22 +6,27 @@ module output_field_mod
 contains
 
     subroutine output_field(field)
-        USE field_mod, only         : r2d_field
+        USE field_mod, only      : r2d_field
+        USE kind_params_mod, only: go_wp
+        USE parallel_mod, only: on_master
 
         implicit none
         TYPE(r2d_field), intent(in) :: field
 
-        integer                     :: j, xstart, xstop, ystart, ystop
+        real(go_wp), dimension(:,:), allocatable :: global_data
+        integer                                  :: j
 
-        xstart = field%internal%xstart+1
-        xstop  = field%internal%xstop-1
-        ystart = field%internal%ystart+1
-        ystop  = field%internal%ystop-1
+        ! Gather the (potentially distributed) arrays into a
+        ! 2d Fortran array
+        call field%gather_inner_data(global_data)
 
-        do j=ystart, ystop
-            write(*,"(99F2.0)") field%data(xstart:xstop, j)
-        enddo
-        write(*,*)
+        if (on_master()) then
+            do j=lbound(global_data, 2), ubound(global_data, 2)
+                write(*,"(99F2.0)") global_data(1:ubound(global_data,1), j)
+            enddo
+            write(*,*)
+        endif
+        deallocate(global_data)
 
     end subroutine output_field
 
--- tutorial/training/2.4-GameOfLife-psyclone/solution/read_config_mod.f90
+++ tutorial/training/2.4-GameOfLife-psyclone/solution/read_config_mod.f90
@@ -9,7 +9,7 @@ contains
 subroutine read_config(grid, initial, time_steps)
 
     USE grid_mod, only           : grid_type, grid_init, GO_ARAKAWA_C,     &
-                                   GO_BC_PERIODIC, GO_BC_NONE, GO_OFFSET_SW
+                                   GO_BC_EXTERNAL, GO_BC_NONE, GO_OFFSET_SW
     use parallel_mod, only       : parallel_init
     USE field_mod, only          : r2d_field, go_t_points
 
@@ -21,6 +21,7 @@ subroutine read_config(grid, initial, time_steps)
 
     character(len=256)           :: filename
     integer                      :: n_rows, n_cols
+    real(kind=8), dimension(:,:), allocatable :: initial_state
 
     call getarg(1, filename)
 
@@ -41,37 +42,40 @@ subroutine read_config(grid, initial, time_steps)
 
     ! 2) Create the information about the grid type to be used.
     grid = grid_type(GO_ARAKAWA_C,                                  &
-                     (/GO_BC_PERIODIC,GO_BC_PERIODIC,GO_BC_PERIODIC/),  &
+                     (/GO_BC_EXTERNAL,GO_BC_EXTERNAL,GO_BC_EXTERNAL/),  &
                      GO_OFFSET_SW)
 
-    ! 3) Create the domain decomposition - we are just using 1 process
-    call grid%decompose(n_cols+2, n_rows+2, ndomains=1, &
+    ! 3) Create the domain decomposition - we are using 4 process,
+    !    allow for a boundary size of 1
+    call grid%decompose(n_cols, n_rows, ndomains=1, &
                         halo_width=1)
 
     ! 4) Grid init
     call grid_init(grid, dxarg=1.0_8, dyarg=1.0_8)
 
     ! Now we can use the grid to create the field that stores the
-    ! initial data:
-    initial = r2d_field(grid, GO_T_POINTS)
+    ! initial data.
+    allocate(initial_state(n_rows, n_cols))
+    initial_state = 0.0
 
-    call get_initial_state(initial, n_rows, n_cols)
+    call get_initial_state(initial_state, n_rows, n_cols)
+
+    initial = r2d_field(grid, grid_points = GO_T_POINTS,   &
+                        init_global_data = initial_state)
 
     close(15)
 
 end subroutine read_config
 
 ! ----------------------------------------------------------------------------
-subroutine get_initial_state(field, n_rows, n_cols)
+subroutine get_initial_state(initial_state, n_rows, n_cols)
     USE field_mod, only            : r2d_field
     implicit none
-    TYPE(r2d_field), intent(inout) :: field
+    real(kind=8), dimension(:,:), allocatable :: initial_state
     integer, intent(in)            :: n_rows, n_cols
 
-    integer :: line_count, ios, x, y, dx, dy
+    integer :: line_count, ios, x, y
 
-    dx = field%internal%xstart
-    dy = field%internal%ystart
     line_count = 0
 
     do
@@ -81,7 +85,7 @@ subroutine get_initial_state(field, n_rows, n_cols)
             exit
         endif
         if (x>=1 .and. x<=n_cols .and. y>=1 .and. y<=n_rows) then
-            field%data(x+dx, y+dy) = 1.0_8
+            initial_state(x, y) = 1.0_8
         else
             print *,"Ignoring line ",line_count,": ",x, y
         endif
