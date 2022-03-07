611de449e62ddc9c15b53cc098a1d393387c36d3
Aidan Chalk <3043914+LonelyCat124@users.noreply.github.com>
2022-03-07 14:58:42 +0000

    #1636 Combined if statements

--- src/psyclone/psyir/nodes/reference.py
+++ src/psyclone/psyir/nodes/reference.py
@@ -155,9 +155,8 @@ class Reference(DataNode):
         '''
         if (self.parent and isinstance(self.parent, Operation) and
                 self.parent.operator in [BinaryOperation.Operator.LBOUND,
-                                         BinaryOperation.Operator.UBOUND]):
-            # We can't check .index(self) as that checks == not is
-            if self.parent.children[0] is self:
+                                         BinaryOperation.Operator.UBOUND]
+                and self.parent.children[0] is self):
                 # This reference is the first argument to a lbound or
                 # ubound intrinsic. These intrinsics do not access the
                 # array elements, they determine the array
