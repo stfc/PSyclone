a4e2f4fce22881668e911d85c84f630519cfbe78
rupertford <rupert.ford@stfc.ac.uk>
2022-03-07 17:00:26 +0000

    pr #1633. Add validation checks and initial test.

--- src/psyclone/domain/common/kernel/psyir.py
+++ src/psyclone/domain/common/kernel/psyir.py
@@ -36,10 +36,12 @@
 '''This module contains PSyclone Kernel-layer-specific PSyIR classes.
 
 '''
+from psyclone.parse.utils import ParseError
 from psyclone.psyir.symbols import DataTypeSymbol
 from fparser.common.readfortran import FortranStringReader
 from fparser.two import Fortran2003
 from fparser.two.utils import walk
+from psyclone.domain.gocean import GOceanConstants
 
 
 class KernelMetadataSymbol(DataTypeSymbol):
@@ -48,8 +50,9 @@ class KernelMetadataSymbol(DataTypeSymbol):
     information to PSyclone in an easier to access form.
 
     '''
-    class KernelMetadataArg():
-        '''Internal class to capture Kernel metadata argument information.
+    class GridArg():
+        '''Internal class to capture Kernel metadata argument information for
+        a field argument.
 
         :param meta_arg: the native PSyIR representation of a kernel \
             metadata argument. This is currently part of a codeblock \
@@ -58,13 +61,45 @@ class KernelMetadataSymbol(DataTypeSymbol):
 
         '''
         def __init__(self, meta_arg):
-            # TODO? Check name == "go_arg" print(repr(meta_args))
             arg_list = meta_arg.children[1]
+            if len(arg_list.children) != 2:
+                raise Exception("xxx")
             self._access = arg_list.children[0].string
-            self._description = arg_list.children[1].string
-            if len(arg_list.children) == 2:
-                # Grid properties have 2 arguments
-                return
+            self._grid_property = arg_list.children[1].string
+
+        @property
+        def access(self):
+            '''
+            :returns: the value of access.
+            :rtype: str
+            '''
+            return self._access
+
+        @property
+        def name(self):
+            '''
+            :returns: the grid property name.
+            :rtype: str
+            '''
+            return self._grid_property
+
+
+    class FieldArg():
+        '''Internal class to capture Kernel metadata argument information for
+        a field argument.
+
+        :param meta_arg: the native PSyIR representation of a kernel \
+            metadata argument. This is currently part of a codeblock \
+            in PSyIR so is actually an fparser2 tree.
+        :type meta_arg: :py:class:`fparser.two.Fortran2003.Part_Ref`
+
+        '''
+        def __init__(self, meta_arg):
+            arg_list = meta_arg.children[1]
+            if len(arg_list.children) != 3:
+                raise Exception("xxx")
+            self._access = arg_list.children[0].string
+            self._stagger = arg_list.children[1].string
             if isinstance(arg_list.children[2], Fortran2003.Name):
                 self._form = arg_list.children[2].string
                 self._stencil = None
@@ -74,36 +109,153 @@ class KernelMetadataSymbol(DataTypeSymbol):
                 for stencil_dim in arg_list.children[2].children[1].children:
                     self._stencil.append(stencil_dim.children[0])
 
-        def __repr__(self):
-            return(f"({self._access}, {self._description}, {self._errr}, {self._stencil})")
+        @property
+        def access(self):
+            '''
+            :returns: the value of access.
+            :rtype: str
+            '''
+            return self._access
 
+        @property
+        def stagger(self):
+            '''
+            :returns: the value of stagger.
+            :rtype: str
+            '''
+            return self._stagger
+
+        @property
+        def form(self):
+            '''
+            :returns: the form of access.
+            :rtype: str
+            '''
+            return self._form
+
+        @property
+        def stencil(self):
+            '''
+            :returns: the stencil value, or None if there is no stencil.
+            :rtype: str[3] or NoneType
+            '''
+            return self._stencil
+
+
+    @property
+    def iterates_over(self):
+        '''
+        :returns: the value of iterates_over.
+        :rtype: str
+        '''
+        return self._iterates_over
+
+    @property
+    def index_offset(self):
+        '''
+        :returns: the value of index_offset.
+        :rtype: str
+        '''
+        return self._index_offset
+
+    @property
+    def args(self):
+        '''
+        :returns: a list of arg objects capturing their metadata values.
+        :rtype: list of :py:class:`psyclone.psyir.common.kernel. \
+            KernelMetadataSymbol.KernelMetadataArg`
+        '''
+        return self._meta_args
+
+    @property
+    def code(self):
+        '''
+        :returns: the kernel code routine name.
+        :rtype: str
+        '''
+        return self._routine_name
 
     def _setup(self):
-        '''Validates the metadata and extracts the values to make them easier
-        to access from an instance of this class.
+        '''Validates the non-api-specific aspects of the metadata and extracts
+        the values to make them easier to access from an instance of
+        this class.
 
         '''
+        const = GOceanConstants()
         unknown_fortran_type = self.datatype
         # The type is stored as a string so parse it with fparser2
         reader = FortranStringReader(unknown_fortran_type.declaration)
         spec_part = Fortran2003.Derived_Type_Def(reader)
         component_part = spec_part.children[1]
 
-        meta_args_def = component_part.children[0]
-        self._meta_args = []
-        for meta_arg in walk(meta_args_def, Fortran2003.Ac_Value_List)[0].children:
-            self._meta_args.append(self.KernelMetadataArg(meta_arg))
-            
-        iterates_over_def = component_part.children[1]
-        self._iterates_over = walk(
-            iterates_over_def,
-            Fortran2003.Component_Initialization)[0].children[1].string
+        found_meta_args = False
+        found_iterates_over = False
+        found_index_offset = False
+        for entry in component_part.children:
+            name = entry.children[2].children[0].children[0].string.lower()
+            if name == "meta_args":
+                if found_meta_args:
+                    raise ParseError(
+                        f"'meta_args' should only be defined once in the "
+                        f"metadata, but found {str(component_part)}.")
+                found_meta_args = True
+                self._meta_args = []
+                for meta_arg in walk(
+                        entry, Fortran2003.Ac_Value_List)[0].children:
+                    if len(meta_arg.children[1].children) == 2:
+                        self._meta_args.append(self.GridArg(meta_arg))
+                    else: # nargs should be 3
+                        self._meta_args.append(self.FieldArg(meta_arg))
+            elif name == "iterates_over":
+                if found_iterates_over:
+                    raise ParseError(
+                        f"'iterates_over' or should only be defined once in "
+                        f"the metadata, but found {str(component_part)}.")
+                found_iterates_over = True
+                iterates_over_def = component_part.children[1]
+                self._iterates_over = walk(
+                    entry,
+                    Fortran2003.Component_Initialization)[0].children[1].string
+                if self._iterates_over.lower() not in \
+                   const.VALID_ITERATES_OVER:
+                    raise ParseError(
+                        f"The value of 'iterates_over' should be one of "
+                        f"{str(const.VALID_ITERATES_OVER)}, but found "
+                        f"'{self._iterates_over}'.")
+            elif name == "index_offset":
+                if found_index_offset:
+                    raise ParseError(
+                        f"'index_offset' should only be defined once in the "
+                        f"metadata, but found {str(component_part)}.")
+                found_index_offset = True
+                self._index_offset = walk(
+                    entry,
+                    Fortran2003.Component_Initialization)[0].children[1].string
+                if self._index_offset.lower() not in const.VALID_OFFSET_NAMES:
+                    raise ParseError(
+                        f"The value of 'index_offset' should be one of "
+                        f"{str(const.VALID_OFFSET_NAMES)}, but found "
+                        f"'{self._index_offset}'.")
+            else:
+                raise ParseError(
+                    f"Expecting metadata entries to be one of 'meta_args', "
+                    f"'iterates_over', or 'index_offset', but found {name} "
+                    "in {str(component_part)}.")
 
-        index_offset_def = component_part.children[2]
-        self._index_offset = walk(
-            index_offset_def,
-            Fortran2003.Component_Initialization)[0].children[1].string
+        if not found_meta_args:
+            raise ParseError(
+                f"Expecting 'meta_args' to be an entry in the metadata but "
+                f"it was not found in {str(component_part)}.")
+        if not found_iterates_over:
+            raise ParseError(
+                f"Expecting 'iterates_over' to be an entry in the metadata "
+                "but it was not found in {str(component_part)}.")
+        if not found_index_offset:
+            raise ParseError(
+                f"Expecting 'index_offset' to be an entry in the metadata but "
+                f"it was not found in {str(component_part)}.")
 
+        # TODO RAISE EXCEPTION IF INVALID OR DOES NOT EXIST
         type_bound_procedure = spec_part.children[2]
         self._routine_name = walk(
             type_bound_procedure,
---
+++ src/psyclone/tests/domain/common/kernel/kern_psyir_test.py
@@ -0,0 +1,91 @@
+# -----------------------------------------------------------------------------
+# BSD 3-Clause License
+#
+# Copyright (c) 2022, Science and Technology Facilities Council
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# -----------------------------------------------------------------------------
+# Author R. W. Ford, STFC Daresbury Lab
+
+'''Module containing tests for the KernelMetadataSymbol
+kernel-layer-specific symbol. The tests include
+translation of PSyIR to PSyclone Kernel PSyIR and PSyclone
+Kernel PSyIR to processed PSyIR.
+
+'''
+from psyclone.domain.common.transformations import KernTrans
+from psyclone.domain.common.kernel import KernelMetadataSymbol
+
+# TODO STENCIL, GRIDVALUE
+METADATA_GOCEAN = (
+    "program dummy\n"
+    "  type, extends(kernel_type) :: compute_cu\n"
+    "     type(go_arg), dimension(2) :: meta_args =                 &\n"
+    "          (/ go_arg(GO_WRITE, GO_CU, GO_POINTWISE),            &\n"
+    "             go_arg(GO_READ,  GO_CT, GO_STENCIL(000,011,000)), &\n"
+    "             go_arg(GO_READ,  GO_GRID_AREA_T)                  &\n"
+    "           /)\n"
+    "     integer :: ITERATES_OVER = GO_ALL_PTS\n"
+    "     integer :: index_offset = GO_OFFSET_SW\n"
+    "  contains\n"
+    "    procedure, nopass :: code => compute_cu_code\n"
+    "  end type compute_cu\n"
+    "end program dummy\n")
+
+
+def test_kernelmetadatasymbol_create(fortran_reader):
+    '''Test we can create a KernelMetadataSymbol and capture the expected
+    values. We use the gocean1.0 API.
+
+    '''
+    kernel_psyir = fortran_reader.psyir_from_source(METADATA_GOCEAN)
+    kern_trans = KernTrans()
+    kern_trans.metadata_name = "compute_cu"
+    kern_trans.apply(kernel_psyir)
+    symbol = kernel_psyir.children[0].symbol_table.lookup("compute_cu")
+    assert isinstance(symbol, KernelMetadataSymbol)
+    assert symbol.iterates_over == "GO_ALL_PTS"
+    assert symbol.index_offset == "GO_OFFSET_SW"
+    assert symbol.code == "compute_cu_code"
+    assert isinstance(symbol.args, list)
+    assert len(symbol.args) == 3
+    assert isinstance(symbol.args[0], KernelMetadataSymbol.FieldArg)
+    assert symbol.args[0].access == "GO_WRITE"
+    assert symbol.args[0].stagger == "GO_CU"
+    assert symbol.args[0].form == "GO_POINTWISE"
+    assert symbol.args[0].stencil is None
+    assert isinstance(symbol.args[1], KernelMetadataSymbol.FieldArg)
+    assert symbol.args[1].access == "GO_READ"
+    assert symbol.args[1].stagger == "GO_CT"
+    assert symbol.args[1].form == "GO_STENCIL"
+    assert isinstance(symbol.args[1].stencil, list)
+    assert symbol.args[1].stencil == ["000", "011", "000"]
+    assert isinstance(symbol.args[2], KernelMetadataSymbol.GridArg)
+    assert symbol.args[2].access == "GO_READ"
+    assert symbol.args[2].name == "GO_GRID_AREA_T"
