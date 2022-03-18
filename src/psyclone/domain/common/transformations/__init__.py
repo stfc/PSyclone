5fd53981139903a0a540a62e277e879cd6dfc33e
Andrew Porter <andrew.porter@stfc.ac.uk>
2022-03-18 10:07:48 +0000

    #1666 more renaming

--- src/psyclone/domain/common/transformations/__init__.py
+++ src/psyclone/domain/common/transformations/__init__.py
@@ -31,10 +31,8 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 # -----------------------------------------------------------------------------
-from psyclone.domain.common.transformations.invokecall_trans import \
-    InvokeCallTrans
+from psyclone.domain.common.transformations.raise_call_2_invoke_trans import \
+    RaiseCall2InvokeTrans
 from psyclone.domain.common.transformations.alg_invoke_2_psy_call_trans \
     import AlgInvoke2PSyCallTrans
 from psyclone.domain.common.transformations.alg_trans import AlgTrans
-
-__all__ = ["InvokeCallTrans", "AlgTrans"]
