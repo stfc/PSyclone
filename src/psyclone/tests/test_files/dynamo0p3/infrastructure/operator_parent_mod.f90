af42ab20bab505725e0ff27a62cb394497373d46
TeranIvy <TeranIvy@users.noreply.github.com>
2022-06-01 21:26:25 +0100

    I #1738: Updated Copyrights

--- src/psyclone/tests/test_files/dynamo0p3/infrastructure/operator_parent_mod.f90
+++ src/psyclone/tests/test_files/dynamo0p3/infrastructure/operator_parent_mod.f90
@@ -8,7 +8,7 @@
 !-------------------------------------------------------------------------------
 ! BSD 3-Clause License
 !
-! Copyright (c) 2022, Science and Technology Facilities Council
+! Modifications copyright (c) 2022, Science and Technology Facilities Council
 ! All rights reserved.
 !
 ! Redistribution and use in source and binary forms, with or without
