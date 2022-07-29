3450767f3d587df0f43d72366b3e6a5f009d2335
Nuno Miguel Nobre <nuno.nobre@stfc.ac.uk>
2022-07-30 00:55:11 +0100

    #1554 Add comments explaining the update directive placing algorithm

--- src/psyclone/psyir/transformations/acc_update_trans.py
+++ src/psyclone/psyir/transformations/acc_update_trans.py
@@ -57,7 +57,7 @@ class ACCUpdateTrans(Transformation):
         # Perform some set-up required by the recursive routine.
         self._dep_tools = DependencyTools()
         self._acc_regions = (ACCParallelDirective, ACCKernelsDirective)
-        # Assume CodeBlocks do not call routines executed on the GPU
+        # Assume CodeBlocks do not call routines executed on the device.
         self._brk_nodes = (Call, )
 
         super().__init__()
@@ -116,16 +116,16 @@ class ACCUpdateTrans(Transformation):
     def _add_updates_to_schedule(self, sched):
         '''
         Recursively identify those statements that are not being executed
-        on the GPU and add any required OpenACC update directives.
+        on the device and add any required OpenACC update directives.
 
         :param sched: the schedule of statements to process.
         :type sched: :py:class:`psyclone.psyir.nodes.Schedule`
 
         '''
         # We must walk through the Schedule and find those nodes representing
-        # contiguous regions of code that are not executed on the GPU. Any
+        # contiguous regions of code that are not executed on the device. Any
         # Call nodes are taken as boundaries of such regions
-        # because it may be that their bodies are executed on the GPU.
+        # because it may be that their bodies are executed on the device.
         node_list = []
         for child in sched.children[:]:
             if isinstance(child, ACCEnterDataDirective):
@@ -148,9 +148,9 @@ class ACCUpdateTrans(Transformation):
                     if child.else_body:
                         self._add_updates_to_schedule(child.else_body)
                 elif isinstance(child, Loop):
-                    # A loop on the CPU with code that may be on the GPU.
+                    # A loop on the host with code that may be on the device.
                     # The loop start, stop and step values could all
-                    # potentially have been previously written to on the GPU.
+                    # potentially have been written to on the device.
                     self._add_update_directives([child.start_expr,
                                                  child.stop_expr,
                                                  child.step_expr])
@@ -158,7 +158,7 @@ class ACCUpdateTrans(Transformation):
                     self._add_updates_to_schedule(child.loop_body)
 
         # We've reached the end of the list of children - are there any
-        # last nodes that represent computation on the CPU?
+        # last nodes that represent computation on the host?
         self._add_update_directives(node_list)
 
     def _add_update_directives(self, node_list):
@@ -169,7 +169,7 @@ class ACCUpdateTrans(Transformation):
         :param sched: the schedule which contains the nodes in node_list.
         :type sched: :py:class:`psyclone.psyir.nodes.Schedule`
         :param node_list: the PSyIR nodes representing code executed on the \
-                          CPU rather than the GPU.
+                          host rather than the device.
         :type node_list: list of :py:class:`psyclone.psyir.nodes.Node`
 
         '''
@@ -179,12 +179,17 @@ class ACCUpdateTrans(Transformation):
         inputs, outputs = self._dep_tools.get_in_out_parameters(node_list)
         inputs, outputs = set(inputs), set(outputs)
 
-        # Workaround for lack of precise access descriptions, e.g. in the
-        # presence of array slices we currently overapproximate dependencies.
+        # As a workaround for the lack of precise access descriptions, we
+        # currently overapproximate dependencies by adding any variables being
+        # written to the input set (in addition to the output set). The aim is
+        # to avoid situations where the host writes an array slice and a later
+        # update device directive would then incorrectly overwrite the entire
+        # array on the device, including the data outside said slice.
         inputs.update(outputs)
 
-        # Workaround for CodeBlocks
-        # TODO We are probably relying on undefined behaviour here.
+        # For CodeBlock nodes, until we have semantic information on the
+        # statements involved, we conservatively assume every referenced
+        # variable is both an input and an output.
         for node in node_list:
             if isinstance(node, CodeBlock):
                 for symbol_name in node.get_symbol_names():
@@ -194,9 +199,7 @@ class ACCUpdateTrans(Transformation):
         # Copy any data that is read by this region to the host (resp. device)
         # if it is on the device (resp. host).
         IN, OUT = 0, 1
-        for idx, cpu_sig in enumerate((inputs, outputs)):
-            if not cpu_sig:
-                continue
+        for idx, host_sig in enumerate((inputs, outputs)):
             if idx == IN:     # inputs
                 node_index = 0
                 node_offset = 0
@@ -208,15 +211,30 @@ class ACCUpdateTrans(Transformation):
                 direction = "device"
                 tentative_update_pos = "end"
 
-            # Since the supplied nodes may be the children of an IfBlock or
-            # Loop, we have to search up to find the ancestor that *is* a
-            # child of a Schedule.
+            # Since the supplied nodes may be the children of an IfBlock or a
+            # Loop, we get the deepest ancestor that is a child of a Schedule.
             child = node_list[node_index]
             sched = child.parent
             while not isinstance(sched, Schedule):
                 child, sched = child.parent, sched.parent
 
-            while cpu_sig:
+            # While there are variables from this host region which have not
+            # been place in an update directive, we check if they need to be
+            # placed at the current schedule, in which case they are, or
+            # if we can bring them up to the next deepest ancestor schedule.
+            # The main goal is to avoid placing update directives inside loop
+            # statements, but the algorithm also tries to minimise the total
+            # number of update directives by promoting the coalescence of
+            # neighbouring directives.
+            while host_sig:
+                # Most conservative position for a new update directive at this
+                # schedule, i.e. that closest to the host region. This is used
+                # as a boundary to compute textual and loop-carried
+                # dependencies. It is also the position for the update
+                # directive used by those variables with textual dependencies.
+                # Perhaps, eventually, specially if we adopt the async clause,
+                # it may be benefecial to move up update host directives within
+                # the schedule as much as possible.
                 update_pos = sched.children.index(child) + node_offset
 
                 if idx == IN:
@@ -224,36 +242,48 @@ class ACCUpdateTrans(Transformation):
                 elif idx == OUT:
                     beg, end = update_pos, None
 
-                text_dep_stmts = sched.children[beg:end]
-                text_sig, text_sync = set(), set()
+                # The (in)ability to place variables in update directives in
+                # outer schedules is determined by the (in)existence of textual
+                # and loop-carried dependencies in the current schedule.
+                text_dep_stmts, text_sync = sched.children[beg:end], set()
+                loop_dep_stmts, loop_sync = sched.children[end:beg], set()
 
-                loop_dep_stmts = sched.children[end:beg]
-                loop_sig, loop_sync = set(), set()
-
-                for dep_stmts, sig, sync in [
-                     (text_dep_stmts, text_sig, text_sync),
-                     (loop_dep_stmts, loop_sig, loop_sync)]:
+                for dep_stmts, sync in [(text_dep_stmts, text_sync),
+                                        (loop_dep_stmts, loop_sync)]:
+                    kern_sig = set()
                     for stmt in dep_stmts:
                         for acc in stmt.walk(self._acc_regions):
-                            # Workaround for lack of precise access
-                            # descriptions, used trivial condition as reminder.
-                            if idx == IN or idx == OUT:
-                                sig.update(acc.out_kernel_references)
+                            # Kernel outputs are potential both input and
+                            # output dependencies. The latter is because we
+                            # must guarantee any kernel write is not
+                            # overwritten by an earlier host write.
+                            kern_sig.update(acc.out_kernel_references)
                             if idx == OUT:
-                                sig.update(acc.in_kernel_references)
-                    # If there are data dependencies or there is a statement
-                    # (e.g a call) that requires synchronisation.
-                    sync.update(cpu_sig.intersection(sig))
+                                kern_sig.update(acc.in_kernel_references)
+                    # Amongst the variables touched in the host region, find
+                    # those that need synchronisation at this schedule.
+                    sync.update(host_sig.intersection(kern_sig))
+                    # If there is a statement (e.g. a call) in the current
+                    # schedule that may launch device kernels touching data
+                    # which is also touched in this host region, we
+                    # conservatively insert update directives in this schedule
+                    # for all the variables involved regardless of direction.
                     if any(stmt.walk(self._brk_nodes) for stmt in dep_stmts):
-                        sync.update(cpu_sig)
+                        sync.update(host_sig)
 
+                # Those variables that require both textual and, if within a
+                # loop, also loop-carried synchronisation, need only be updated
+                # once and can be dropped from the latter set.
                 loop_sync.difference_update(text_sync)
 
                 # If textual synchronisation is required, those variables need
                 # to be updated straight away.
                 self._place_update(sched, update_pos, text_sync, direction)
-                cpu_sig.difference_update(text_sync)
+                host_sig.difference_update(text_sync)
 
+                # All the remaining variables that need synchronisation at this
+                # schedule can placed in an update directive at the edge
+                # of the schedule, promoting coalescence with other directives.
                 update_pos = tentative_update_pos
 
                 # If within loop body, we must cover loop-carried dependencies.
@@ -262,25 +292,28 @@ class ACCUpdateTrans(Transformation):
                 # always exists as all outputs are inputs - has already been
                 # moved out as well. This happens iff there's no textually
                 # preceding kernel within the if stmt which writes that
-                # variable which would appear in loop_sync as part of the
-                # inputs of all textually preceding kernels.
+                # variable. Since that variable would appear in loop_sync as
+                # part of the outputs of all textually preceding kernels, we
+                # may simply place an update device directive inside the if
+                # statement for those variables in loop_sync.
                 if isinstance(sched.parent, Loop) or \
                    isinstance(sched.parent, IfBlock) and idx == OUT:
                     self._place_update(sched, update_pos, loop_sync, direction)
-                    cpu_sig.difference_update(loop_sync)
+                    host_sig.difference_update(loop_sync)
 
                 # This schedule is the body of a routine and, at least until we
                 # can do interprocedural analysis, this is the end of the road.
                 if isinstance(sched, InvokeSchedule):
-                    self._place_update(sched, update_pos, cpu_sig, direction)
+                    self._place_update(sched, update_pos, host_sig, direction)
                     break
 
+                # Move up the code tree to the next deepest ancestor schedule.
                 while True:
                     child, sched = child.parent, sched.parent
                     if isinstance(sched, Schedule):
                         break
 
-    def _place_update(self, sched, update_pos, cpu_sig, direction):
+    def _place_update(self, sched, update_pos, host_sig, direction):
         '''
         Places, avoiding redundancy where possible, an update directive in the
         provided schedule, at the requested position, for the requested
@@ -289,9 +322,9 @@ class ACCUpdateTrans(Transformation):
         :param sched: the schedule in which to add the directive.
         :type sched: :py:class:`psyclone.psyir.nodes.Schedule`
         :param int update_pos: where to place the directive in the schedule.
-        :param cpu_sig: the access signature(s) that need to be synchronised \
-                        with the accelerator.
-        :type cpu_sig: Set[:py:class:`psyclone.core.Signature`]
+        :param host_sig: the access signature(s) that need to be synchronised \
+                         with the accelerator.
+        :type host_sig: Set[:py:class:`psyclone.core.Signature`]
         :param str direction: the direction of the synchronisation.
 
         '''
@@ -301,27 +334,34 @@ class ACCUpdateTrans(Transformation):
             from psyclone.nemo import NemoACCUpdateDirective as \
                 ACCUpdateDirective
 
-        cpu_sig = cpu_sig.copy()
+        # Avoid rewriting the set of signatures on the caller.
+        host_sig = host_sig.copy()
 
+        # For when the update directive can be placed at the edge of the
+        # schedule, promoting coalescence with existing update directives.
         if update_pos == "beg":
             update_pos = 0
         elif update_pos == "end":
             update_pos = len(sched.children)
 
+        # Check preceding and succeeding schedule positions for existing update
+        # directives. If at the beginning or at the end of the schedule, we may
+        # go out of bounds, thus the try statement.
         for update_offset in (-1, 0):
             try:
                 neighbour_node = sched.children[update_pos + update_offset]
                 if isinstance(neighbour_node, ACCUpdateDirective):
                     # Merge neighbour update directives in the same direction.
                     if neighbour_node.direction == direction:
-                        neighbour_node.sig_set.update(cpu_sig)
-                        cpu_sig.clear()
+                        neighbour_node.sig_set.update(host_sig)
+                        host_sig.clear()
                     # Avoid updating the host just after updating the device.
                     else:
-                        cpu_sig.difference_update(neighbour_node.sig_set)
+                        host_sig.difference_update(neighbour_node.sig_set)
             except IndexError:
                 pass
 
-        if cpu_sig:
-            update_dir = ACCUpdateDirective(cpu_sig, direction)
+        # Add the update directive to the schedule.
+        if host_sig:
+            update_dir = ACCUpdateDirective(host_sig, direction)
             sched.addchild(update_dir, update_pos)
