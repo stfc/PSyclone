df0e528d421f5d4c2741db87fc92d223acc11a42
Sergi Siso <sergi.siso@stfc.ac.uk>
2022-09-05 20:45:08 +0100

    #1783 Initial Nemo OpenMP for GPU script

---
+++ examples/nemo/scripts/omp_gpu_trans.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# -----------------------------------------------------------------------------
+# BSD 3-Clause License
+#
+# Copyright (c) 2021-2022, Science and Technology Facilities Council.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# -----------------------------------------------------------------------------
+# Authors: S. Siso, STFC Daresbury Lab
+
+''' PSyclone transformation script showing the introduction of OpenMP for GPU
+directives into Nemo code. '''
+
+from psyclone.psyGen import TransInfo
+from utils import insert_explicit_loop_parallelism, normalise_loops, \
+    enhance_tree_information
+
+
+def trans(psy):
+    ''' Add OpenMP Target and Loop directives to all loops, including the
+    implicit ones, to parallelise the code and execute it in an acceleration
+    device.
+
+    :param psy: the PSy object which this script will transform.
+    :type psy: :py:class:`psyclone.psyGen.PSy`
+    :returns: the transformed PSy object.
+    :rtype: :py:class:`psyclone.psyGen.PSy`
+
+    '''
+    omp_target_trans = TransInfo().get_trans_name('OMPTargetTrans')
+    omp_loop_trans = TransInfo().get_trans_name('OMPLoopTrans')
+    # Disabling worksharing will produce the 'loop' directive which is better
+    # suited to map the work into the GPU
+    omp_loop_trans.omp_worksharing = False
+
+    print("Invokes found:")
+    for invoke in psy.invokes.invoke_list:
+        print(invoke.name)
+
+        if invoke.name in ("tra_zdf", "iscpl_rst_interpol", "dom_ngb"):
+            print("Skipping", invoke.name)
+            continue
+
+        if invoke.name in ("obs_surf_alloc", "copy_obfbdata"):
+            print("Skipping", invoke.name)
+            continue
+
+        enhance_tree_information(invoke.schedule)
+
+        normalise_loops(
+                invoke.schedule,
+                unroll_array_ranges=True,
+                hoist_expressions=True,
+        )
+
+        insert_explicit_loop_parallelism(
+                invoke.schedule,
+                region_directive_trans=omp_target_trans,
+                loop_directive_trans=omp_loop_trans,
+                collapse=True
+        )
+
+    return psy
---
+++ examples/nemo/scripts/utils.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+# -----------------------------------------------------------------------------
+# BSD 3-Clause License
+#
+# Copyright (c) 2022, Science and Technology Facilities Council.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# -----------------------------------------------------------------------------
+# Authors: S. Siso, STFC Daresbury Lab
+
+''' Utilities file to parallelise Nemo code. '''
+
+from psyclone.psyir.nodes import Loop, Assignment, Directive, Container, \
+    Reference
+from psyclone.psyir.symbols import DataSymbol, INTEGER_TYPE, REAL_TYPE
+from psyclone.psyir.transformations import HoistLoopBoundExprTrans, HoistTrans
+from psyclone.domain.nemo.transformations import NemoAllArrayRange2LoopTrans
+from psyclone.transformations import TransformationError
+
+
+def enhance_tree_information(schedule):
+    mod_sym_tab = schedule.ancestor(Container).symbol_table
+
+    if "oce" in mod_sym_tab:
+        oce_symbol = mod_sym_tab.lookup("oce")
+        mod_sym_tab.resolve_imports(container_symbols=[oce_symbol])
+
+    if "par_oce" in mod_sym_tab:
+        par_oce_symbol = mod_sym_tab.lookup("par_oce")
+        mod_sym_tab.resolve_imports(container_symbols=[par_oce_symbol])
+
+    if "dom_oce" in mod_sym_tab:
+        dom_oce_symbol = mod_sym_tab.lookup("dom_oce")
+        mod_sym_tab.resolve_imports(container_symbols=[dom_oce_symbol])
+
+    if "phycst" in mod_sym_tab:
+        phycst_symbol = mod_sym_tab.lookup("phycst")
+        mod_sym_tab.resolve_imports(container_symbols=[phycst_symbol])
+
+    #if invoke.name == "nonosc":
+    #    tmask_symbol = schedule.symbol_table.lookup("tmask")
+    #    zbup_symbol = schedule.symbol_table.lookup("zbup")
+    #    tmask_symbol.datatype = zbup_symbol.datatype
+
+    for reference in schedule.walk(Reference):
+        if reference.symbol.name in ('jpi', 'jpim1', 'jpj', 'jpjm1', 'jp_tem', 'jp_sal', 'jpkm1'):
+            if not isinstance(reference.symbol, DataSymbol):
+                reference.symbol.specialise(DataSymbol, datatype=INTEGER_TYPE)
+        if reference.symbol.name in ('rn_avt_rnf'):
+            if not isinstance(reference.symbol, DataSymbol):
+                reference.symbol.specialise(DataSymbol, datatype=REAL_TYPE)
+
+def normalise_loops(
+        schedule,
+        unroll_array_ranges: bool = True,
+        hoist_expressions: bool = True,
+        ):
+    ''' Normalise all loops in the given schedule so that they are in an
+    appropriate form for the Parallelisation transformations to analyse
+    them.
+
+    :param schedule: the PSyIR Schedule to transform.
+    :param unroll_array_ranges: whether to convert ranges to explicit loops.
+    :param hoist_expressions: whether to hoist bounds and loop invariant \
+        statements out of the loop nest.
+    '''
+    if unroll_array_ranges:
+        # Convert all array implicit loops to explicit loops
+        explicit_loops = NemoAllArrayRange2LoopTrans()
+        for assignment in schedule.walk(Assignment):
+            explicit_loops.apply(assignment)
+
+    if hoist_expressions:
+        # First hoist all possible expressions
+        for loop in schedule.walk(Loop):
+            HoistLoopBoundExprTrans().apply(loop)
+
+        # Hoist all possible assignments (in reverse order so the inner loop
+        # constants are hoisted all the way out if possible)
+        for loop in reversed(schedule.walk(Loop)):
+            for statement in list(loop.loop_body):
+                try:
+                    HoistTrans().apply(statement)
+                except TransformationError:
+                    pass
+
+
+def insert_explicit_loop_parallelism(
+        schedule,
+        region_directive_trans=None,
+        loop_directive_trans=None,
+        collapse: bool = True
+        ):
+    ''' For each loop in the schedule that doesn't already have a Directive
+    as an ancestor, attempt to insert the given region and loop directives.
+
+    :param region_directive_trans: PSyclone transformation to insert the \
+        region directive.
+    :param loop_directive_trans: PSyclone transformation to use to insert the \
+        loop directive.
+    :param collapse: whether to attempt to insert the collapse clause to as \
+        many nested loops as possible.
+    '''
+
+    # Add the parallel directives in each loop
+    for loop in schedule.walk(Loop):
+        if loop.ancestor(Directive):
+            continue  # Skip if an outer loop is already parallelised
+
+        try:
+            loop_directive_trans.apply(loop)
+            # Only add the region directive if the loop was successfully
+            # parallelised.
+            region_directive_trans.apply(loop.parent.parent)
+        except TransformationError as err:
+            # This loop can not be transformed, proceed to next loop
+            print("Loop not parallelised because:", str(err))
+            continue
+
+        if collapse:
+            # Count the number of perfectly nested loops
+            num_nested_loops = 0
+            next_loop = loop
+            while isinstance(next_loop, Loop):
+                num_nested_loops += 1
+                if len(next_loop.loop_body.children) > 1:
+                    break
+                next_loop = next_loop.loop_body.children[0]
+
+            if num_nested_loops > 1:
+                loop.parent.parent.collapse = num_nested_loops
