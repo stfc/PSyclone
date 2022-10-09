06f58125dcfc5ff03f610fc81dd6c19e917162ba
Joerg Henrichs <henrichsjoerg@gmail.com>
2022-10-10 10:51:58 +1100

    #1883 Moved all new KernCallArgList tests into a test with the appropriate name.

---
+++ src/psyclone/tests/domain/lfric/kern_call_arg_list_test.py
@@ -0,0 +1,138 @@
+# -----------------------------------------------------------------------------
+# BSD 3-Clause License
+#
+# Copyright (c) 2017-2021, Science and Technology Facilities Council.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# -----------------------------------------------------------------------------
+# Authors R. W. Ford and A. R. Porter, STFC Daresbury Lab
+# Modified I. Kavcic, Met Office
+# Modified J. Henrichs, Bureau of Meteorology
+
+''' This module tests the LFric classes based on ArgOrdering.'''
+
+import os
+
+from psyclone.domain.lfric import (KernCallArgList)
+from psyclone.parse.algorithm import parse
+from psyclone.psyGen import PSyFactory
+from psyclone.psyir.nodes import Reference
+from psyclone.tests.lfric_build import LFRicBuild
+from psyclone.tests.utilities import get_base_path, get_invoke
+
+TEST_API = "dynamo0.3"
+
+
+def check_psyir_results(create_arg_list, fortran_writer):
+    '''Helper function to check if the PSyIR representation of the arguments
+     is identical to the old style textual representation. It checks that each
+     member of the psyir_arglist is a Reference, and that the textural
+     representation matches the textual presentation (which was already
+     verified).
+
+     '''
+    # Check the PSyIR representation
+    result = []
+    for node in create_arg_list.psyir_arglist:
+        assert isinstance(node, Reference)
+        result.append(fortran_writer(node))
+
+    assert result == create_arg_list._arglist
+
+
+def test_field_prolong(dist_mem, fortran_writer):
+    ''' Check that we generate correct psy-layer code for an invoke
+    containing a kernel that performs a prolongation operation '''
+
+    full_path = os.path.join(get_base_path(TEST_API),
+                             "22.0_intergrid_prolong.f90")
+
+    _, invoke_info = parse(full_path, api=TEST_API)
+    psy = PSyFactory(TEST_API,
+                     distributed_memory=dist_mem).create(invoke_info)
+    schedule = psy.invokes.invoke_list[0].schedule
+    kernel = schedule.kernels()[0]
+
+    create_arg_list = KernCallArgList(kernel)
+    create_arg_list.generate()
+    assert create_arg_list._arglist == [
+        'nlayers', 'cell_map_field2(:,:,cell)', 'ncpc_field1_field2_x',
+        'ncpc_field1_field2_y', 'ncell_field1', 'field1_proxy%data',
+        'field2_proxy%data', 'ndf_w1', 'undf_w1', 'map_w1', 'undf_w2',
+        'map_w2(:,cell)']
+
+    check_psyir_results(create_arg_list, fortran_writer)
+
+
+#def test_kerncallarglist_2qr_shapes(dist_mem, tmpdir):
+def test_kerncallarglist_2qr_shapes(tmpdir, fortran_writer):
+    ''' Check the handling of basis, diff_basis, and quad_rule '''
+
+    dist_mem = False
+    psy, _ = get_invoke("1.1.9_single_invoke_2qr_shapes_int_field.f90",
+                        TEST_API, dist_mem=dist_mem, idx=0)
+
+    assert LFRicBuild(tmpdir).code_compiles(psy)
+
+    schedule = psy.invokes.invoke_list[0].schedule
+    create_arg_list = KernCallArgList(schedule.kernels()[0])
+
+    create_arg_list.generate()
+    assert create_arg_list._arglist == [
+        'nlayers', 'f1_proxy%data', 'f2_proxy(1)%data', 'f2_proxy(2)%data',
+        'f2_proxy(3)%data', 'f3_proxy%data', 'istp', 'ndf_w2', 'undf_w2',
+        'map_w2(:,cell)', 'basis_w2_qr_xyoz', 'basis_w2_qr_face', 'ndf_wchi',
+        'undf_wchi', 'map_wchi(:,cell)', 'diff_basis_wchi_qr_xyoz',
+        'diff_basis_wchi_qr_face', 'ndf_adspc1_f3', 'undf_adspc1_f3',
+        'map_adspc1_f3(:,cell)', 'basis_adspc1_f3_qr_xyoz',
+        'basis_adspc1_f3_qr_face', 'diff_basis_adspc1_f3_qr_xyoz',
+        'diff_basis_adspc1_f3_qr_face', 'np_xy_qr_xyoz', 'np_z_qr_xyoz',
+        'weights_xy_qr_xyoz', 'weights_z_qr_xyoz', 'nfaces_qr_face',
+        'np_xyz_qr_face', 'weights_xyz_qr_face']
+
+    result = []
+    for node in create_arg_list.psyir_arglist:
+        assert isinstance(node, Reference)
+        result.append(fortran_writer(node))
+    print("OLD", psy.gen)
+    print(result)
+    print("NEW", fortran_writer(schedule))
+    assert result == [
+        'nlayers', 'f1_proxy%data', 'f2_proxy(1)%data', 'f2_proxy(2)%data',
+        'f2_proxy(3)%data', 'f3_proxy%data', 'istp', 'ndf_w2', 'undf_w2',
+        'map_w2(:,cell)', 'basis_w2_qr_xyoz', 'basis_w2_qr_face', 'ndf_wchi',
+        'undf_wchi', 'map_wchi(:,cell)', 'diff_basis_wchi_qr_xyoz',
+        'diff_basis_wchi_qr_face', 'ndf_adspc1_f3', 'undf_adspc1_f3',
+        'map_adspc1_f3(:,cell)', 'basis_adspc1_f3_qr_xyoz',
+        'basis_adspc1_f3_qr_face', 'diff_basis_adspc1_f3_qr_xyoz',
+        'diff_basis_adspc1_f3_qr_face']
+
+    # assert result == create_arg_list._arglist
+
+    # check_psyir_results(create_arg_list, fortran_writer)
