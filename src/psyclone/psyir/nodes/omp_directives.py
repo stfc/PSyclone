d6cf1c05453dcf2f13bc4252c391d774433bfbdd
Aidan Chalk <3043914+LonelyCat124@users.noreply.github.com>
2023-01-10 15:31:23 +0000

    Fixed issue with no continuing on a simple Reference

--- src/psyclone/psyir/nodes/omp_directives.py
+++ src/psyclone/psyir/nodes/omp_directives.py
@@ -703,6 +703,8 @@ class OMPSerialDirective(OMPRegionDirective, metaclass=abc.ABCMeta):
                 # If we have (exactly) Reference objects we filter out
                 # non-matching ones with the symbol check, and matching ones
                 # are always valid since they are simple accesses.
+                if isinstance(mem[0], Reference):
+                    continue
 
                 # If we have a StructureReference with no ArrayMixin children
                 # then we can skip it, as it will be done
@@ -756,6 +758,8 @@ class OMPSerialDirective(OMPRegionDirective, metaclass=abc.ABCMeta):
                 # If we have (exactly) Reference objects we filter out
                 # non-matching ones with the symbol check, and matching ones
                 # are always valid since they are simple accesses.
+                if isinstance(mem[0], Reference):
+                    continue
 
                 # If we have a StructureReference with no ArrayMixin children
                 # then we can skip it, as it will be done
