16c830f674127596dc49153cdf441b43066e1a58
Sergi Siso <sergiesg@gmail.com>
2023-02-17 16:26:26 +0000

    #598 Add test of a OMPLoopTrans that needs a firstprivate clause

--- src/psyclone/tests/psyir/transformations/transformations_test.py
+++ src/psyclone/tests/psyir/transformations/transformations_test.py
@@ -444,6 +444,46 @@ def test_parallellooptrans_validate_dependencies(fortran_reader):
     omplooptrans.validate(loops[0])
 
 
+@pytest.mark.xfail(reason="#598 Firstprivate clauses still missing")
+def test_omplooptrans_apply_firstprivate(fortran_reader, fortran_writer):
+
+    psyir = fortran_reader.psyir_from_source(f'''
+        subroutine my_subroutine()
+            integer :: ji, jj, jk, jpkm1, jpjm1, jpim1, scalar1, scalar2
+            real, dimension(10, 10, 10) :: zwt, zwd, zwi, zws
+            do jk = 2, jpkm1, 1
+              do jj = 2, jpjm1, 1
+                do ji = 2, jpim1, 1
+                   if (.true.) then
+                      scalar1 = zwt(ji,jj,jk)
+                   endif
+                   scalar2 = scalar1 + zwt(ji,jj,jk)
+                enddo
+              enddo
+            enddo
+        end subroutine''')
+    omplooptrans = OMPParallelLoopTrans()
+    loop = psyir.walk(Loop)[0]
+    omplooptrans.apply(loop)
+    expected = '''\
+!$omp parallel do default(shared), private(ji,jj,jk,scalar2), \
+firstprivate(scalar1), schedule(auto)
+do jk = 2, jpkm1, 1
+  do jj = 2, jpjm1, 1
+    do ji = 2, jpim1, 1
+      if (.true.) then
+        scalar1 = zwt(ji,jj,jk)
+      end if
+      scalar2 = scalar1 + zwt(ji,jj,jk)
+    enddo
+  enddo
+enddo
+!$omp end parallel do\n'''
+
+    gen = fortran_writer(psyir.children[0].children[0])
+    assert expected == gen
+
+
 def test_omplooptrans_apply(sample_psyir, fortran_writer):
     ''' Test OMPLoopTrans works as expected with the different options. '''
 
