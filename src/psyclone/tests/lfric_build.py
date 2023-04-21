a2e8a1b59cd4fa30ed1b98b4dd708ae573a7b0d1
Andrew Porter <andrew.porter@stfc.ac.uk>
2023-04-21 17:12:11 +0100

    #2117 ensure pytest build flags are passed through to LFRic infrastructure build

--- src/psyclone/tests/lfric_build.py
+++ src/psyclone/tests/lfric_build.py
@@ -110,7 +110,8 @@ class LFRicBuild(Compile):
             # files can be used by all test compilations later.
             LFRicBuild._compilation_path = str(self._tmpdir)
             makefile = os.path.join(self._infrastructure_path, "Makefile")
-            arg_list = [LFRicBuild._make_command, "-f", makefile]
+            arg_list = [LFRicBuild._make_command, f"F90={self._f90}",
+                        f"F90FLAGS={self._f90flags}", "-f", makefile]
             try:
                 with subprocess.Popen(arg_list, stdout=subprocess.PIPE,
                                       stderr=subprocess.STDOUT) as build:
