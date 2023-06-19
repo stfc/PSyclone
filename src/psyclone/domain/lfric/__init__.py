6285ad1a20cceea2dbfaef202d181cf39377c212
Lottie Turner <charlotte.turner@metoffice.gov.uk>
2023-06-19 15:50:26 +0100

    #1312 removing lfric_array_args and relevant tests as was getting ahead of myself

--- src/psyclone/domain/lfric/__init__.py
+++ src/psyclone/domain/lfric/__init__.py
@@ -63,7 +63,6 @@ from psyclone.domain.lfric.metadata_to_arguments_rules import \
 from psyclone.domain.lfric.arg_index_to_metadata_index import \
     ArgIndexToMetadataIndex
 from psyclone.domain.lfric.lfric_collection import LFRicCollection
-from psyclone.domain.lfric.lfric_array_args import LFRicArrayArgs
 
 
 __all__ = [
@@ -78,5 +77,4 @@ __all__ = [
     'LFRicConstants',
     'LFRicExtractDriverCreator',
     'LFRicInvoke',
-    'LFRicSymbolTable',
-    'LFRicArrayArgs']
+    'LFRicSymbolTable']
