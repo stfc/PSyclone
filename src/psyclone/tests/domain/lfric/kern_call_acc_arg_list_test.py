69224d5dc54d932af2c8379ca9bde78493d38f1c
Andrew Porter <andrew.porter@stfc.ac.uk>
2023-07-07 09:43:06 +0100

    #2189 extend tests to get coverage

--- src/psyclone/tests/domain/lfric/kern_call_acc_arg_list_test.py
+++ src/psyclone/tests/domain/lfric/kern_call_acc_arg_list_test.py
@@ -42,28 +42,21 @@ import os
 import pytest
 
 from psyclone.core import VariablesAccessInfo
-from psyclone.domain.lfric import (KernCallAccArgList, LFRicConstants,
-                                   LFRicSymbolTable, LFRicTypes)
-from psyclone.errors import GenerationError, InternalError
+from psyclone.domain.lfric import FunctionSpace, KernCallAccArgList
+from psyclone.errors import InternalError
 from psyclone.dynamo0p3 import DynKern
 from psyclone.parse.algorithm import parse
 from psyclone import psyGen
-from psyclone.psyir.nodes import Literal, Loop, Reference, UnaryOperation
-from psyclone.psyir.symbols import ArrayType, ScalarType
 from psyclone.tests.utilities import get_base_path, get_invoke
 from psyclone.transformations import ACCParallelTrans, ACCEnterDataTrans
 
 # constants
-BASE_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(
-    os.path.abspath(__file__)))),
-                         "test_files", "dynamo0p3")
 TEST_API = "dynamo0.3"
+BASE_PATH = get_base_path(TEST_API)
 
 
-def test_acc_arg_list_single_coarse_mesh(dist_mem, monkeypatch):
-    '''Tests that an exception is raised as expected if a kernel has more than
-    one coarse mesh.
-    '''
+def test_acc_arg_list_cell_map(dist_mem, monkeypatch):
+    '''Test the cell_map() method.'''
     # We need a DynKern in order to construct an instance of KernCallAccArgList
     _, invoke_info = parse(os.path.join(BASE_PATH,
                                         "22.0_intergrid_prolong.f90"),
@@ -73,6 +66,9 @@ def test_acc_arg_list_single_coarse_mesh(dist_mem, monkeypatch):
     schedule = psy.invokes.invoke_list[0].schedule
     kern = schedule.walk(DynKern)[0]
     arg_list = KernCallAccArgList(kern)
+    # Check that the cell_map method works as expected.
+    arg_list.cell_map()
+    assert "cell_map_field2" in arg_list._arglist
     # Rather than attempt to break the kernel itself, we monkeypatch the
     # args_filter routine so that it returns all args rather than just the
     # one requested.
@@ -81,6 +77,56 @@ def test_acc_arg_list_single_coarse_mesh(dist_mem, monkeypatch):
         arg_list.cell_map()
     assert ("An LFRic intergrid kernel should have only one coarse mesh but "
             "'prolong_test_kernel_code' has 2" in str(err.value))
+
+
+def test_stencil_2d():
+    '''Test for the stencil_2d() and stencil_2d_unknown_extent methods.'''
+    _, invoke = get_invoke("19.7_multiple_stencils.f90", TEST_API,
+                           name="invoke_0_testkern_stencil_multi_type",
+                           dist_mem=False)
+    sched = invoke.schedule
+    kern = sched.walk(DynKern)[0]
+    arg_list = KernCallAccArgList(kern)
+    arg_list.stencil_2d(kern.arguments._args[1])
+    # This should result in the whole stencil dofmap being added as an arg.
+    assert arg_list._arglist == ['f2_stencil_dofmap']
+    arg_list.stencil_2d_unknown_extent(kern.arguments._args[1])
+    # This should result in the stencil extent being added as an arg.
+    assert arg_list._arglist == ['f2_stencil_dofmap', 'f2_stencil_size']
+
+
+def test_fs_compulsory_field_no_cell_column():
+    '''Check that the fs_compulsory_field method does nothing if the kernel
+    does not iterate over cell columns.'''
+    _, invoke = get_invoke("25.0_domain.f90", TEST_API,
+                           name="invoke_0_testkern_domain_type",
+                           dist_mem=False)
+    sched = invoke.schedule
+    kern = sched.walk(DynKern)[0]
+    arg_list = KernCallAccArgList(kern)
+    fspace = FunctionSpace("w3", kern.arguments)
+    arg_list.fs_compulsory_field(fspace)
+    assert arg_list._arglist == []
+
+
+def test_fs_intergrid():
+    '''Test the fs_intergrid() method.'''
+    _, invoke = get_invoke("22.2_intergrid_3levels.f90", TEST_API,
+                           name="invoke_0", dist_mem=False)
+    sched = invoke.schedule
+    kernels = sched.walk(DynKern)
+    prolong_kern = kernels[0]
+    restrict_kern = kernels[2]
+    fspace = FunctionSpace("any_space_1", restrict_kern.arguments)
+    arg_list = KernCallAccArgList(restrict_kern)
+    arg_list.fs_intergrid(fspace)
+    # For the coarse mesh we need undf and the dofmap for the column.
+    assert arg_list._arglist == ['undf_aspc1_fld_m', 'map_aspc1_fld_m']
+    fspace = FunctionSpace("w1", prolong_kern.arguments)
+    arg_list = KernCallAccArgList(prolong_kern)
+    arg_list.fs_intergrid(fspace)
+    # For the fine mesh we just need the whole dofmap.
+    assert arg_list._arglist == ['map_w1']
 
 
 def test_lfric_acc():
