d05123fcf077b28faa8837fcfde4721cc871dd74
oakley.brunt <obrunt@vld419.cmpd1.metoffice.gov.uk>
2023-08-15 14:40:17 +0100

    #2263 repaired dynamo0p3.py imports of LFRicScalarArgs

--- src/psyclone/dynamo0p3.py
+++ src/psyclone/dynamo0p3.py
@@ -61,7 +61,7 @@ from psyclone.domain.lfric import (FunctionSpace, KernCallAccArgList,
                                    LFRicArgDescriptor, KernelInterface,
                                    LFRicCollection, LFRicConstants,
                                    LFRicSymbolTable, LFRicInvoke,
-                                   LFRicKernCallFactory)
+                                   LFRicKernCallFactory, LFRicScalarArgs)
 from psyclone.errors import GenerationError, InternalError, FieldNotFoundError
 from psyclone.f2pygen import (AllocateGen, AssignGen, CallGen, CommentGen,
                               DeallocateGen, DeclGen, DoGen, IfThenGen,
@@ -9584,7 +9584,6 @@ __all__ = [
     'LFRicFields',
     'DynProxies',
     'DynCellIterators',
-    'LFRicScalarArgs',
     'DynLMAOperators',
     'DynCMAOperators',
     'DynMeshes',
