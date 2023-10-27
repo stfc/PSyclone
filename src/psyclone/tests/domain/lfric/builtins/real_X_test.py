9c2268f760aa49895141f017f319d7b797e212db
TeranIvy <TeranIvy@users.noreply.github.com>
2023-10-27 02:33:23 +0100

    #1525: Updated real conversion built-in tests

--- src/psyclone/tests/domain/lfric/builtins/real_X_test.py
+++ src/psyclone/tests/domain/lfric/builtins/real_X_test.py
@@ -39,6 +39,7 @@
     (converting integer to real field elements).'''
 
 import os
+import re
 import pytest
 
 from psyclone.configuration import Config
@@ -130,7 +131,7 @@ def test_real_X(tmpdir, monkeypatch, annexed, dist_mem):
             "      ! Call our kernels\n"
             "      !\n"
             "      DO df=loop0_start,loop0_stop\n"
-            "        f2_data(df) = real(f1_data(df), r_def)\n"
+            "        f2_data(df) = REAL(f1_data(df), kind=r_def)\n"
             "      END DO\n"
             "      !\n"
             "    END SUBROUTINE invoke_0\n")
@@ -142,7 +143,7 @@ def test_real_X(tmpdir, monkeypatch, annexed, dist_mem):
             "      ! Call kernels and communication routines\n"
             "      !\n"
             "      DO df=loop0_start,loop0_stop\n"
-            "        f2_data(df) = real(f1_data(df), r_def)\n"
+            "        f2_data(df) = REAL(f1_data(df), kind=r_def)\n"
             "      END DO\n"
             "      !\n"
             "      ! Set halos dirty/clean for fields modified in the "
@@ -156,24 +157,41 @@ def test_real_X(tmpdir, monkeypatch, annexed, dist_mem):
 
 
 @pytest.mark.parametrize("kind_name", ["r_solver", "r_tran"])
-def test_real_X_precision(tmpdir, monkeypatch, kind_name):
+def test_real_X_precision(tmpdir, kind_name):
     '''Test that the built-in picks up and creates correct code for a
     scalar with precision that is not the default i.e. not
     'r_def'. Try with two examples to make sure it works in general.
 
     '''
-    _, invoke_info = parse(os.path.join(BASE_PATH,
-                                        "15.28.2_real_X_builtin.f90"),
-                           api=API)
+    with open(os.path.join(BASE_PATH, "15.28.2_real_X_builtin.f90"),
+              "r", encoding='utf-8') as alg_file:
+        alg_code = alg_file.read()
+
+    pattern = re.compile(r"\bfield_")
+    alg_code = re.sub(pattern, f"{kind_name}_field_", alg_code)
+
+    os.mkdir(str(tmpdir.join("tmp")))
+    tmp_fname = str(tmpdir.join("tmp", f"real_{kind_name}_X_builtin_alg.f90"))
+    with open(tmp_fname, "w", encoding='utf-8') as tmp_file:
+        tmp_file.write(alg_code)
+    tmp_file.close()
+
+    with open(tmp_fname, "r", encoding='utf-8') as alg_file:
+        _, invoke_info = parse(alg_file, api=API)
     psy = PSyFactory(API).create(invoke_info)
-    # Test string method
-    first_invoke = psy.invokes.invoke_list[0]
-    kern = first_invoke.schedule.children[0].loop_body[0]
-    monkeypatch.setattr(kern.args[0], "_precision", kind_name)
     code = str(psy.gen)
-    assert f"USE constants_mod, ONLY: {kind_name}, i_def" in code
-    assert f"f2_data(df) = real(f1_data(df), {kind_name})" in code
+
     # Test code generation
+    assert f"USE constants_mod, ONLY: {kind_name}, i_def" in code
+    assert (f"USE {kind_name}_field_mod, ONLY: {kind_name}_field_type, "
+            f"{kind_name}_field_proxy_type") in code
+    assert f"TYPE({kind_name}_field_type), intent(in) :: f2" in code
+    assert (f"REAL(KIND={kind_name}), pointer, dimension(:) :: "
+            "f2_data => null()") in code
+    assert f"TYPE({kind_name}_field_proxy_type) f2_proxy" in code
+    assert f"f2_data(df) = REAL(f1_data(df), kind={kind_name})" in code
+
+    # Test compilation
     assert LFRicBuild(tmpdir).code_compiles(psy)
 
 
@@ -189,9 +207,9 @@ def test_real_X_lowering(fortran_writer):
                      distributed_memory=False).create(invoke_info)
     first_invoke = psy.invokes.invoke_list[0]
     kern = first_invoke.schedule.children[0].loop_body[0]
-    #kern.lower_to_language_level()
-    #loop = first_invoke.schedule.walk(Loop)[0]
-    #code = fortran_writer(loop)
-    #assert ("do df = loop0_start, loop0_stop, 1\n"
-    #        "  asum = asum + f1_data(df)\n"
-    #        "enddo") in code
+    kern.lower_to_language_level()
+    loop = first_invoke.schedule.walk(Loop)[0]
+    code = fortran_writer(loop)
+    assert ("do df = loop0_start, loop0_stop, 1\n"
+            "  f2_data(df) = REAL(f1_data(df), kind=r_def)\n"
+            "enddo") in code
