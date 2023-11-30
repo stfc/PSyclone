7f35c457b067267168607178c7c401b8f8a3ba36
oakley.brunt <obrunt@vld419.cmpd1.metoffice.gov.uk>
2023-11-30 17:02:19 +0000

    #1786 Updated testing of real_to_real_X builtin

--- src/psyclone/tests/domain/lfric/builtins/real_to_real_x_test.py
+++ src/psyclone/tests/domain/lfric/builtins/real_to_real_x_test.py
@@ -37,6 +37,7 @@
     (converting real-valued field element of precision r_<prec> to 
     real-valued field elements of r_<prec>).'''
 
+from itertools import permutations
 import os
 import re
 import pytest
@@ -76,7 +77,7 @@ def test_real_to_real_x(tmpdir, monkeypatch, annexed, dist_mem):
     # Test with and without annexed DoFs
     monkeypatch.setattr(api_config, "_compute_annexed_dofs", annexed)
     _, invoke_info = parse(os.path.join(BASE_PATH,
-                                        "15.29.1_real_to_real_X_builtin.f90"),
+                                        "15.10.8_real_to_real_X_builtin.f90"),
                            api=API)
     psy = PSyFactory(API, distributed_memory=dist_mem).create(invoke_info)
 
@@ -89,16 +90,18 @@ def test_real_to_real_x(tmpdir, monkeypatch, annexed, dist_mem):
     # Test code generation
     code = str(psy.gen)
 
+    print(code)
+
     # Check that the correct field types and constants are used
     output = (
-        "    USE constants_mod, ONLY: r_def, i_def\n"
+        "    USE constants_mod, ONLY: r_tran, r_solver, r_def, i_def\n"
         "    USE field_mod, ONLY: field_type, field_proxy_type\n")
     assert output in code
 
     # Check built-in loop
     output = (
         "      DO df=loop0_start,loop0_stop\n"
-        "        f2_data(df) = REAL(f1_data(df), kind=r_def)\n"
+        "        f2_data(df) = REAL(f1_data(df), kind=r_tran)\n"
         "      END DO\n")
     assert output in code
 
@@ -116,64 +119,36 @@ def test_real_to_real_x(tmpdir, monkeypatch, annexed, dist_mem):
     # Test compilation of generated code
     assert LFRicBuild(tmpdir).code_compiles(psy)
 
-
-@pytest.mark.parametrize("kind_name", ["r_solver", "r_tran", "r_bl", "r_phys"])
-def test_real_to_real_x_precision(tmpdir, kind_name):
-    '''
-    Test that the built-in picks up and creates correct code for field
-    data with precision that is not the default, i.e. not 'r_def'. Try with
-    all supported LFRic real-valued field precisions to make sure all work.
-
-    '''
-    # Note that monkeypatching required to change field type, field proxy
-    # type and the field data precisions is extensive and complicated.
-    # Modifying the test algorithm is easier and more effective.
-    with open(os.path.join(BASE_PATH, "15.29.1_real_to_real_X_builtin.f90"),
-              "r", encoding='utf-8') as alg_file:
-        alg_code = alg_file.read()
-
-    # Modify the 'real'-valued field type and precision, and store the
-    # modified temporary algorithm
-    pattern = re.compile(r"\bfield_")
-    alg_code = re.sub(pattern, f"{kind_name}_field_", alg_code)
-    os.mkdir(str(tmpdir.join("tmp")))
-    tmp_fname = str(tmpdir.join("tmp", f"real_{kind_name}_X_builtin_alg.f90"))
-    with open(tmp_fname, "w", encoding='utf-8') as tmp_file:
-        tmp_file.write(alg_code)
-    tmp_file.close()
-
-    # Read and parse the modified algorithm
-    with open(tmp_fname, "r", encoding='utf-8') as alg_file:
-        _, invoke_info = parse(alg_file, api=API)
-    psy = PSyFactory(API).create(invoke_info)
-    code = str(psy.gen)
-
-    # Test code generation
-    assert f"USE constants_mod, ONLY: {kind_name}, i_def" in code
-    assert (f"REAL(KIND={kind_name}), pointer, dimension(:) :: "
-            "f2_data => null()") in code
-    assert f"f2_data(df) = REAL(f1_data(df), kind={kind_name})" in code
-
-
-def test_real_to_real_x_lowering(fortran_writer):
+@pytest.mark.parametrize("kind_name", ["r_bl", "r_phys", "r_um"])
+def test_real_to_real_x_lowering(monkeypatch, kind_name):
     '''
     Test that the lower_to_language_level() method works as expected.
 
     '''
     _, invoke_info = parse(os.path.join(BASE_PATH,
-                                        "15.29.1_real_to_real_X_builtin.f90"),
+                                        "15.10.8_real_to_real_X_builtin.f90"),
                            api=API)
     psy = PSyFactory(API,
                      distributed_memory=False).create(invoke_info)
     first_invoke = psy.invokes.invoke_list[0]
-    kern = first_invoke.schedule.children[0].loop_body[0]
-    parent = kern.parent
-    lowered = kern.lower_to_language_level()
-    assert parent.children[0] is lowered
-    assert isinstance(parent.children[0], Assignment)
+    table = first_invoke.schedule.symbol_table
+    arg = first_invoke.schedule.children[0].loop_body[0].args[0]
+    # Set 'f2_data' to another 'r_<prec>'
+    sym_kern = table.lookup_with_tag(f"{arg.name}:data")
+    monkeypatch.setattr(arg, "_precision", f"{kind_name}")
+    monkeypatch.setattr(sym_kern.datatype.partial_datatype.precision,
+                        "_name", f"{kind_name}")
 
-    loop = first_invoke.schedule.walk(Loop)[0]
-    code = fortran_writer(loop)
-    assert ("do df = loop0_start, loop0_stop, 1\n"
-            "  f2_data(df) = REAL(f1_data(df), kind=r_def)\n"
-            "enddo") in code
+    # Test limited code generation (no equivalent field type)
+    code = str(psy.gen)
+
+    if kind_name < 'r_def':
+        assert f"USE constants_mod, ONLY: r_solver, r_def, {kind_name}" in code
+    elif 'r_solver' > kind_name > 'r_def':
+        assert f"USE constants_mod, ONLY: r_solver, {kind_name}, r_def" in code
+    else:
+        assert f"USE constants_mod, ONLY: {kind_name}, r_solver, r_def" in code
+
+    assert (f"REAL(KIND={kind_name}), pointer, dimension(:) :: "
+            "f2_data => null()") in code
+    assert f"f2_data(df) = REAL(f1_data(df), kind={kind_name})" in code
--- src/psyclone/tests/test_files/dynamo0p3/15.10.8_real_to_real_X_builtin.f90
+++ src/psyclone/tests/test_files/dynamo0p3/15.10.8_real_to_real_X_builtin.f90
@@ -37,13 +37,18 @@ program single_invoke
 
   ! Description: single point-wise operation (conversion of real-valued to
   ! real-valued field elements) specified in an invoke call.
-  use field_mod,         only: field_type
+  use r_solver_field_mod,          only: r_solver_field_type
+  use r_tran_field_mod,           only: r_tran_field_type
+  use field_mod,                   only: field_type
 
   implicit none
 
-  type(field_type)         :: f2
-  type(field_type)         :: f1
+  type(field_type)               :: f1
+  type(r_tran_field_type)        :: f2
+  type(r_solver_field_type)      :: f3
 
-  call invoke( real_to_real_X(f2, f1) )
+  call invoke( real_to_real_X(f2, f1),   &
+                real_to_real_X(f1, f3),  &
+                real_to_real_X(f3, f2) )
 
 end program single_invoke
