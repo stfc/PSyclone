03b51beaa260fb048be4c8478ccfa9d159692e60
TeranIvy <TeranIvy@users.noreply.github.com>
2023-12-18 18:50:13 +0000

    #2273 Minor left-over edit

--- src/psyclone/domain/lfric/lfric_fields.py
+++ src/psyclone/domain/lfric/lfric_fields.py
@@ -172,5 +172,5 @@ class LFRicFields(LFRicCollection):
 
 # ---------- Documentation utils -------------------------------------------- #
 # The list of module members that we wish AutoAPI to generate
-# documentation for. (see [https://psyclone-ref.readthedocs.io])
+# documentation for (see [https://psyclone-ref.readthedocs.io]).
 __all__ = ['LFRicFields']
