40c8fa56fa60b940238454c7a7d05b8e196c7c1d
Joerg Henrichs <henrichsjoerg@gmail.com>
2024-02-21 10:10:22 +1100

    #1623 Remove the working code, added TODOs.

--- tutorial/training/gocean/2.6-GameOfLife-fuse/fuse_loops.py
+++ tutorial/training/gocean/2.6-GameOfLife-fuse/fuse_loops.py
@@ -1,7 +1,7 @@
 # -----------------------------------------------------------------------------
 # BSD 3-Clause License
 #
-# Copyright (c) 2021-2023, Science and Technology Facilities Council.
+# Copyright (c) 2021-2024, Science and Technology Facilities Council.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -70,16 +70,17 @@ def trans(psy):
     # do j do i die
     # do j do i combine
 
-    # First merge the first two j loops
-    fuse.apply(schedule[0], schedule[1])
+    # TODO: First merge the first two j loops
+    fuse.apply ...
+
     # do j do i count
     #      do i born
     # do j do i die
     # do j do i combine
 
-    # Then merge the (previous third, now second) loop to the
+    # TODO: Then merge the (previous third, now second) loop to the
     # fused loop
-    fuse.apply(schedule[0], schedule[1])
+    fuse.apply ...
     # do j do i count
     #      do i born
     #      do i die
@@ -87,14 +88,16 @@ def trans(psy):
 
     # You cannot fuse the two remaining outer loops!
 
-    # Fuse the three inner loops: first the first two
-    fuse.apply(schedule[0].loop_body[0], schedule[0].loop_body[1])
+    # TODO: Fuse the three inner loops: first the first two
+    # To access an inner loop you need to get the loop_body of the
+    # outer loop, e.g.: schedule[0].loop_body[0]
+    fuse.apply ...
     # do j do i count born
     #      do i die
     # do j do i combine
 
-    # Then merge in the previous third, now second) loop
-    fuse.apply(schedule[0].loop_body[0], schedule[0].loop_body[1])
+    # TODO: Then merge in the previous third, now second) inner loop
+    fuse.apply ...
     # do j do i count born die
     # do j do i combine
     print(schedule.view())
