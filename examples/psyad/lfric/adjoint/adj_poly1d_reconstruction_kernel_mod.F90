52e1141d72a3763d5ab25d8181864783c9cfca6d
Andrew Porter <andrew.porter@stfc.ac.uk>
2024-03-28 16:48:50 +0000

    #1719 add Christines corrected adjoint of poly1d kernel [skip ci]

--- examples/psyad/lfric/adjoint/adj_poly1d_reconstruction_kernel_mod.F90
+++ examples/psyad/lfric/adjoint/adj_poly1d_reconstruction_kernel_mod.F90
@@ -1,6 +1,8 @@
 module adj_poly1d_reconstruction_kernel_mod
-  use argument_mod, only : any_discontinuous_space_1, any_discontinuous_space_2, any_discontinuous_space_3, arg_type, cell_column, &
-&cross, func_type, gh_field, gh_integer, gh_read, gh_real, gh_scalar, gh_write, reference_element_data_type, stencil
+  use argument_mod, only : any_discontinuous_space_1, any_discontinuous_space_2, &
+       any_discontinuous_space_3, arg_type, cell_column, cross, func_type, gh_field, &
+       gh_integer, gh_read, gh_real, gh_scalar, gh_write, reference_element_data_type, &
+       stencil, gh_readwrite
   use constants_mod, only : i_def, r_tran
   use kernel_mod, only : kernel_type
   implicit none
@@ -8,16 +10,13 @@ module adj_poly1d_reconstruction_kernel_mod
   type(ARG_TYPE) :: META_ARGS(5) = (/ &
     arg_type(gh_field, gh_real, gh_read, any_discontinuous_space_1, stencil(cross)), &
     arg_type(gh_field, gh_real, gh_readwrite, any_discontinuous_space_2), &
-    arg_type(gh_field, gh_real, gh_read, any_discontinuous_space_3), &
+    arg_type(gh_field, gh_real, gh_read, any_discontinuous_space_3, stencil(cross)), &
     arg_type(gh_scalar, gh_integer, gh_read), &
     arg_type(gh_scalar, gh_integer, gh_read)/)
   INTEGER :: OPERATES_ON = cell_column
   CONTAINS
     PROCEDURE, NOPASS :: adj_poly1d_reconstruction_code
   END TYPE adj_poly1d_reconstruction_kernel_type
-!!$  integer, parameter :: i_def = kind(1)
-!!$  integer, parameter :: r_def = kind(1.0d0)
-!!$  integer, parameter :: r_tran = kind(1.0d0)
   
   private
 
@@ -25,7 +24,8 @@ module adj_poly1d_reconstruction_kernel_mod
 
   contains
     subroutine adj_poly1d_reconstruction_code( &
-         nlayers, reconstruction, stencil_size, stencil_map, tracer, coeff, ndata, order, &
+         nlayers, reconstruction, stencil_size, stencil_map, tracer, &
+         coeff, cstencil_size, cstencil_map, ndata, order, &
          ndf_md, undf_md, map_md, ndf_ws, undf_ws, map_ws, ndf_c, undf_c, map_c)
     integer(kind=i_def), parameter :: nfaces = 4
     integer(kind=i_def), intent(in) :: nlayers
@@ -40,24 +40,17 @@ module adj_poly1d_reconstruction_kernel_mod
     integer(kind=i_def), dimension(ndf_c), intent(in) :: map_c
     integer(kind=i_def), intent(in) :: ndata
     integer(kind=i_def), intent(in) :: order
-    integer(kind=i_def), intent(in) :: stencil_size
+    integer(kind=i_def), intent(in) :: stencil_size, cstencil_size
     real(kind=r_tran), dimension(undf_md), intent(in) :: reconstruction
     real(kind=r_tran), dimension(undf_ws), intent(inout) :: tracer
     real(kind=r_tran), dimension(undf_c), intent(in) :: coeff
     integer(kind=i_def), dimension(ndf_ws,stencil_size), intent(in) :: stencil_map
-    integer(kind=i_def) :: k
-    integer(kind=i_def) :: f
+    integer(kind=i_def), dimension(ndf_c,cstencil_size), intent(in) :: cstencil_map
+    integer(kind=i_def) :: f, face, face_mod, stencil_depth
     integer(kind=i_def) :: p
-    integer(kind=i_def) :: face
-    integer(kind=i_def) :: stencil
-    integer(kind=i_def) :: stencil_depth
-    integer(kind=i_def) :: depth
-    integer(kind=i_def) :: face_mod
-    integer(kind=i_def) :: ijp
-    integer(kind=i_def) :: df
+    integer(kind=i_def) :: stencil, depth
     integer(kind=i_def) :: nl
-    INTEGER(KIND = i_def), DIMENSION(order + 1, nfaces) :: map1d
-    integer :: idx
+    INTEGER(KIND = i_def), DIMENSION(order + 1, nfaces) :: map1d, adj_map1d
 
     nl = ndf_ws + nlayers - 2
     stencil_depth = order / 2
@@ -72,19 +65,49 @@ module adj_poly1d_reconstruction_kernel_mod
       enddo
     enddo
 
+    ! The key principle is that if the forward code is
+    !   a_i = c_i ^ {i + 1 } b_{i + 1}
+
+    ! then the adjoint code is
+    !   b_{i} = c_{i-1} ^ {i-1 + 1} a_{i-1}
+
+    ! In the forward we loop over faces and for face 1 we use c at the
+    ! centre-cell for the face +1 (the ijp addition) and b for the
+    ! next-cell for face 1.
+
+    ! In the adjoint, we loop over faces, and for face 1 we use c at the
+    ! next-cell for face -1 but for the face +1 (the ijp addition), and b
+    ! for the next-cell for face -1. So this means that we can loop over
+    ! the faces in the same order as the forward but use a different map
+    ! (that I've called adj_map1d), and then use the same formula for ijp.
+    ! i.e. use almost the same looping as the forward, but the only
+    ! difference is that the map is pointing to the cells on the opposite
+    ! side of the centre cell.
+    adj_map1d(1,:) = map1d(1,:)
+    adj_map1d(2,:) = map1d(3,:)
+    adj_map1d(3,:) = map1d(2,:)
+
     do p = 1, order + 1, 1
        do f = 1, nfaces, 1
+
           if(map1d(p,f) /= 1)then
-             ! Off-diagonal term
-            call adj_compute_reconstruction(p, f, order,                               &
-                                            reconstruction, stencil_map(1,map1d(p,f)), &
-                                            coeff, map_c(1), tracer, map_md(1), nl)
+             ! Off-diagonal term. Indices to 'reconstruction'/'coeff' are swapped with those
+             ! for 'tracer' with the change that 'map_md(1)' is taken to mean the 'owned cell'
+             ! of the multi-data field and thus corresponds to 'map_ws(1)' for the 'tracer'
+             ! field.
+             ! Stencil access is now on read-only 'reconstruction' and 'coeff' fields.
+            call adj_compute_reconstruction(p, f, order,                                    &
+                                            reconstruction, stencil_map(1, adj_map1d(p,f)), &
+                                            coeff, cstencil_map(1, adj_map1d(p,f)),         &
+                                            tracer, map_ws(1), nl)
          else
             ! Diagonal term (update owned cell only)
-            call adj_compute_reconstruction(p, f, order,                               &
-                                            reconstruction, map_md(1),                 &
-                                            coeff, map_c(1),                           &
-                                            tracer, stencil_map(1,map1d(p,f)), nl)
+            call adj_compute_reconstruction(p, f, order,                                &
+                                            reconstruction, stencil_map(1, map1d(p,f)), &
+                                            coeff, map_c(1),                            &
+                                            ! We want the owned cell and for tracer that is
+                                            ! given by map_ws(1)
+                                            tracer, map_ws(1), nl)
 
          end if
       enddo
@@ -114,15 +137,12 @@ module adj_poly1d_reconstruction_kernel_mod
     integer :: k
     integer :: ijp
     integer :: df
-    integer :: stencil_depth
-    integer :: face_mod
-    integer :: face
-    integer :: depth
 
     df = f * nl + f - nl + recon_cell - 1
     ijp = coeff_cell + f * order + f - order + p - 2
     do k = nl, 0, -1
-      tracer(k + tracer_cell) = tracer(k + tracer_cell) + coeff(ijp) * reconstruction(df + k)
+       tracer(k + tracer_cell) = tracer(k + tracer_cell) + &
+            coeff(ijp) * reconstruction(df + k)
     enddo
 
   end subroutine adj_compute_reconstruction
