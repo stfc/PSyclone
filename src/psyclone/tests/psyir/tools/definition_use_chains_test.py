38aac4bdcccd8eaf72adb75558f08ec73dfed29c
Aidan Chalk <3043914+LonelyCat124@users.noreply.github.com>
2024-09-18 16:16:52 +0100

    Failing test for structures

--- src/psyclone/tests/psyir/tools/definition_use_chains_test.py
+++ src/psyclone/tests/psyir/tools/definition_use_chains_test.py
@@ -495,3 +495,22 @@ def test_definition_use_chain_find_forward_accesses_while_loop_example(
     assert len(reaches) == 2
     assert reaches[0] is routine.children[2].loop_body.children[0].rhs.children[0]
     assert reaches[1] is routine.children[2].loop_body.children[0].lhs
+
+
+def test_definition_use_chain_find_forward_accesses_structure_example(
+    fortran_reader,
+):
+    code = """
+    subroutine x()
+    use some_mod
+    a%b = 1
+    a%c = 2
+    end subroutine"""
+
+    psyir = fortran_reader.psyir_from_source(code)
+    routine = psyir.walk(Routine)[0]
+    chains = DefinitionUseChain(
+        routine.children[0].lhs
+    )
+    reaches = chains._find_forward_accesses()
+    assert len(reaches) == 0
