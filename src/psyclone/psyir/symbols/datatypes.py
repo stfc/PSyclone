1b6a426e2d86a7a5738fa6ae11d8e0480dd9b82e
JulienRemy <julien.remy@inria.fr>
2024-12-05 17:46:59 +0100

    # 2577 Fix ArrayType copy.

--- src/psyclone/psyir/symbols/datatypes.py
+++ src/psyclone/psyir/symbols/datatypes.py
@@ -894,7 +894,8 @@ class ArrayType(DataType):
                 # This dimension is specified with an ArrayType.Extent
                 # so no need to copy.
                 new_shape.append(dim)
-        return ArrayType(self.datatype, new_shape)
+        array_copy = ArrayType(self.datatype, new_shape, self.is_pointer, self.is_target)
+        return array_copy
 
     def replace_symbols_using(self, table):
         '''
