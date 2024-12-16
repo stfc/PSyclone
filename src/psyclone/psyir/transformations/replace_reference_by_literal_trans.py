3047fdd6fd39c716814c9c00ce66939a0dfd03d6
Hugo Brunie <hugo.brunie@univ-grenoble-alpes.fr>
2024-12-16 18:13:05 +0100

    [ReplaceRefByLiteral] transformation first draft

---
+++ src/psyclone/psyir/transformations/replace_reference_by_literal_trans.py
@@ -0,0 +1,135 @@
+# -----------------------------------------------------------------------------
+# BSD 3-Clause License
+#
+# Copyright (c) 2022-2024, Science and Technology Facilities Council.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# -----------------------------------------------------------------------------
+# Author: H. Brunie, University of Grenoble Alpes
+
+"""Module providing a transformation that replace PsyIR Node static const Reference 
+with a Literal node when possible. """
+
+from psyclone.psyir.symbols import DataSymbol, Symbol, SymbolTable
+from psyclone.psyGen import Transformation
+from psyclone.psyir.nodes import (
+    Routine,
+    Reference,
+    Literal,
+    Container,
+)
+from psyclone.psyir.transformations.transformation_error import (
+    TransformationError,
+)
+
+from typing import Dict
+
+
+class ReplaceReferenceByLiteralTrans(Transformation):
+    """Replace Reference by Literal. For example:
+
+    TODO: shoule we make it also for the module symbol table parameter?
+
+    eg:
+    module toto
+    integer, parameter x= 3
+    integer, parameter z = x*2
+
+    becomes
+    module toto
+    integer, parameter x= 3
+    integer, parameter z = 3*2
+
+    """
+
+    def __str__(self):
+        return "Replaces all static const Reference by its Literal in a subroutine."
+
+    def _update_param_table(
+        self, param_table: Dict[str, Literal], symbol_table: SymbolTable
+    ) -> Dict[str, Literal]:
+        for sym in symbol_table.symbols:
+            sym: Symbol
+            if not isinstance(sym, DataSymbol):
+                continue
+            if sym.is_constant:
+                sym_name = sym.name
+                if param_table.get(sym_name):
+                    raise TransformationError(
+                        f"Symbol already found {sym_name}."
+                    )
+                if not isinstance(sym.initial_value, Literal):
+                    raise TransformationError(
+                        f"DataSymbol {sym_name} initial value is not a Literal {type(sym.initial_value)}."
+                    )
+                new_literal = sym.initial_value.copy().detach()
+                param_table[sym_name] = new_literal
+        return param_table
+
+    # ------------------------------------------------------------------------
+    def apply(self, node: Routine, options=None):
+        self.validate(node, options)
+        param_table: Dict[str, Literal] = {}
+        if isinstance(node.parent, Container):
+            param_table = self._update_param_table(
+                param_table, node.parent.symbol_table
+            )
+
+        param_table = self._update_param_table(param_table, node.symbol_table)
+
+        for ref in node.walk(Reference):
+            ref: Reference
+            if ref.name in param_table:
+                literal = param_table[ref.name]
+                ref.replace_with(literal.copy())
+
+    # ------------------------------------------------------------------------
+    def validate(self, node, options=None):
+        """Perform various checks to ensure that it is valid to apply the
+        ReplaceReferenceByLiteralTrans transformation to the supplied PSyIR
+        Node.
+
+        :param node: the node that is being checked.
+        :type node: :py:class:`psyclone.psyir.nodes.Routine`
+
+        :raises TransformationError: if the node argument is not a \
+            Routine.
+
+        """
+        if not isinstance(node, Routine):
+            raise TransformationError(
+                f"Error in {self.name} transformation. The supplied node "
+                f"argument should be a PSyIR Routine, but found "
+                f"'{type(node).__name__}'."
+            )
+        if node.symbol_table is None:
+            raise TransformationError("SymbolTable is None")
+
+
+__all__ = ["ReplaceReferenceByLiteralTrans"]
---
+++ src/psyclone/tests/psyir/transformations/replace_reference_by_literal_trans_test.py
@@ -0,0 +1,135 @@
+# -----------------------------------------------------------------------------
+# BSD 3-Clause License
+#
+# Copyright (c) 2022-2024, Science and Technology Facilities Council.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derrbled from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# -----------------------------------------------------------------------------
+# Author: H. Brunie, University of Grenoble Alpes
+
+
+"""This module tests the ReplaceReferenceByLiteralTrans transformation.
+"""
+
+import pytest
+
+from psyclone.psyir.nodes import Literal, Routine
+from psyclone.psyir.symbols import INTEGER_TYPE
+from psyclone.psyir.transformations import (
+    ReplaceReferenceByLiteralTrans,
+    TransformationError,
+)
+
+
+# ----------------------------------------------------------------------------
+def test_rrbl_general():
+    """Test general functionality of the transformation."""
+
+    rrbl = ReplaceReferenceByLiteralTrans()
+
+    assert (
+        str(rrbl)
+        == "Replaces all static const Reference by its Literal in a subroutine."
+    )
+    assert rrbl.name == "ReplaceReferenceByLiteralTrans"
+
+
+# ----------------------------------------------------------------------------
+def test_rrbl_errors():
+    """Test errors that should be thrown."""
+
+    rrbl = ReplaceReferenceByLiteralTrans()
+    lit = Literal("1", INTEGER_TYPE)
+    with pytest.raises(TransformationError) as err:
+        rrbl.apply(lit)
+
+    assert (
+        "Error in ReplaceReferenceByLiteralTrans transformation. The "
+        "supplied node argument should be a PSyIR Routine, but found "
+        "'Literal'" in str(err.value)
+    )
+
+
+# ----------------------------------------------------------------------------
+def test_rrbl_working(fortran_reader, fortran_writer):
+    """Tests if subroutine parameters are replaced as expected."""
+    source = """program test
+                use mymod
+                type(my_type):: t1, t2, t3, t4
+                integer, parameter :: x=3, y=12, z=13
+                integer, parameter :: u1=1, u2=2, u3=3, u4=4
+
+                integer i, invariant, ic1, ic2, ic3
+                real, dimension(10) :: a
+                invariant = 1
+                do i = 1, 10
+                    t1%a = z
+                    a(ic1) = u1+(ic1+x)*ic1
+                    a(ic2) = u2+(ic2+y)*ic2
+                    a(ic3) = u3+(ic3+z)*ic3
+                    a(t1%a) = u4+(t1%a+u4*z)*t1%a
+                end do
+                end program test"""
+    psyir = fortran_reader.psyir_from_source(source)
+    # The first child is the assignment to 'invariant'
+    routine = psyir.walk(Routine)[0]
+
+    rrbl = ReplaceReferenceByLiteralTrans()
+    rrbl.apply(routine)
+    written_code = fortran_writer(routine)
+
+    assert "a(ic1) = 1 + (ic1 + 3) * ic1" in written_code
+    assert "a(ic2) = 2 + (ic2 + 12) * ic2" in written_code
+    assert "a(ic3) = 3 + (ic3 + 13) * ic3" in written_code
+    assert "a(t1%a) = 4 + (t1%a + 4 * 13) * t1%a" in written_code
+
+
+# ----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+def test_rrbl_module_defined_parameter(fortran_reader, fortran_writer):
+    source = """module test
+                integer, parameter :: x=1, y=2, z=3
+                real, dimension(10) :: a
+                contains 
+                subroutine foo()
+                integer i,ic1
+                do i = 1, 10, 5
+                    ic1 = i+1
+                    a(ic1) = x+(ic1+y)*ic1 * z
+                end do
+                end subroutine
+                end module"""
+    psyir = fortran_reader.psyir_from_source(source)
+    routine = psyir.walk(Routine)[0]
+
+    rrbl = ReplaceReferenceByLiteralTrans()
+    rrbl.apply(routine)
+    out = fortran_writer(routine)
+
+    assert "a(ic1) = 1 + (ic1 + 2) * ic1 * 3" in out
