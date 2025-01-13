1ac47d51e7ee508efa7c10c9e3e1023457aed7ca
Joerg Henrichs <henrichsjoerg@gmail.com>
2025-01-14 00:12:06 +1100

    #1623 Updated examples 2.8 to new trans arguments.

--- tutorial/training/gocean/2.8-GameOfLife-openmp/solution/openmp.py
+++ tutorial/training/gocean/2.8-GameOfLife-openmp/solution/openmp.py
@@ -1,7 +1,7 @@
 # -----------------------------------------------------------------------------
 # BSD 3-Clause License
 #
-# Copyright (c) 2021-2023, Science and Technology Facilities Council.
+# Copyright (c) 2021-2025, Science and Technology Facilities Council.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -41,25 +41,23 @@ to the code.
 from psyclone.domain.common.transformations import KernelModuleInlineTrans
 from psyclone.gocean1p0 import GOKern, GOLoop
 from psyclone.transformations import OMPParallelLoopTrans
+from psyclone.psyGen import InvokeSchedule
 
 
-def trans(psy):
+def trans(psyir):
     '''
-    Take the supplied psy object, and fuse the first two loops
+    Take the supplied psy object, and apply 'omp parallel do' to all loops.
 
-    :param psy: the PSy layer to transform.
-    :type psy: :py:class:`psyclone.psyGen.PSy`
-
-    :returns: the transformed PSy object.
-    :rtype: :py:class:`psyclone.psyGen.PSy`
+    :param psyir: the PSyIR of the PSy-layer.
+    :type psyir: :py:class:`psyclone.psyir.nodes.FileContainer`
 
     '''
     omp_parallel = OMPParallelLoopTrans(omp_schedule="dynamic")
     omp_parallel.omp_schedule = "static"
     inline = KernelModuleInlineTrans()
 
-    invoke = psy.invokes.get("invoke_compute")
-    schedule = invoke.schedule
+    # We know that there is only one schedule
+    schedule = psyir.walk(InvokeSchedule)[0]
 
     # Inline all kernels to help gfortran with inlining.
     for kern in schedule.walk(GOKern):
