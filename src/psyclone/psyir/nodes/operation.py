185729a9d595a8faccb9b37cb0ccf46811947246
Sergi Siso <sergiesg@gmail.com>
2025-01-21 14:28:48 +0000

    #2862 Fix doc build warning

--- src/psyclone/psyir/nodes/operation.py
+++ src/psyclone/psyir/nodes/operation.py
@@ -169,11 +169,11 @@ class UnaryOperation(Operation):
         '''Create a UnaryOperation instance given an operator and operand.
 
         :param operator: the specified operator.
-        :type operator:
+        :type operator: \
             :py:class:`psyclone.psyir.nodes.UnaryOperation.Operator`
         :param operand: the PSyIR node that oper operates on, or a tuple
             containing the name of the argument and the PSyIR node.
-        :type operand: Union[:py:class:`psyclone.psyir.nodes.Node` |
+        :type operand: Union[:py:class:`psyclone.psyir.nodes.Node`,
             Tuple[str, :py:class:`psyclone.psyir.nodes.Node`]]
 
         :returns: a UnaryOperation instance.
