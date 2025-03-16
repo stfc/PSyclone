2e18358699711986881bf6004d100672361705bd
Hugo Brunie <hbrunie0@gmail.com>
2025-03-16 20:22:20 +0100

    wip evaluate condition

--- src/psyclone/psyir/tools/evaluate_condition.py
+++ src/psyclone/psyir/tools/evaluate_condition.py
@@ -1,12 +1,55 @@
+from enum import Enum
+from typing import Union
+
 from psyclone.psyir import nodes
+from psyclone.psyir.symbols import ScalarType, SymbolTable
+from psyclone.psyir.transformations import TransformationError
+
+
+class EvaluationError(TransformationError):
+    pass
+
+
+class BooleanValue(Enum):
+    UNKNOWN = 0
+    DYNAMIC = 1
+    ## STATIC
+    ALWAYS_TRUE = 2
+    ALWAYS_FALSE = 3
+
 
 class EvaluateCondition:
 
     def __init__(self, known_variables) -> None:
         self._known_variables: dict[str, bool] = known_variables
 
+    def get_integer_value_from_literal(self, psyir_node: nodes.Literal) -> int:
+        if psyir_node.datatype.intrinsic == ScalarType.Intrinsic.INTEGER:
+            return int(psyir_node.value)
+        else:
+            raise ValueError("Not an integer literal")
+
+    def get_boolean_value_from_literal(self, psyir_node: nodes.Literal) -> bool:
+        if psyir_node.datatype.intrinsic == ScalarType.Intrinsic.BOOLEAN:
+            if psyir_node.value == "true":
+                return True
+            else:
+                assert psyir_node.value == "false"
+                return False
+        else:
+            raise ValueError("Not a boolean literal")
+
+    def get_value_from_literal(self, psyir_node: nodes.Literal) -> Union[int, bool]:
+        try:
+            return self.get_boolean_value_from_literal(psyir_node)
+        except ValueError as ve1:
+            try:
+                return self.get_integer_value_from_literal(psyir_node)
+            except ValueError as ve2:
+                raise ValueError(f"Not known Literal value: {ve2}") from ve1
+
     def _evaluate_literal(self, psyir_node: nodes.Literal, is_not: bool = False) -> BooleanValue:
-        value: bool = Utils.get_boolean_value_from_literal(psyir_node)
+        value: Union[int, bool] = self.get_value_from_literal(psyir_node)
         ## is_not XOR LiteralValue
         if is_not != value:
             return BooleanValue.ALWAYS_TRUE
@@ -31,7 +74,7 @@ class EvaluateCondition:
         if expr:
             return BooleanValue.ALWAYS_TRUE
         else:
-            return BooleanValue.ALWAYS_TRUE
+            return BooleanValue.ALWAYS_FALSE
 
     def _and(self, boolean1: BooleanValue, boolean2: BooleanValue) -> BooleanValue:
         if boolean1 in (BooleanValue.DYNAMIC, BooleanValue.UNKNOWN):
@@ -56,10 +99,10 @@ class EvaluateCondition:
     def _evaluate_equality(self, psyir_ref1: nodes.Reference, psyir_ref2: nodes.Reference) -> BooleanValue:
         name1 = psyir_ref1.name
         name2 = psyir_ref2.name
-        boolvalue1 = self._known_boolean_table.get(name1)
-        intvalue1 = self._known_int_table.get(name1)
-        boolvalue2 = self._known_boolean_table.get(name2)
-        intvalue2 = self._known_int_table.get(name2)
+        boolvalue1 = self._known_variables.get(name1)
+        intvalue1 = self._known_variables.get(name1)
+        boolvalue2 = self._known_variables.get(name2)
+        intvalue2 = self._known_variables.get(name2)
         if boolvalue1 is not None and boolvalue2 is not None:
             return self._from_bool_to_boolean(boolvalue1 == boolvalue2)
         elif intvalue1 is not None and intvalue2 is not None:
@@ -70,9 +113,19 @@ class EvaluateCondition:
     def _evaluate_nonequality(self, psyir_ref1: nodes.Reference, psyir_ref2: nodes.Reference) -> BooleanValue:
         pass
 
+    def _evaluate_lt(self, psyir_ref1: nodes.Reference, psyir_ref2: nodes.Reference) -> BooleanValue:
+        pass
+
+    def _evaluate_gt(self, psyir_ref1: nodes.Reference, psyir_ref2: nodes.Reference) -> BooleanValue:
+        pass
+
     def _evaluate_binary_operation(self, psyir_node: nodes.BinaryOperation, is_not: bool = False) -> BooleanValue:
         boolean0: BooleanValue = self.rec_evaluate(psyir_node.children[0], is_not)
         boolean1: BooleanValue = self.rec_evaluate(psyir_node.children[1], is_not)
+        if boolean0 == BooleanValue.DYNAMIC or boolean0 == BooleanValue.UNKNOWN:
+            return boolean0
+        if boolean1 == BooleanValue.DYNAMIC or boolean0 == BooleanValue.UNKNOWN:
+            return boolean1
         if psyir_node.operator == nodes.BinaryOperation.Operator.AND:
             return self._not(self._and(boolean0, boolean1))
         elif psyir_node.operator == nodes.BinaryOperation.Operator.OR:
@@ -83,25 +136,28 @@ class EvaluateCondition:
         elif psyir_node.operator == nodes.BinaryOperation.Operator.NE:
             assert len(psyir_node.children) == 2
             return self._evaluate_nonequality(psyir_node.children[0], psyir_node.children[1])
+        elif psyir_node.operator == nodes.BinaryOperation.Operator.LT:
+            assert len(psyir_node.children) == 2
+            return self._evaluate_lt(psyir_node.children[0], psyir_node.children[1])
+        elif psyir_node.operator == nodes.BinaryOperation.Operator.GT:
+            assert len(psyir_node.children) == 2
+            return self._evaluate_gt(psyir_node.children[0], psyir_node.children[1])
         else:
             raise Exception("Not supported.")
 
     def _evaluate_reference_as_known_bool_or_int(self, psyir_node: nodes.Reference) -> Union[bool, int]:
         var_name: str = psyir_node.name
         ## If it is found in NameList table, get its value
-        if var_name in self._known_reference_bool:
-            return self._known_reference_bool[var_name]
-        ## if is parameter
-        elif var_name in self._known_reference_int:
-            return self._known_reference_int[var_name]
+        if var_name in self._known_variables:
+            return self._known_variables[var_name]
         else:
-            raise TransformationError("Not in namelist not parameter table.")
+            raise EvaluationError("Not a known variable.")
 
     def _evaluate_single_reference(self, psyir_node: nodes.Reference, is_not: bool = False) -> Union[int, BooleanValue]:
         assert isinstance(psyir_node, nodes.Reference)
         try:
             value: Union[bool, int] = self._evaluate_reference_as_known_bool_or_int(psyir_node)
-        except TransformationError as e:
+        except EvaluationError:
             return BooleanValue.DYNAMIC
 
         if isinstance(value, int):
@@ -135,16 +191,21 @@ class EvaluateCondition:
         else:
             raise TransformationError("Not implemented.")
 
-    def evaluate(self, condition: Node) -> bool:
-        boolean = self.rec_evaluate(condition)
+    def evaluate(self, condition: nodes.Node) -> bool:
+        error_msg = ""
+        try:
+            boolean = self.rec_evaluate(condition)
+        except ValueError as ve:
+            error_msg = f"{ve}"
+            boolean = BooleanValue.DYNAMIC
         if boolean == BooleanValue.ALWAYS_TRUE:
             return True
         elif boolean == BooleanValue.ALWAYS_FALSE:
             return False
         else:
-            raise TransformationError("Unknown or Dynamic condition.")
+            raise EvaluationError(f"Condition is {type(boolean)}: {error_msg}")
 
-    def evaluate_with_sympy(self, condition: Node, sym_table: SymbolTable) -> bool:
+    def evaluate_with_sympy(self, condition: nodes.Node, sym_table: SymbolTable) -> bool:
         from psyclone.psyir.frontend.sympy_reader import SymPyReader
         from psyclone.psyir.backend.sympy_writer import SymPyWriter
         import sympy
@@ -153,6 +214,6 @@ class EvaluateCondition:
         new_expr = sympy.simplify(expr_sympy)
         reader = SymPyReader(expr_sympy)
 
-        psyir_expr: Node = reader.psyir_from_expression(new_expr, sym_table)
+        psyir_expr: nodes.Node = reader.psyir_from_expression(new_expr, sym_table)
         print(psyir_expr.debug_string())
         return True
