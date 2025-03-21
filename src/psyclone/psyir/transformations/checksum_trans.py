1ac197a6578b1b76291358fe6c082ac358e54f92
LonelyCat124 <3043914+LonelyCat124@users.noreply.github.com.>
2025-03-21 15:12:56 +0000

    Initial implementaiton of ChecksumTrans with test!

---
+++ src/psyclone/psyir/transformations/checksum_trans.py
@@ -0,0 +1,121 @@
+# -----------------------------------------------------------------------------
+# BSD 3-Clause License
+#
+# Copyright (c) 2017-2025, Science and Technology Facilities Council.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# -----------------------------------------------------------------------------
+# Author  A. B. G. Chalk, STFC Daresbury Lab
+
+'''This module contains the ChecksumTrans class.'''
+
+from typing import Union, List
+
+from psyclone.core import VariablesAccessInfo
+from psyclone.psyir.nodes import Assignment, Node, Reference, Routine
+from psyclone.psyir.transformations.region_trans import RegionTrans
+from psyclone.psyir.frontend.fortran import FortranReader
+from psyclone.psyir.symbols import DataSymbol, INTEGER_TYPE, \
+        PreprocessorInterface
+
+
+class ChecksumTrans(RegionTrans):
+    '''
+    Creates a set of checksums (written via print) for all written to arrays
+    inside the provided region.
+    '''
+
+    def apply(self, node: Union[Node, List[Node]], options=None) -> None:
+        '''
+            Applies the checksum transformation to the provided node(s).
+
+            :param nodes: The node or list of nodes to apply the
+                          transformation to.
+            :param options: a dictionary with options for transformations.
+            :type options: Optional[Dict[str, Any]]
+
+        '''
+        self.validate(node)
+
+        node_list = self.get_node_list(node)
+
+        # Find all the writes.
+        vai = VariablesAccessInfo(node_list)
+
+        writes = []
+        for sig in vai.all_data_accesses:
+            if vai.is_written(sig) and vai[sig].is_array():
+                sym = vai[sig].all_accesses[0].node.symbol
+                writes.append(sym)
+        # For each write, add a checksum after.
+        checksum_nodes = []
+        freader = FortranReader()
+        for sym in writes:
+            sym_name = sym.name
+            checksum = freader.psyir_from_statement(
+                    f'print *, "{sym_name} checksum", SUM({sym_name})',
+                    node_list[0].ancestor(Routine).symbol_table)
+            # Remove the comment about this being a code block.
+            checksum.preceding_comment = ""
+            checksum_nodes.append(checksum)
+
+        # Find the last node in the region
+        depth = 10000000
+        position = -1
+        for node in node_list:
+            if node.depth < depth:
+                depth = node.depth
+                position = node.position
+            elif node.depth == depth and node.position > position:
+                position = node.position
+
+        parent = node.parent
+        for node in checksum_nodes:
+            parent.addchild(node, position+1)
+
+        internal_line = \
+            node_list[0].ancestor(Routine).symbol_table.find_or_create(
+                "PSYCLONE_INTERNAL_line_", symbol_type=DataSymbol,
+                datatype=INTEGER_TYPE,
+                )
+        line = node_list[0].ancestor(Routine).symbol_table.find_or_create(
+                "__LINE__", symbol_type=DataSymbol, datatype=INTEGER_TYPE,
+                interface=PreprocessorInterface())
+        # Tell us where we are to output the checksums.
+        explanation_statement = freader.psyir_from_statement(
+                f'print *, "checksums from '
+                f'{node_list[0].ancestor(Routine).name} at line:"'
+                f', PSYCLONE_INTERNAL_line_',
+                node_list[0].ancestor(Routine).symbol_table
+                )
+        # Remove the comment about this being a code block.
+        explanation_statement.preceding_comment = ""
+        assign = Assignment.create(Reference(internal_line), Reference(line))
+        parent.addchild(explanation_statement, position+1)
+        parent.addchild(assign, position+1)
---
+++ src/psyclone/tests/psyir/transformations/checksum_trans_test.py
@@ -0,0 +1,81 @@
+# -----------------------------------------------------------------------------
+# BSD 3-Clause License
+#
+# Copyright (c) 2021-2025, Science and Technology Facilities Council.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# -----------------------------------------------------------------------------
+# Author A. B. G. Chalk, STFC Daresbury Lab
+# -----------------------------------------------------------------------------
+
+''' This module contains the tests for the ChecksumTrans.'''
+
+from psyclone.psyir.transformations import ChecksumTrans
+
+
+def test_checksum(fortran_reader, fortran_writer):
+
+    code = """
+    subroutine test
+        integer, dimension(1:100) :: a, b, c
+        integer :: i
+        integer :: d
+
+        do i = 1, 100
+            a(i) = c(i) + d
+            b(i) = 2 * i
+        enddo
+    end subroutine
+    """
+
+    psyir = fortran_reader.psyir_from_source(code)
+
+    ChecksumTrans().apply(psyir.children[0].children[0])
+
+    correct = """subroutine test()
+  integer, dimension(100) :: a
+  integer, dimension(100) :: b
+  integer, dimension(100) :: c
+  integer :: i
+  integer :: d
+  integer :: PSYCLONE_INTERNAL_line_
+
+  do i = 1, 100, 1
+    a(i) = c(i) + d
+    b(i) = 2 * i
+  enddo
+  PSYCLONE_INTERNAL_line_ = __LINE__
+  PRINT *, "checksums from test at line:", PSYCLONE_INTERNAL_line_
+  PRINT *, "b checksum", SUM(b)
+  PRINT *, "a checksum", SUM(a)
+
+end subroutine test
+"""
+    out = fortran_writer(psyir)
+    assert out == correct
