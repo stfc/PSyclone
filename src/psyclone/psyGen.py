b3a399900535fc8cb9e82ea6cef33f989b12dd91
LonelyCat124 <3043914+LonelyCat124@users.noreply.github.com.>
2025-05-09 14:06:27 +0100

    Initialised logging infrastructure

--- src/psyclone/psyGen.py
+++ src/psyclone/psyGen.py
@@ -44,6 +44,7 @@
 from dataclasses import dataclass
 import inspect
 import os
+import logging
 from collections import OrderedDict
 import abc
 from typing import Any, Dict
@@ -2766,6 +2767,8 @@ class Transformation(metaclass=abc.ABCMeta):
         '''
         # TODO 2668: options are now deprecated:
         if options is not None:
+            logger = logging.getLogger(__name__)
+            logger.warning(self._deprecation_warning)
             print(self._deprecation_warning)
 
     def validate(self, node, options=None, **kwargs):
