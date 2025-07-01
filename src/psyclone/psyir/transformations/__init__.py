05ad389eff78f1f67e6b5889cae73bafad9640d0
Victoria Atkinson <victoria.atkinson@stfc.ac.uk>
2025-07-01 10:25:25 +0100

    #2971 add liscencing and add import to __init__.py

--- src/psyclone/psyir/transformations/__init__.py
+++ src/psyclone/psyir/transformations/__init__.py
@@ -114,6 +114,8 @@ from psyclone.psyir.transformations.scalarisation_trans import \
     ScalarisationTrans
 from psyclone.psyir.transformations.parallel_region_trans import \
     ParallelRegionTrans
+from psyclone.psyir.transformations.omp_taskloop_trans import \
+    OMPTaskloopTrans
 
 # For AutoAPI documentation generation
 __all__ = [
--- src/psyclone/psyir/transformations/omp_taskloop_trans.py
+++ src/psyclone/psyir/transformations/omp_taskloop_trans.py
@@ -1,3 +1,30 @@
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# -----------------------------------------------------------------------------
+# Authors R. W. Ford, A. R. Porter, S. Siso and N. Nobre, STFC Daresbury Lab
+#         A. B. G. Chalk, V. K. Atkinson, STFC Daresbury Lab
+#         J. Henrichs, Bureau of Meteorology
+# Modified I. Kavcic, J. G. Wallwork, O. Brunt and L. Turner, Met Office
+#          S. Valat, Inria / Laboratoire Jean Kuntzmann
+#          M. Schreiber, Univ. Grenoble Alpes / Inria / Lab. Jean Kuntzmann
+#          J. Dendy, Met Office
+
+'''
+This module provides the implementation of OMPTaskloopTrans
+
+'''
+
 from psyclone.psyir.transformations.parallel_loop_trans import (
     ParallelLoopTrans)
 
