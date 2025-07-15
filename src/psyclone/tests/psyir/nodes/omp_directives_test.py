fc5c9b9e469cca348031f6a70c4117a5cd8268d7
Sergi Siso <sergiesg@gmail.com>
2025-07-15 12:25:55 +0100

    #3056 Demote OpenMP lowering error to warning

--- src/psyclone/tests/psyir/nodes/omp_directives_test.py
+++ src/psyclone/tests/psyir/nodes/omp_directives_test.py
@@ -77,7 +77,7 @@ GOCEAN_BASE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                 "gocean1p0")
 
 
-def test_ompparallel_lowering(fortran_reader, monkeypatch):
+def test_ompparallel_lowering(fortran_reader, monkeypatch, caplog):
     ''' Check that lowering an OMP Parallel region leaves it with the
     appropriate begin_string and clauses for the backend to generate
     the right code'''
@@ -140,11 +140,11 @@ def test_ompparallel_lowering(fortran_reader, monkeypatch):
     a_sym = Symbol("a")
     monkeypatch.setattr(pdir, "infer_sharing_attributes",
                         lambda: ({}, {}, {a_sym}))
-    with pytest.raises(GenerationError) as err:
-        pdir.lower_to_language_level()
-    assert ("Lowering 'OMPParallelDirective' does not support symbols that "
-            "need synchronisation unless they are in a depend clause, but "
-            "found: 'a' which is not in a depend clause." in str(err.value))
+    pdir.lower_to_language_level()
+    assert ("Lowering 'OMPParallelDirective' detected a possible race "
+            "condition for symbol 'a'. Make sure this is a false WaW "
+            "dependency or the code includes the necessary synchronisations."
+            "\n" in caplog.text)
 
     # Also a case which contains the symbol in an input dependency clause.
     task_dir = OMPTaskDirective()
@@ -157,14 +157,14 @@ def test_ompparallel_lowering(fortran_reader, monkeypatch):
     task_dir.addchild(in_clause)
     pdir.children[0].addchild(task_dir)
 
-    with pytest.raises(GenerationError) as err:
-        pdir.lower_to_language_level()
-    assert ("Lowering 'OMPParallelDirective' does not support symbols that "
-            "need synchronisation unless they are in a depend clause, but "
-            "found: 'a' which is not in a depend clause." in str(err.value))
+    pdir.lower_to_language_level()
+    assert ("Lowering 'OMPParallelDirective' detected a possible race "
+            "condition for symbol 'a'. Make sure this is a false WaW "
+            "dependency or the code includes the necessary synchronisations."
+            "\n" in caplog.text)
 
 
-def test_omp_parallel_do_lowering(fortran_reader, monkeypatch):
+def test_omp_parallel_do_lowering(fortran_reader, monkeypatch, caplog):
     ''' Check that lowering an OMP Parallel Do leaves it with the
     appropriate begin_string and clauses for the backend to generate
     the right code'''
@@ -230,11 +230,11 @@ def test_omp_parallel_do_lowering(fortran_reader, monkeypatch):
     # Monkeypatch a case with shared variables that need synchronisation
     monkeypatch.setattr(pdir, "infer_sharing_attributes",
                         lambda: ({}, {}, {Symbol("a")}))
-    with pytest.raises(GenerationError) as err:
-        pdir.lower_to_language_level()
-    assert ("Lowering 'OMPParallelDoDirective' does not support symbols that "
-            "need synchronisation unless they are in a depend clause, but "
-            "found: 'a' which is not in a depend clause." in str(err.value))
+    pdir.lower_to_language_level()
+    assert ("Lowering 'OMPParallelDoDirective' detected a possible race "
+            "condition for symbol 'a'. Make sure this is a false WaW "
+            "dependency or the code includes the necessary synchronisations."
+            "\n" in caplog.text)
 
 
 def test_omp_teams_distribute_parallel_do_strings(
