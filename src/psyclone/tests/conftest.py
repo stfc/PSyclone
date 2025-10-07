279ec53f253d4c8715c1f96e18d4547484af365e
Andrew Porter <andrew.porter@stfc.ac.uk>
2025-10-07 09:23:10 +0100

    #2381 ensure any pre-existing Config is wiped by test fixture

--- src/psyclone/tests/conftest.py
+++ src/psyclone/tests/conftest.py
@@ -98,6 +98,9 @@ def setup_psyclone_config():
     independent of a potential psyclone config file installed by
     the user.
     '''
+    # Ensure any Config object that has already been loaded is wiped.
+    Config._instance = None
+
     config_file = Config.get_repository_config_file()
 
     # In case that PSyclone is installed and tested (e.g. GitHub Actions),
