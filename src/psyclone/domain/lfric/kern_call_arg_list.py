9556c2bd094217e13c9ca59faa6917cbe6cf1d31
Alistair Pirrie <187289694+mo-alistairp@users.noreply.github.com>
2025-11-14 14:17:24 +0000

    #1312: Try comment out changes to see if it fixes errors

--- src/psyclone/domain/lfric/kern_call_arg_list.py
+++ src/psyclone/domain/lfric/kern_call_arg_list.py
@@ -249,36 +249,36 @@ class KernCallArgList(ArgOrdering):
 
         '''
         super().scalar(scalar_arg, var_accesses)
-        if scalar_arg.is_scalar:
-            if scalar_arg.is_literal:
-                self.psyir_append(scalar_arg.psyir_expression())
-            else:
-                sym = self._symtab.lookup(scalar_arg.name)
-                self.psyir_append(Reference(sym))
+        # if scalar_arg.is_scalar:
+        if scalar_arg.is_literal:
+            self.psyir_append(scalar_arg.psyir_expression())
         else:
-            # ScalarArray
-            dims_array_symbol = self._symtab.find_or_create(
-                    "dims_" + scalar_arg.name,
-                    symbol_type=DataSymbol,
-                    datatype=ArrayType(
-                        LFRicTypes("LFRicIntegerScalarDataType")(),
-                        [scalar_arg._array_ndims]))
-            self._symtab.append_argument(dims_array_symbol)
+            sym = self._symtab.lookup(scalar_arg.name)
+            self.psyir_append(Reference(sym))
+        # else:
+        #     # ScalarArray
+        #     dims_array_symbol = self._symtab.find_or_create(
+        #             "dims_" + scalar_arg.name,
+        #             symbol_type=DataSymbol,
+        #             datatype=ArrayType(
+        #                 LFRicTypes("LFRicIntegerScalarDataType")(),
+        #                 [scalar_arg._array_ndims]))
+        #     self._symtab.append_argument(dims_array_symbol)
 
-            # Create list of dims_array references
-            sym_list = [ArrayReference.create(
-                dims_array_symbol,
-                [Literal(str(idx), INTEGER_TYPE)])
-                    for idx in range(1, scalar_arg._array_ndims + 1)]
+        #     # Create list of dims_array references
+        #     sym_list = [ArrayReference.create(
+        #         dims_array_symbol,
+        #         [Literal(str(idx), INTEGER_TYPE)])
+        #             for idx in range(1, scalar_arg._array_ndims + 1)]
 
-            # Create ScalarArray reference
-            array_symbol = self._symtab.find_or_create(
-                scalar_arg.name,
-                symbol_type=DataSymbol,
-                datatype=ArrayType(
-                    LFRicTypes("LFRicRealScalarDataType")(),
-                    sym_list))
-            self._symtab.append_argument(array_symbol)
+        #     # Create ScalarArray reference
+        #     array_symbol = self._symtab.find_or_create(
+        #         scalar_arg.name,
+        #         symbol_type=DataSymbol,
+        #         datatype=ArrayType(
+        #             LFRicTypes("LFRicRealScalarDataType")(),
+        #             sym_list))
+        #     self._symtab.append_argument(array_symbol)
     # TODO uncomment this method when ensuring we only pass ncell3d once
     # to any given kernel.
     # def mesh_ncell3d(self):
