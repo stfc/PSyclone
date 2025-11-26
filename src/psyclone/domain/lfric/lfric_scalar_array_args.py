05b2725dcd5df8d282139ef3d9bf9d5fccf54c2d
Alistair Pirrie <187289694+mo-alistairp@users.noreply.github.com>
2025-11-26 15:42:36 +0000

    #1312: Tidy LFRicScalarArrayArgs

--- src/psyclone/domain/lfric/lfric_scalar_array_args.py
+++ src/psyclone/domain/lfric/lfric_scalar_array_args.py
@@ -1,7 +1,7 @@
 # -----------------------------------------------------------------------------
 # BSD 3-Clause License
 #
-# Copyright (c) 2017-2025, Science and Technology Facilities Council.
+# Copyright (c) 2025, Science and Technology Facilities Council.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -57,11 +57,11 @@ from psyclone.psyir.symbols import (DataSymbol, ArrayType,
 
 class LFRicScalarArrayArgs(LFRicCollection):
     '''
-    Handles the declarations of scalar kernel arguments appearing in either
-    an Invoke or a Kernel stub.
+    Handles the declarations of ScalarArray kernel arguments appearing in
+    either an Invoke or a Kernel stub.
 
-    :param node: the Invoke or Kernel stub for which to manage the scalar \
-                 arguments.
+    :param node: the Invoke or Kernel stub for which to manage the \
+                 ScalarArray arguments.
     :type node: :py:class:`psyclone.domain.lfric.LFRicKern` or \
                 :py:class:`psyclone.domain.lfric.LFRicInvoke`
 
@@ -70,7 +70,7 @@ class LFRicScalarArrayArgs(LFRicCollection):
         super().__init__(node)
 
         # Initialise dictionaries of 'real', 'integer' and 'logical'
-        # scalar arguments by data type and intent
+        # ScalarArray arguments by data type and intent
         self._scalar_array_args = {}
         self._real_scalar_arrays = {}
         self._integer_scalar_arrays = {}
@@ -83,22 +83,22 @@ class LFRicScalarArrayArgs(LFRicCollection):
 
     def invoke_declarations(self):
         '''
-        Create argument lists and declarations for all scalar arguments
+        Create argument lists and declarations for all ScalarArray arguments
         in an Invoke.
 
 
         :raises InternalError: for unsupported argument intrinsic types.
-        :raises GenerationError: if the same scalar argument has different \
-                                 data types in different Kernel calls \
-                                 within the same Invoke.
+        :raises GenerationError: if the same ScalarArray argument has \
+                                 different data types in different Kernel \
+                                 calls within the same Invoke.
 
         '''
         super().invoke_declarations()
-        # Create dictionary of all scalar arguments for checks
+        # Create dictionary of all ScalarArray arguments for checks
         const = LFRicConstants()
         self._scalar_array_args = self._invoke.unique_declns_by_intent(
             const.VALID_ARRAY_NAMES)
-        # Filter scalar arguments by intent and intrinsic type
+        # Filter ScalarArray arguments by intent and intrinsic type
         self._real_scalar_arrays = self._invoke.unique_declns_by_intent(
             const.VALID_ARRAY_NAMES,
             intrinsic_type=const.MAPPING_DATA_TYPES["gh_real"])
@@ -127,10 +127,10 @@ class LFRicScalarArrayArgs(LFRicCollection):
                     f"Found unsupported intrinsic types for the ScalarArray "
                     f"arguments {scal_inv} to Invoke '{self._invoke.name}'. "
                     f"Supported types are {const.VALID_INTRINSIC_TYPES}.")
-            # Check that the same scalar name is not found in either of
-            # 'real', 'integer' or 'logical' scalar lists (for instance if
-            # passed to one kernel as a 'real' and to another kernel as an
-            # 'integer' scalar)
+            # Check that the same ScalarArray name is not found in either of
+            # 'real', 'integer' or 'logical' ScalarArray lists (for instance
+            # if passed to one kernel as a 'real' and to another kernel as an
+            # 'logical' ScalarArray)
             scal_multi_type = [item for item, count in
                                Counter(decl_scal).items() if count > 1]
             if scal_multi_type:
@@ -145,20 +145,20 @@ class LFRicScalarArrayArgs(LFRicCollection):
 
     def stub_declarations(self):
         '''
-        Create and add declarations for all scalar arguments in
+        Create and add declarations for all ScalarArray arguments in
         a Kernel stub.
 
         :raises InternalError: for an unsupported argument data type.
 
         '''
         super().stub_declarations()
-        # Extract all scalar arguments
+        # Extract all ScalarArray arguments
         for arg in self.kernel_calls[0].arguments.args:
             if arg.is_scalar_array:
                 self._scalar_array_args[arg.intent].append(arg)
 
         const = LFRicConstants()
-        # Filter scalar arguments by intent and data type
+        # Filter ScalarArray arguments by intent and data type
         for intent in FORTRAN_INTENT_NAMES:
             for arg in self._scalar_array_args[intent]:
                 # Distinguish whether they are ScalarArrays
@@ -181,7 +181,7 @@ class LFRicScalarArrayArgs(LFRicCollection):
 
     def _create_declarations(self):
         '''
-        Add declarations for the scalar arguments.
+        Add declarations for the ScalarArray arguments.
 
         '''
         # Real ScalarArray arguments
@@ -204,16 +204,16 @@ class LFRicScalarArrayArgs(LFRicCollection):
                             dims_array_symbol,
                             [Literal(str(idx), INTEGER_TYPE)])
                                 for idx in range(1, arg._array_ndims + 1)]
-                        # Find ScalarArray tag and convert it to an ArrayType
                         if not self._kernel:
-                            # For code generation
+                            # For code generation, find ScalarArray tag
+                            # and convert it to an ArrayType
                             array_symbol = self.symtab.lookup_with_tag(
                                 "AlgArgs_" + arg.name)
                             array_symbol.datatype = ArrayType(
                                 LFRicTypes("LFRicRealScalarDataType")(),
                                 sym_list)
                         else:
-                            # For stub generation
+                            # For stub generation, create the symbol
                             array_symbol = self.symtab.find_or_create(
                                 arg.name,
                                 symbol_type=DataSymbol,
@@ -244,16 +244,16 @@ class LFRicScalarArrayArgs(LFRicCollection):
                             dims_array_symbol,
                             [Literal(str(idx), INTEGER_TYPE)])
                                 for idx in range(1, arg._array_ndims + 1)]
-                        # Find ScalarArray tag and convert it to an ArrayType
                         if not self._kernel:
-                            # For code generation
+                            # For code generation, find ScalarArray tag
+                            # and convert it to an ArrayType
                             array_symbol = self.symtab.lookup_with_tag(
                                 "AlgArgs_" + arg.name)
                             array_symbol.datatype = ArrayType(
                                 LFRicTypes("LFRicIntegerScalarDataType")(),
                                 sym_list)
                         else:
-                            # For stub generation
+                            # For stub generation, create the symbol
                             array_symbol = self.symtab.find_or_create(
                                 arg.name,
                                 symbol_type=DataSymbol,
@@ -286,16 +286,16 @@ class LFRicScalarArrayArgs(LFRicCollection):
                             [Literal(str(idx), INTEGER_TYPE)])
                                 for idx in range(1, arg._array_ndims + 1)]
 
-                        # Find ScalarArray tag and convert it to an ArrayType
                         if not self._kernel:
-                            # For code generation
+                            # For code generation, find ScalarArray tag
+                            # and convert it to an ArrayType
                             array_symbol = self.symtab.lookup_with_tag(
                                 "AlgArgs_" + arg.name)
                             array_symbol.datatype = ArrayType(
                                 LFRicTypes("LFRicLogicalScalarDataType")(),
                                 sym_list)
                         else:
-                            # For stub generation
+                            # For stub generation, create the symbol
                             array_symbol = self.symtab.find_or_create(
                                 arg.name,
                                 symbol_type=DataSymbol,
