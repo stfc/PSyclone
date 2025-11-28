f9bc269cfbce2857ef2502d72297cd5bc9eb0835
LonelyCat124 <3043914+LonelyCat124@users.noreply.github.com.>
2025-11-28 15:01:58 +0000

    Initial implementation of OMPCritical

---
+++ src/psyclone/tests/psyir/transformations/omp_critical_trans_test.py
@@ -0,0 +1,74 @@
+# -----------------------------------------------------------------------------
+# BSD 3-Clause License
+#
+# Copyright (c) 2021-2025, Science and Technology Facilities Council.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# -----------------------------------------------------------------------------
+# Authors A. B. G. Chalk, STFC Daresbury Lab
+# -----------------------------------------------------------------------------
+
+''' This module contains the tests for the OpenMP Critical
+transformation.'''
+
+from psyclone.psyir.nodes import OMPCriticalDirective, Routine
+from psyclone.psyir.transformations import OMPCriticalTrans
+
+
+def test_omp_critical_apply(fortran_reader, fortran_writer):
+    '''Test the apply function of the OMPCriticalTrans.'''
+    ctrans = OMPCriticalTrans()
+
+    code = """subroutine x
+    integer :: i, j
+
+    i = 2
+    j = 3
+    end subroutine x"""
+
+    psyir = fortran_reader.psyir_from_source(code)
+
+    routine = psyir.walk(Routine)[0]
+
+    ctrans.apply(routine.children[:])
+    assert isinstance(routine.children[0], OMPCriticalDirective)
+
+    output = fortran_writer(psyir)
+    print(output)
+    correct = """subroutine x()
+  integer :: i
+  integer :: j
+
+  !$omp critical
+  i = 2
+  j = 3
+  !$omp end critical
+
+end subroutine x"""
+    assert correct in output
