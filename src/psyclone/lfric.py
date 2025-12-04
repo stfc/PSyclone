cee1d488a390d7b7f95ae15ae310b5e8b32a4bb4
Alistair Pirrie <187289694+mo-alistairp@users.noreply.github.com>
2025-12-04 19:08:22 +0000

    #1312: continuing the potential fix

--- src/psyclone/lfric.py
+++ src/psyclone/lfric.py
@@ -56,9 +56,9 @@ from psyclone.configuration import Config
 from psyclone.core import AccessType, Signature, SymbolicMaths
 from psyclone.domain.lfric.lfric_builtins import LFRicBuiltIn
 from psyclone.domain.lfric import (
-    FunctionSpace, KernCallArgList, LFRicCollection,
-    LFRicConstants, LFRicSymbolTable, LFRicKern,
-    LFRicTypes, LFRicLoop)
+    FunctionSpace, LFRicCollection, LFRicConstants,
+    LFRicSymbolTable, LFRicKern, LFRicTypes, LFRicLoop)
+from psyclone.domain.lfric.kern_call_arg_list import KernCallArgList
 from psyclone.domain.lfric.kern_call_acc_arg_list import KernCallAccArgList
 from psyclone.domain.lfric.lfric_invoke_schedule import LFRicInvokeSchedule
 from psyclone.errors import GenerationError, InternalError, FieldNotFoundError
