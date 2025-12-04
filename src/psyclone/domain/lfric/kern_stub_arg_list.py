699aeacd5556d0b905675eea8f0deaaa6661e9ba
Alistair Pirrie <187289694+mo-alistairp@users.noreply.github.com>
2025-12-04 10:28:22 +0000

    #1312: remove typehint from KernStubArgList

--- src/psyclone/domain/lfric/kern_stub_arg_list.py
+++ src/psyclone/domain/lfric/kern_stub_arg_list.py
@@ -42,7 +42,6 @@ for a kernel subroutine.
 from typing import Optional
 
 from psyclone.core import VariablesAccessMap
-from psyclone.lfric import LFRicKernelArgument
 from psyclone.domain.lfric.arg_ordering import ArgOrdering
 from psyclone.domain.lfric.lfric_constants import LFRicConstants
 from psyclone.errors import InternalError
@@ -135,7 +134,7 @@ class KernStubArgList(ArgOrdering):
         self.extend(_local_args, var_accesses)
 
     def scalar(self,
-               scalar_arg: LFRicKernelArgument,
+               scalar_arg,
                var_accesses: Optional[VariablesAccessMap] = None):
         '''Add the name associated with the scalar argument to the argument
         list and optionally add this scalar to the variable access
