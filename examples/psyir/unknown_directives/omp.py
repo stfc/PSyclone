9219cf3656cff539947f15e5dd3db8b9e39bfe4b
LonelyCat124 <3043914+LonelyCat124@users.noreply.github.com.>
2025-12-10 15:54:02 +0000

    Added a simple example for using directives to filter behaviour of PSyclone scripts

---
+++ examples/psyir/unknown_directives/omp.py
@@ -0,0 +1,55 @@
+# -----------------------------------------------------------------------------
+# BSD 3-Clause License
+#
+# Copyright (c) 2025, Science and Technology Facilities Council
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# -----------------------------------------------------------------------------
+# Author: A. B. G. Chalk, STFC Daresbury Lab
+
+from psyclone.psyir.transformations import OMPLoopTrans
+from psyclone.psyir.nodes import Loop, Routine, UnknownDirective
+
+# Find the PSyDirectives, check what the directive is for and then find the
+# next loop to skip when parallelising.
+def trans(psyir):
+    for routine in psyir.walk(Routine):
+        psy_dirs = routine.walk(UnknownDirective)
+        loops_to_skip = []
+        for psy_dir in psy_dirs:
+            if psy_dir.directive_string == "psy no_par":
+                position = psy_dir.position
+                parent = psy_dir.parent
+                # Remove the directive as we don't need it in the output now.
+                psy_dir.detach()
+                loops_to_skip.append(parent.children[position])
+    for loop in routine.walk(Loop):
+        if loop in loops_to_skip:
+            continue
+        OMPLoopTrans(omp_directive="paralleldo").apply(loop)
