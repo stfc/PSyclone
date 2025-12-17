72b2f906b84daab458ad79efec5bba1c70105ba6
Joerg Henrichs <henrichsjoerg@gmail.com>
2025-12-17 11:42:25 +1100

    #1623 Properly test openmp.

--- tutorial/training/gocean/gol-lib/gol.f90
+++ tutorial/training/gocean/gol-lib/gol.f90
@@ -5,7 +5,7 @@ program GameOfLife
     use time_step_alg_mod, only: time_step
     USE grid_mod, only         : grid_type
     USE field_mod, only        : r2d_field
-
+!$ use omp_lib, only: omp_get_max_threads
     implicit none
 
     TYPE(grid_type), target    :: grid
@@ -18,6 +18,7 @@ program GameOfLife
         stop
     endif
 
+!$ print *,"Using", omp_get_max_threads(), "threads"
     ! Read in the initial condition into the field 'current',
     ! and initialise dl_esm_inf.
     call read_config(grid, initial, time_steps) 
