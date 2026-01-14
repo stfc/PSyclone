99f49730546872e02fc6493a3ae3570ae68135bb
Sergi Siso <sergiesg@gmail.com>
2026-01-14 16:34:36 +0000

    #3207 Start updating LFRic integration test to use new lfric_apps git repo

---
+++ examples/lfric/scripts/KGOs/amdclang++.mk
@@ -0,0 +1,10 @@
+##############################################################################
+# (c) Crown copyright 2017 Met Office. All rights reserved.
+# The file LICENCE, distributed with this code, contains details of the terms
+# under which the code may be used.
+##############################################################################
+
+$(info ** Chosen GNU C++ compiler)
+
+CXX_RUNTIME_LIBRARY=stdc++
+
