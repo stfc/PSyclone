b269f7a739620e37078a4f2e7724ef685eea6a17
Andrew Porter <andrew.porter@stfc.ac.uk>
2026-01-27 16:39:10 +0000

    #1823 add docstrings to new mixin

--- src/psyclone/domain/common/transformations/kernel_transformation_mixin.py
+++ src/psyclone/domain/common/transformations/kernel_transformation_mixin.py
@@ -1,3 +1,43 @@
+# -----------------------------------------------------------------------------
+# BSD 3-Clause License
+#
+# Copyright (c) 2026, Science and Technology Facilities Council.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# -----------------------------------------------------------------------------
+# Author: A. R. Porter, STFC Daresbury Lab
+
+"""
+This module provides the KernelTransformationMixin class.
+
+"""
+
 from typing import Union
 
 from psyclone.psyGen import CodedKern
@@ -8,10 +48,27 @@ from psyclone.psyir.transformations.transformation_error import (
 
 
 class KernelTransformationMixin:
-    """ """
+    """
+    A mixin class to be used by all Transformations that act upon PSyKAl
+    Kernels.
 
-    def _check_kernel_is_local(self, node: Union[Node, CodedKern]):
-        """ """
+    Provides functionality to check that a Kernel has been module-inlined
+    before subsequent transformations are applied to it.
+
+    """
+    def _check_kernel_is_local(self, node: Union[Node, CodedKern]) -> None:
+        """
+        Check that the supplied kernel node has been module inlined.
+
+        :param node: the PSyKAl Kernel to check.
+
+        :raises TransformationError: if the supplied Kernel has not been
+            module inlined.
+        :raises TransformationError: if the supplied Kernel (call) is not
+            within a Container or the Container does not contain the
+            implementation of the Kernel.
+
+        """
         if not isinstance(node, CodedKern):
             return
 
