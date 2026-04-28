2f62a9673b02d376c9f73a982e02cd5772d14265
Andrew Porter <andrew.porter@stfc.ac.uk>
2026-04-28 14:28:19 +0100

    #3398 update example scripts

--- examples/lfric/scripts/gpu_offloading.py
+++ examples/lfric/scripts/gpu_offloading.py
@@ -46,6 +46,8 @@ import sys
 from psyclone.domain.common.transformations import KernelModuleInlineTrans
 from psyclone.domain.lfric import LFRicConstants
 from psyclone.domain.lfric.lfric_builtins import LFRicBuiltIn
+from psyclone.domain.lfric.transformations import (
+    LFRicRedundantComputationTrans)
 from psyclone.psyir.nodes import (
     Call, Directive, IntrinsicCall, Loop, Routine, Schedule)
 from psyclone.psyir.transformations import (
@@ -53,7 +55,6 @@ from psyclone.psyir.transformations import (
     OMPDeclareTargetTrans, OMPParallelTrans)
 from psyclone.transformations import (
     LFRicColourTrans, LFRicOMPLoopTrans,
-    LFRicRedundantComputationTrans,
     ACCParallelTrans, ACCLoopTrans, ACCRoutineTrans,
     OMPLoopTrans)
 
