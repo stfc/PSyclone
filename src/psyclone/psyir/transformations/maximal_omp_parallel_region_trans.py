68fb64c1f998810e727dd450d4fb31f8b868f4da
LonelyCat124 <3043914+LonelyCat124@users.noreply.github.com.>
2026-07-01 13:31:07 +0100

    First implementation

--- src/psyclone/psyir/transformations/maximal_omp_parallel_region_trans.py
+++ src/psyclone/psyir/transformations/maximal_omp_parallel_region_trans.py
@@ -38,6 +38,7 @@
 from typing import Union
 
 from psyclone.psyir.nodes import (
+        Assignment,
         OMPTaskwaitDirective,
         OMPBarrierDirective,
         OMPSerialDirective,
@@ -47,10 +48,12 @@ from psyclone.psyir.nodes import (
         OMPTaskDirective,
         DynamicOMPTaskDirective,
         Node,
+        Reference,
         Schedule
 )
 from psyclone.psyir.transformations.maximal_region_trans import (
-        MaximalRegionTrans)
+        MaximalRegionTrans
+)
 from psyclone.psyir.transformations.omp_parallel_trans import OMPParallelTrans
 from psyclone.utils import transformation_documentation_wrapper
 
@@ -67,6 +70,8 @@ class MaximalOMPParallelRegionTrans(MaximalRegionTrans):
     _transformation = OMPParallelTrans
     _SUB_TRANSFORMATIONS = [OMPParallelTrans]
     # Tuple of statement nodes allowed inside the _transformation
+    # Note that Assignments may also be allowed, but they are a special
+    # case not included in this selection.
     _allowed_contiguous_statements = (
         OMPTaskwaitDirective,
         OMPBarrierDirective,
@@ -85,6 +90,83 @@ class MaximalOMPParallelRegionTrans(MaximalRegionTrans):
         OMPTaskDirective,
         DynamicOMPTaskDirective,
     )
+
+    def _node_allowed(self, node: Node, current_block: list[Node]) -> bool:
+        '''Returns whether the provided node is allowed in the _transformation.
+        
+        FIXME document why this differs.
+
+        :param node: the candidate node to be in the transformation region.
+        :param current_block: The current block that node would be added into.
+
+        :returns: whether the node is allowed to be in the transformed region.
+        '''
+        if isinstance(node, self._allowed_contiguous_statements):
+            return True
+
+        if not isinstance(node, Assignment):
+            return False
+
+        # Assignments are a special case.
+        # If the lhs is an array or a non-local symbol then it is not allowed.
+        if node.lhs.is_array or not node.lhs.symbol.is_automatic:
+            return False
+
+        # If the lhs symbol appears on the rhs then its read first so we
+        # dissallow it for now. Note that in theory this could be allowed
+        # if its previously been accessed in this region and is private
+        # but we have no ability to determine this in PSyclone at this stage.
+        lhs_sym = node.lhs.symbol
+        for reference in node.rhs.walk(Reference):
+            if reference.symbol.name == lhs_sym.name:
+                return False
+
+        # We know we have an assignment where the lhs is a local scalar
+        # and does not appear on the right hand side. We need to check all
+        # the following read accesses appear in the current_block.
+        accesses = node.lhs.next_accesses()
+        # Find the abs_position of the final node in the current_block.
+        last_element = current_block[-1]
+        # For reviewer - this could be implemented using
+        # following_node.abs_position-1, but if there is no following_node
+        # we'd still need to return to this (or I guess some recursive
+        # loop over children[-1]. Let me know if you have a preference
+        # w.r.t performance as this is probably the worst option but
+        # easy to read.
+        start_position = node.lhs.abs_position
+        end_position = last_element.walk(Node)[-1].abs_position
+
+        for access in accesses:
+            # We can ignore writes as this overwrites any data from
+            # this assignment.
+            if isinstance(access, Reference) and access.is_write:
+                continue
+            pos = access.abs_position
+            # If we have a read access outside of the region then
+            # we can't add this assignment to the region.
+            if pos < start_position or pos > end_position:
+                return False
+        # Privatisation of variables is handled when applying the
+        # transformation.
+        return True
+
+    def _apply_transformation(self, block: list[Node],
+                              **kwargs):
+        '''
+        Applies the transformation defined by self._transformation to the
+        block supplied, with the kwargs provided.
+
+        The default implementation does nothing else, however the
+        functionality is provided so subclasses can perform specific
+        operations (or provide additional specific options) as required for
+        their transformations.
+
+        :param block: The block to apply the transformations to.
+        '''
+        # FIXME privatisation.
+        # If we have any assignments directly in the block then we need to
+        # do privatisation
+        self._transformation.apply(block, **kwargs)
 
     def apply(self, nodes: Union[Node, Schedule, list[Node]], **kwargs):
         '''Applies the transformation to the nodes provided.
