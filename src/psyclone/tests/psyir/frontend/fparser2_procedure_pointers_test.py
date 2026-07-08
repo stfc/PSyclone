e64cf67c1ffa2f664e1a9ed7baed8a9da82d8e49
Sergi Siso <sergiesg@gmail.com>
2026-07-08 11:59:18 +0100

    #2533 Improve procedure declaration tests

--- src/psyclone/tests/psyir/frontend/fparser2_procedure_pointers_test.py
+++ src/psyclone/tests/psyir/frontend/fparser2_procedure_pointers_test.py
@@ -38,6 +38,7 @@
     procedure pointers. '''
 
 from psyclone.psyir.nodes import CodeBlock
+from psyclone.psyir.symbols import Symbol
 
 
 def test_procedure_pointers(fortran_reader, fortran_writer):
@@ -45,10 +46,17 @@ def test_procedure_pointers(fortran_reader, fortran_writer):
     code = """
     module procedures
         use other
+        procedure(my_proc_interface) :: proc_ptr_private
+        procedure(my_proc_interface) :: proc_ptr_public
+        public proc_ptr_public
+        private proc_ptr_private
         implicit none
         contains
         subroutine test
-            procedure(my_proc_interface), pointer :: proc_ptr
+            procedure(my_proc_interface), pointer :: proc_ptr, &
+                proc_ptr_2 => null()
+
+            save proc_ptr
 
             ! They can be assinged to
             proc_ptr => my_func
@@ -66,3 +74,17 @@ def test_procedure_pointers(fortran_reader, fortran_writer):
 
     # This code must not have CodeBlocks
     assert len(psyir.walk(CodeBlock)) == 0
+
+    # Check that the module proc_delcs exist and have the correct visibility
+    module = psyir.children[0]
+    priv = Symbol.Visibility.PRIVATE
+    pub = Symbol.Visibility.PUBLIC
+    assert module.symbol_table.lookup("proc_ptr_private").visibility == priv
+    assert module.symbol_table.lookup("proc_ptr_public").visibility == pub
+    
+    # Check that the routine proc_decls exist and have the correct attributes
+    routine = module.children[0]
+    # assert routine.symbol_table.lookup("proc_ptr").is_static
+    # assert not routine.symbol_table.lookup("proc_ptr_2").is_static
+    # assert routine.symbol_table.lookup("proc_ptr").initial_value is None
+    # assert routine.symbol_table.lookup("proc_ptr_2").initial_value
