77ef83d5a2b420d268f2d609311182d2cd61ec08
Sergi Siso <sergiesg@gmail.com>
2026-07-28 10:18:54 +0100

    #1995 Update header to SPDX format

--- doc/conf.py
+++ doc/conf.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# SPDX-FileCopyrightText: Copyright (c) 2017-2026 Science and Technology
+#                         Facilities Council
+# SPDX-License-Identifier: BSD-3-Clause
+# See the full LICENSE file in the project root for details.
+# -----------------------------------------------------------------------------
 
 '''
  PSyclone documentation build configuration file, created by
--- examples/xdsl/stencil_lower.py
+++ examples/xdsl/stencil_lower.py
@@ -1,3 +1,10 @@
+# -----------------------------------------------------------------------------
+# SPDX-FileCopyrightText: Copyright (c) 2017-2026 Science and Technology
+#                         Facilities Council
+# SPDX-License-Identifier: BSD-3-Clause
+# See the full LICENSE file in the project root for details.
+# -----------------------------------------------------------------------------
+
 from subprocess import run, PIPE
 
 '''
