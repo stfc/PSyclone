f9263f6d393b101b5fa7a6e9f006b307a6e40587
LonelyCat124 <3043914+LonelyCat124@users.noreply.github.com.>
2026-07-31 09:21:38 +0100

    skip the bad argument error test with ifx

--- src/psyclone/tests/utilities_test.py
+++ src/psyclone/tests/utilities_test.py
@@ -148,7 +148,7 @@ def test_compiler_with_flags(monkeypatch):
     we pass something that is definitely not a flag and check that
     the compiler complains. This test is skipped if no compilation
     tests have been requested (--compile flag to py.test). '''
-    if not Compile.TEST_COMPILE:
+    if not Compile.TEST_COMPILE or Compile.F90 == "ifx":
         # If compilation is disable, use '/usr/bin/true' as 'compile'
         # to cover more lines:
         monkeypatch.setattr(Compile, "TEST_COMPILE", True)
