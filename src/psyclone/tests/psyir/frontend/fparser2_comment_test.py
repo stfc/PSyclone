be525a28ff790df91a8434417c7b59ea426cdefb
LonelyCat124 <3043914+LonelyCat124@users.noreply.github.com.>
2026-08-12 11:21:31 +0100

    Fixed comment

--- src/psyclone/tests/psyir/frontend/fparser2_comment_test.py
+++ src/psyclone/tests/psyir/frontend/fparser2_comment_test.py
@@ -636,6 +636,13 @@ def test_labels_found_with_comments(fortran_writer):
     out = fortran_writer(psyir)
     # Correct is formatted strangely to avoid issues with
     # linting due to empty lines or trailing whitespace.
+    # The \n   \n  100\ section appears as:
+    #!  - Unsupported statement: Goto_Stmt
+    #
+    #  100 DO i=...
+    # in the code output, but formatting correct like that
+    # results in errors from flake8, so this is chosen to mitigate those
+    # errors.
     correct = """subroutine foo()
   integer :: i
   integer :: j
