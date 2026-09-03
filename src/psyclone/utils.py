846ba1f99111a5e535ac9565fcfa90c5cbe08632
Sergi Siso <sergiesg@gmail.com>
2026-09-03 16:34:27 +0100

    #3579 Update changelog and add a comment

--- src/psyclone/utils.py
+++ src/psyclone/utils.py
@@ -155,6 +155,8 @@ def transformation_documentation_wrapper(*args,
         :raises InternalError: if cls is not a Transformation.
         '''
         if not __debug__:
+            # Python without debug info doesn't have the necessary docstrings,
+            # so we return without modifying anything
             return cls
         # pylint: disable=import-outside-toplevel
         from psyclone.psyGen import Transformation
