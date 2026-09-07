1832b6fa9857df4534116a1036d00c6663a1786e
LonelyCat124 <3043914+LonelyCat124@users.noreply.github.com.>
2026-09-07 13:37:06 +0100

    #3583 Update changelog

--- src/psyclone/tests/psyir/symbols/symbol_table_test.py
+++ src/psyclone/tests/psyir/symbols/symbol_table_test.py
@@ -710,7 +710,8 @@ def test_swap_symbol_preserves_tag():
     sym_table.swap(symbol1, symbol2)
 
     assert sym_table.lookup("var1") is symbol2
-    # Search for tag must find the new symbol rather than being dropped.
+    # Searching for the tag must find the new symbol rather than being
+    # dropped.
     assert sym_table.lookup_with_tag("var1_tag") is symbol2
 
 
