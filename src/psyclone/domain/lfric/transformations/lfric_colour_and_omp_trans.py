ddb350fc31997c6d0d1ff741194037d6276bf06a
Hannah Frost <hanfros@yahoo.com>
2026-09-10 11:02:44 +0100

    #3503 port colouring half and add colouring tests

--- src/psyclone/domain/lfric/transformations/lfric_colour_and_omp_trans.py
+++ src/psyclone/domain/lfric/transformations/lfric_colour_and_omp_trans.py
@@ -5,16 +5,19 @@
 # See the full LICENSE file in the project root for details.
 # -----------------------------------------------------------------------------
 
-'''This module provides a meta-transofmation that applies colouring and
+'''This module provides a meta-transformation that applies colouring and
 then OpenMP parallelisation to the loops in an LFRic routine.'''
 
+from typing import Union
+
+from psyclone.domain.lfric.lfric_constants import LFRicConstants
 from psyclone.psyGen import Transformation
-from psyclone.psyir.nodes import Routine
+from psyclone.psyir.nodes import Routine, Loop, ProfileNode
 from psyclone.psyir.transformations.omp_parallel_trans import OMPParallelTrans
+from psyclone.psyir.transformations.transformation_error import (
+    TransformationError)
 from psyclone.transformations import LFRicColourTrans, LFRicOMPLoopTrans
 from psyclone.utils import transformation_documentation_wrapper
-
-from typing import Union
 
 
 @transformation_documentation_wrapper
@@ -28,6 +31,7 @@ class LFRicColourAndOMPTrans(Transformation):
 
     def validate(self, node: Routine,
                  reprod: Union[bool, None] = None, **kwargs):
+        # pylint: disable=arguments-renamed
         '''
         Validates the input options of the LFRicColourAndOMPTrans.
 
@@ -35,15 +39,54 @@ class LFRicColourAndOMPTrans(Transformation):
         :param reprod: whether to use reproducible form of OpenMP reduction.
         If none, the default value from the configuration is used.
         '''
-        raise NotImplementedError("TODO #3503")
+        self.validate_options(reprod=reprod, **kwargs)
+
+        if not isinstance(node, Routine):
+            raise TransformationError(
+                f"Error in {self.name} transformation. The supplied "
+                f"node should be a Routine but found '{type(node).__name__}'.")
+
+        # Colouring must happen before any profiling calipers are added
+        # otherwise the loops we need to transform are no longer children
+        # of the Routine.
+        if node.walk(ProfileNode):
+            raise TransformationError(
+                f"Error in {self.name} transformation. This transformation "
+                f"must be applied BEFORE any profiling transformation but "
+                f"the supplied Routine '{node.name}' already contains a "
+                f"ProfileNode.")
+
+    def _colour_loops(self, node: Routine, **colour_kwargs):
+        '''
+        Applies colouring to every loop over cell-columns that is on a
+        continuous function space.
+
+        :param node: the Routine whose loops are to be coloured.
+        :param colour_kwargs: keyword arguments for the LFRicColourTrans
+        '''
+        const = LFRicConstants()
+        ctrans = LFRicColourTrans()
+
+        for child in node.children[:]:
+            if (isinstance(child, Loop)
+                    and child.iteration_space.endswith("cell_column")
+                    and child.field_space.orig_name
+                    not in const.VALID_DISCONTINUOUS_NAMES):
+                ctrans.apply(child, **colour_kwargs)
 
     def apply(self, node: Routine, reprod: Union[bool, None] = None, **kwargs):
+        # pylint: disable=arguments-renamed
         '''
         :param node: the Routine node to transform
         :param reprod: whether to use reproducible form of OpenMP reduction.
         If none, the default value from the configuration is used.
         '''
-        raise NotImplementedError("TODO #3503")
+        local_kwargs, colour_kwargs, _, _ = self.split_kwargs(
+            reprod=reprod, **kwargs)
+
+        self.validate(node, **local_kwargs)
+
+        self._colour_loops(node, **colour_kwargs)
 
 
 # For Sphinx AutoAPI documentation generation
--- src/psyclone/tests/domain/lfric/transformations/lfric_colour_and_omp_trans_test.py
+++ src/psyclone/tests/domain/lfric/transformations/lfric_colour_and_omp_trans_test.py
@@ -7,10 +7,71 @@
 
 '''Module containing tests for the LFRicColourAndOMPTrans transformation.'''
 
+import pytest
+
 from psyclone.domain.lfric.transformations import LFRicColourAndOMPTrans
+from psyclone.psyir.nodes import Loop
+from psyclone.psyir.transformations import ProfileTrans, TransformationError
+from psyclone.tests.utilities import get_invoke
+
+# The version of the API that the tests in this file
+# exercise.
+TEST_API = "lfric"
 
 
-# so CI doesnt complain
-def test_import():
-    ''' Check the transformation can be imported. '''
-    assert LFRicColourAndOMPTrans()
+def test_validate_wrong_node_type():
+    ''' Check that validate rejects a node that is not a Routine. '''
+    _, invoke = get_invoke("1_single_invoke.f90", TEST_API, idx=0,
+                           dist_mem=False)
+    loop = invoke.schedule.walk(Loop)[0]
+    with pytest.raises(TransformationError) as err:
+        LFRicColourAndOMPTrans().validate(loop)
+    assert "should be a Routine but found 'LFRicLoop'" in str(err.value)
+
+
+def test_validate_rejects_existing_profile_node():
+    ''' Check that validate rejects a Routine that already has profiling. '''
+    _, invoke = get_invoke("1_single_invoke.f90", TEST_API, idx=0,
+                           dist_mem=False)
+    sched = invoke.schedule
+    ProfileTrans().apply(sched.children[0])
+    with pytest.raises(TransformationError) as err:
+        LFRicColourAndOMPTrans().validate(sched)
+    assert "already contains a ProfileNode" in str(err.value)
+
+
+def test_validate_rejects_invalid_option():
+    ''' Check that an unrecognised option is rejected. '''
+    _, invoke = get_invoke("1_single_invoke.f90", TEST_API, idx=0,
+                           dist_mem=False)
+    with pytest.raises(ValueError):
+        LFRicColourAndOMPTrans().validate(invoke.schedule, not_an_option=True)
+
+
+def test_apply_colours_continuous_loop():
+    ''' Check a loop on a continuous space (w1) is coloured. '''
+    _, invoke = get_invoke("1_single_invoke.f90", TEST_API, idx=0,
+                           dist_mem=False)
+    sched = invoke.schedule
+    LFRicColourAndOMPTrans().apply(sched)
+    assert [loop.loop_type for loop in sched.walk(Loop)] == \
+        ["colours", "cells_in_colour"]
+
+
+def test_apply_skips_discontinuous_loop():
+    ''' Check a loop on a discontinuous space (w3) is not coloured. '''
+    _, invoke = get_invoke("1_single_invoke_w3.f90", TEST_API, idx=0,
+                           dist_mem=False)
+    sched = invoke.schedule
+    LFRicColourAndOMPTrans().apply(sched)
+    assert [loop.loop_type for loop in sched.walk(Loop)] == [""]
+
+
+def test_apply_passes_tiling_to_colour_trans():
+    ''' Check the tiling option is routed to LFRicColourTrans. '''
+    _, invoke = get_invoke("1_single_invoke.f90", TEST_API, idx=0,
+                           dist_mem=False)
+    sched = invoke.schedule
+    LFRicColourAndOMPTrans().apply(sched, tiling=True)
+    assert [loop.loop_type for loop in sched.walk(Loop)] == \
+        ["colours", "tiles_in_colour", "cells_in_tile"]
