db0b6265fc37be3cbf1ecc606ffa950229cc1772
LonelyCat124 <3043914+LonelyCat124@users.noreply.github.com.>
2026-09-17 15:59:37 +0100

    update examples

--- examples/lfric/eg6/omp_script.py
+++ examples/lfric/eg6/omp_script.py
@@ -39,7 +39,7 @@ def trans(psyir):
     else:
         # Loop fuse the two built-in kernels. The 'same_space' flag needs to
         # be set as built-ins are over ANY_SPACE.
-        ftrans.apply(schedule[0], schedule[1], {"same_space": True})
+        ftrans.apply((schedule[0], schedule[1]), {"same_space": True})
 
         # Add an OpenMP parallel do directive to the resultant loop-fused loop
         otrans.apply(schedule.children[0])
