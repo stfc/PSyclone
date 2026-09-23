e93a73ca878928780efc733e9de9197be81e8f62
Andrew Porter <andrew.porter@stfc.ac.uk>
2026-09-23 10:14:00 +0100

    #3569 update version for 3.4.0-rc1

--- src/psyclone/version.py
+++ src/psyclone/version.py
@@ -12,11 +12,11 @@
     It is NOT used in doc/reference_guide/doxygen.config'''
 
 __MAJOR__ = 3
-__MINOR__ = 3
-__MICRO__ = 1
+__MINOR__ = 4
+__MICRO__ = 0
 
 # Version suffix e.g. "-rc1", "-dev" or "" (for a full release)
-_VERSION_SUFFIX = ""
+_VERSION_SUFFIX = "-rc1"
 
 __SHORT_VERSION__ = f"{__MAJOR__:d}.{__MINOR__:d}{_VERSION_SUFFIX}"
 __VERSION__ = f"{__MAJOR__:d}.{__MINOR__:d}.{__MICRO__:d}{_VERSION_SUFFIX}"
